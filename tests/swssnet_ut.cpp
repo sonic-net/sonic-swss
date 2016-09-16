5ed18a2531d19f84d5fb2cd414930ea6f0c87e45
Qi Luo <qiluo-msft@users.noreply.github.com>
2016-09-15 17:57:54 -0700

    Add unit test

---
+++ tests/swssnet_ut.cpp
@@ -0,0 +1,64 @@
+#include <arpa/inet.h>
+#include <gtest/gtest.h>
+#include <string>
+#include <iostream>
+#include "swssnet.h"
+
+using namespace std;
+using namespace swss;
+
+TEST(IpAddress, copy)
+{
+    IpAddress ip("2001:4898:f0:f153:357c:77b2:49c9:627c");
+    sai_ip_address_t dst;
+    copy(dst, ip);
+    EXPECT_EQ(dst.addr_family, SAI_IP_ADDR_FAMILY_IPV6);
+    
+    char buf[INET6_ADDRSTRLEN];
+    inet_ntop(AF_INET6, dst.addr.ip6, buf, INET6_ADDRSTRLEN);
+    EXPECT_STREQ(buf, "2001:4898:f0:f153:357c:77b2:49c9:627c");
+}
+
+TEST(IpPrefix, copy)
+{
+    IpPrefix ip("2001:4898:f0:f153:357c:77b2:49c9:627c/27");
+    sai_ip_prefix_t dst;
+    copy(dst, ip);
+    EXPECT_EQ(dst.addr_family, SAI_IP_ADDR_FAMILY_IPV6);
+    
+    char buf[INET6_ADDRSTRLEN];
+    inet_ntop(AF_INET6, dst.addr.ip6, buf, INET6_ADDRSTRLEN);
+    EXPECT_STREQ(buf, "2001:4898:f0:f153:357c:77b2:49c9:627c");
+    inet_ntop(AF_INET6, dst.mask.ip6, buf, INET6_ADDRSTRLEN);
+    printf("%s\n", buf);
+    EXPECT_STREQ(buf, "ffff:ffe0::");
+}
+
+TEST(IpAddress, copy2)
+{
+    IpAddress ip("2001:4898:f0:f153:357c:77b2:49c9:627c");
+    sai_ip_prefix_t dst;
+    copy(dst, ip);
+    EXPECT_EQ(dst.addr_family, SAI_IP_ADDR_FAMILY_IPV6);
+    
+    char buf[INET6_ADDRSTRLEN];
+    inet_ntop(AF_INET6, dst.addr.ip6, buf, INET6_ADDRSTRLEN);
+    EXPECT_STREQ(buf, "2001:4898:f0:f153:357c:77b2:49c9:627c");
+    inet_ntop(AF_INET6, dst.mask.ip6, buf, INET6_ADDRSTRLEN);
+    EXPECT_STREQ(buf, "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff");
+}
+
+TEST(sai_ip_prefix_t, subnet)
+{
+    sai_ip_prefix_t dst, src;
+    src.addr_family = SAI_IP_ADDR_FAMILY_IPV6;
+    inet_pton(AF_INET6, "2001:4898:f0:f153:357c:77b2:49c9:627c", src.addr.ip6);
+    inet_pton(AF_INET6, "ffff:ffe0::", src.mask.ip6);
+    
+    subnet(dst, src);
+    char buf[INET6_ADDRSTRLEN];
+    inet_ntop(AF_INET6, dst.addr.ip6, buf, INET6_ADDRSTRLEN);
+    EXPECT_STREQ(buf, "2001:4880::");
+    inet_ntop(AF_INET6, dst.mask.ip6, buf, INET6_ADDRSTRLEN);
+    EXPECT_STREQ(buf, "ffff:ffe0::");
+}
