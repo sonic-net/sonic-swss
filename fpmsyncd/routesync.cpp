d8ca7ea11b880d096801d9c41dac3b8e812e3aa1
Qi Luo <qiluo-msft@users.noreply.github.com>
2016-09-27 14:58:50 -0700

    Fix gateway ipaddress: support ipv6

--- fpmsyncd/routesync.cpp
+++ fpmsyncd/routesync.cpp
@@ -92,8 +92,9 @@ void RouteSync::onMsg(int nlmsg_type, struct nl_object *obj)
 
         if (addr != NULL)
         {
-            IpAddress nh(*(uint32_t*)nl_addr_get_binary_addr(addr));
-            nexthops += nh.to_string();
+            char gwipprefix[MAX_ADDR_SIZE + 1] = {0};
+            nl_addr2str(addr, gwipprefix, MAX_ADDR_SIZE);
+            nexthops += gwipprefix;
         }
 
         rtnl_link_i2name(m_link_cache, ifindex, ifname, IFNAMSIZ);
