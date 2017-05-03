9858bad5c9f2f9715867a89dac6cfabab5d67e37
Oleksandr Ivantsiv <oleksandri@mellanox.com>
2017-05-03 16:56:20 +0300

    Revert "[aclorch]: Temporarily disable ACL thread of collecting counters (#202)"
    This reverts commit 2f594c75036dfd41b6b40dd5b92f4fda7ebb7a68.

--- orchagent/aclorch.h
+++ orchagent/aclorch.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <sstream>
+#include <thread>
 #include <mutex>
 #include <tuple>
 #include <condition_variable>
@@ -227,7 +228,7 @@ inline void split(string str, Iterable& out, char delim = ' ')
     }
 }
 
-class AclOrch : public Orch, public Observer
+class AclOrch : public Orch, public Observer, public thread
 {
 public:
     AclOrch(DBConnector *db, vector<string> tableNames, PortsOrch *portOrch, MirrorOrch *mirrorOrch);
