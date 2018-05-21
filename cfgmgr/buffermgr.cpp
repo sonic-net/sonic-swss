c4b7c387fd618ea5091596b16513a93d5501ecca
Zhenggen Xu <zxu@linkedin.com>
2018-05-20 23:24:57 -0700

    Suppress Cable length error in case buffer config file is not provided

--- cfgmgr/buffermgr.cpp
+++ cfgmgr/buffermgr.cpp
@@ -34,6 +34,8 @@ void BufferMgr::readPgProfileLookupFile(string file)
     ifstream infile(file);
     if (!infile.is_open())
     {
+        m_pgfile_readable = false;
+        SWSS_LOG_WARN("PG profile lookup file: %s is not readable", file.c_str());
         return;
     }
 
@@ -211,7 +213,7 @@ void BufferMgr::doTask(Consumer &consumer)
                     task_status = doCableTask(fvField(i), fvValue(i));
                 }
                 // In case of PORT table update, Buffer Manager is interested in speed update only
-                if (table_name == CFG_PORT_TABLE_NAME && fvField(i) == "speed")
+                if (m_pgfile_readable && table_name == CFG_PORT_TABLE_NAME && fvField(i) == "speed")
                 {
                     // create/update profile for port
                     task_status = doSpeedUpdateTask(port, fvValue(i));
