35ba30e0909b7e2abff1dcb8f75a3f9b6a9cc899
Sihui Han <sihan@microsoft.com>
2018-08-02 23:54:07 +0000

    [aclorch]: only bind to port for ACL_TABLE_PFCWD type
    Signed-off-by: Sihui Han <sihan@microsoft.com>

--- orchagent/aclorch.cpp
+++ orchagent/aclorch.cpp
@@ -1022,7 +1022,7 @@ bool AclTable::create()
 
     sai_attribute_t attr;
     vector<sai_attribute_t> table_attrs;
-    vector<int32_t> bpoint_list = { SAI_ACL_BIND_POINT_TYPE_PORT, SAI_ACL_BIND_POINT_TYPE_LAG };
+    vector<int32_t> bpoint_list = type == ACL_TABLE_PFCWD ? { SAI_ACL_BIND_POINT_TYPE_PORT} : { SAI_ACL_BIND_POINT_TYPE_PORT, SAI_ACL_BIND_POINT_TYPE_LAG };
     attr.id = SAI_ACL_TABLE_ATTR_ACL_BIND_POINT_TYPE_LIST;
     attr.value.s32list.count = static_cast<uint32_t>(bpoint_list.size());
     attr.value.s32list.list = bpoint_list.data();
