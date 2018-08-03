401d6194987d89eebdc660e57b2ce807b65a5c0e
Sihui Han <sihan@microsoft.com>
2018-08-03 00:39:28 +0000

    make the code easier to read
    Signed-off-by: Sihui Han <sihan@microsoft.com>

--- orchagent/aclorch.cpp
+++ orchagent/aclorch.cpp
@@ -1022,7 +1022,12 @@ bool AclTable::create()
 
     sai_attribute_t attr;
     vector<sai_attribute_t> table_attrs;
-    vector<int32_t> bpoint_list = type == ACL_TABLE_PFCWD ? { SAI_ACL_BIND_POINT_TYPE_PORT} : { SAI_ACL_BIND_POINT_TYPE_PORT, SAI_ACL_BIND_POINT_TYPE_LAG };
+    vector<int32_t> bpoint_list = { SAI_ACL_BIND_POINT_TYPE_PORT, SAI_ACL_BIND_POINT_TYPE_LAG };
+    if (type == ACL_TABLE_PFCWD)
+    {
+        bpoint_list = { SAI_ACL_BIND_POINT_TYPE_PORT };
+    }
+
     attr.id = SAI_ACL_TABLE_ATTR_ACL_BIND_POINT_TYPE_LIST;
     attr.value.s32list.count = static_cast<uint32_t>(bpoint_list.size());
     attr.value.s32list.list = bpoint_list.data();
