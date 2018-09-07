36ce83682cc31021bb67762189f9df1df5ee378d
Shu0T1an ChenG <shuche@microsoft.com>
2018-09-07 13:24:44 -0700

    [mirrororch]: Change Mellanox GRE type to 0x8949
    Signed-off-by: Shu0T1an ChenG <shuche@microsoft.com>

--- orchagent/mirrororch.cpp
+++ orchagent/mirrororch.cpp
@@ -44,7 +44,7 @@ MirrorEntry::MirrorEntry(const string& platform) :
 {
     if (platform == MLNX_PLATFORM_SUBSTRING)
     {
-        greType = 0x6558;
+        greType = 0x8949;
         queue = 1;
     }
     else
