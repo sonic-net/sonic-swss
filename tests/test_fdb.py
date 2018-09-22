4ba2adf05c6666af953c798f1e1cf88eedc91769
Qi Luo <qiluo-msft@users.noreply.github.com>
2018-09-22 02:44:04 +0000

    Fix test
    Signed-off-by: Qi Luo <qiluo-msft@users.noreply.github.com>

--- tests/test_fdb.py
+++ tests/test_fdb.py
@@ -39,70 +39,16 @@ def how_many_entries_exist(db, table):
     tbl =  swsscommon.Table(db, table)
     return len(tbl.getKeys())
 
-def test_FDBAddedAfterMemberCreated(dvs):
-    appl_db = swsscommon.DBConnector(swsscommon.APPL_DB, dvs.redis_sock, 0)
-    asic_db = swsscommon.DBConnector(swsscommon.ASIC_DB, dvs.redis_sock, 0)
-    conf_db = swsscommon.DBConnector(swsscommon.CONFIG_DB, dvs.redis_sock, 0)
-
-    # create a FDB entry in Application DB
-    create_entry_pst(
-        appl_db,
-        "FDB_TABLE", "Vlan2:52-54-00-25-06-E9",
-        [
-            ("port", "Ethernet0"),
-            ("type", "dynamic"),
-        ]
-    )
-
-    # check that the FDB entry wasn't inserted into ASIC DB
-    assert how_many_entries_exist(asic_db, "ASIC_STATE:SAI_OBJECT_TYPE_FDB_ENTRY") == 0, "The fdb entry leaked to ASIC"
-
-    # create vlan
-    create_entry_tbl(
-        conf_db,
-        "VLAN", "Vlan2",
-        [
-            ("vlanid", "2"),
-        ]
-    )
-
-    # create vlan member entry in application db
-    create_entry_tbl(
-        conf_db,
-        "VLAN_MEMBER", "Vlan2|Ethernet0",
-        [
-            ("tagging_mode", "untagged"),
-        ]
-    )
-
-    # check that the vlan information was propagated
-    assert how_many_entries_exist(asic_db, "ASIC_STATE:SAI_OBJECT_TYPE_VLAN") == 2, "The 2 vlan wasn't created"
-    assert how_many_entries_exist(asic_db, "ASIC_STATE:SAI_OBJECT_TYPE_BRIDGE_PORT") == 1, "The bridge port wasn't created"
-    assert how_many_entries_exist(asic_db, "ASIC_STATE:SAI_OBJECT_TYPE_VLAN_MEMBER") == 1, "The vlan member wasn't added"
-
-    # Get mapping between interface name and its bridge port_id
-    iface_2_bridge_port_id = get_map_iface_bridge_port_id(asic_db, dvs)
-
-    # check that the FDB entry was inserted into ASIC DB
-    assert how_many_entries_exist(asic_db, "ASIC_STATE:SAI_OBJECT_TYPE_FDB_ENTRY") == 1, "The fdb entry wasn't inserted to ASIC"
-
-    ok, extra = dvs.is_fdb_entry_exists(asic_db, "ASIC_STATE:SAI_OBJECT_TYPE_FDB_ENTRY",
-                    [("mac", "52-54-00-25-06-E9"), ("vlan", "2")],
-                    [("SAI_FDB_ENTRY_ATTR_TYPE", "SAI_FDB_ENTRY_TYPE_DYNAMIC"),
-                     ("SAI_FDB_ENTRY_ATTR_BRIDGE_PORT_ID", iface_2_bridge_port_id["Ethernet0"]),
-                     ('SAI_FDB_ENTRY_ATTR_PACKET_ACTION', 'SAI_PACKET_ACTION_FORWARD')]
-    )
-    assert ok, str(extra)
-
-
 def test_fdb_notifications(dvs):
     dvs.setup_db()
 
-    dvs.runcmd("crm config polling interval 1")
+    #dvs.runcmd("sonic-clear fdb all")
 
+    dvs.runcmd("crm config polling interval 1")
     dvs.setReadOnlyAttr('SAI_OBJECT_TYPE_SWITCH', 'SAI_SWITCH_ATTR_AVAILABLE_FDB_ENTRY', '1000')
 
     time.sleep(2)
+    counter_before = dvs.getCrmCounterValue('STATS', 'crm_stats_fdb_entry_used')
 
     # create vlan; create vlan member
     dvs.create_vlan("6")
@@ -120,10 +66,14 @@ def test_fdb_notifications(dvs):
     dvs.servers[17].runcmd("ip route add default via 6.6.6.1")
 
     # get neighbor and arp entry
+    time.sleep(2)
     rc = dvs.servers[16].runcmd("ping -c 1 6.6.6.7")
+    assert rc == 0
+    rc = dvs.servers[17].runcmd("ping -c 1 6.6.6.6")
     assert rc == 0
 
     # Get mapping between interface name and its bridge port_id
+    time.sleep(2)
     iface_2_bridge_port_id = get_map_iface_bridge_port_id(dvs.adb, dvs)
 
     # check that the FDB entries were inserted into ASIC DB
@@ -143,8 +93,8 @@ def test_fdb_notifications(dvs):
     assert ok, str(extra)
 
     time.sleep(2)
-    used_counter = dvs.getCrmCounterValue('STATS', 'crm_stats_fdb_entry_used')
-    assert used_counter == 2
+    counter_inserted = dvs.getCrmCounterValue('STATS', 'crm_stats_fdb_entry_used')
+    assert counter_inserted - counter_before == 2
 
     # check that the FDB entries were inserted into State DB
     ok, extra = dvs.is_table_entry_exists(dvs.sdb, "FDB_TABLE",
@@ -199,8 +149,8 @@ def test_fdb_notifications(dvs):
         assert ok, str(extra)
 
         time.sleep(2)
-        used_counter = dvs.getCrmCounterValue('STATS', 'crm_stats_fdb_entry_used')
-        assert used_counter == 2
+        counter_restarted = dvs.getCrmCounterValue('STATS', 'crm_stats_fdb_entry_used')
+        assert counter_inserted == counter_restarted
 
     finally:
         # disable warm restart
@@ -212,3 +162,67 @@ def test_fdb_notifications(dvs):
                 ("enable", "false"),
             ]
         )
+
+def test_FDBAddedAfterMemberCreated(dvs):
+    dvs.setup_db()
+
+    dvs.runcmd("sonic-clear fdb all")
+    time.sleep(2)
+
+    # create a FDB entry in Application DB
+    create_entry_pst(
+        dvs.pdb,
+        "FDB_TABLE", "Vlan2:52-54-00-25-06-E9",
+        [
+            ("port", "Ethernet0"),
+            ("type", "dynamic"),
+        ]
+    )
+
+    # check that the FDB entry wasn't inserted into ASIC DB
+    assert how_many_entries_exist(dvs.adb, "ASIC_STATE:SAI_OBJECT_TYPE_FDB_ENTRY") == 0, "The fdb entry leaked to ASIC"
+
+    vlan_before = how_many_entries_exist(dvs.adb, "ASIC_STATE:SAI_OBJECT_TYPE_VLAN")
+    bp_before = how_many_entries_exist(dvs.adb, "ASIC_STATE:SAI_OBJECT_TYPE_BRIDGE_PORT")
+    vm_before = how_many_entries_exist(dvs.adb, "ASIC_STATE:SAI_OBJECT_TYPE_VLAN_MEMBER")
+
+    # create vlan
+    create_entry_tbl(
+        dvs.cdb,
+        "VLAN", "Vlan2",
+        [
+            ("vlanid", "2"),
+        ]
+    )
+
+    # create vlan member entry in application db
+    create_entry_tbl(
+        dvs.cdb,
+        "VLAN_MEMBER", "Vlan2|Ethernet0",
+        [
+            ("tagging_mode", "untagged"),
+        ]
+    )
+
+    # check that the vlan information was propagated
+    vlan_after = how_many_entries_exist(dvs.adb, "ASIC_STATE:SAI_OBJECT_TYPE_VLAN")
+    bp_after = how_many_entries_exist(dvs.adb, "ASIC_STATE:SAI_OBJECT_TYPE_BRIDGE_PORT")
+    vm_after = how_many_entries_exist(dvs.adb, "ASIC_STATE:SAI_OBJECT_TYPE_VLAN_MEMBER")
+
+    assert vlan_after - vlan_before == 1, "The Vlan2 wasn't created"
+    assert bp_after - bp_before == 1, "The bridge port wasn't created"
+    assert vm_after - vm_before == 1, "The vlan member wasn't added"
+
+    # Get mapping between interface name and its bridge port_id
+    iface_2_bridge_port_id = get_map_iface_bridge_port_id(dvs.adb, dvs)
+
+    # check that the FDB entry was inserted into ASIC DB
+    assert how_many_entries_exist(dvs.adb, "ASIC_STATE:SAI_OBJECT_TYPE_FDB_ENTRY") == 1, "The fdb entry wasn't inserted to ASIC"
+
+    ok, extra = dvs.is_fdb_entry_exists(dvs.adb, "ASIC_STATE:SAI_OBJECT_TYPE_FDB_ENTRY",
+                    [("mac", "52-54-00-25-06-E9"), ("vlan", "2")],
+                    [("SAI_FDB_ENTRY_ATTR_TYPE", "SAI_FDB_ENTRY_TYPE_DYNAMIC"),
+                     ("SAI_FDB_ENTRY_ATTR_BRIDGE_PORT_ID", iface_2_bridge_port_id["Ethernet0"]),
+                     ('SAI_FDB_ENTRY_ATTR_PACKET_ACTION', 'SAI_PACKET_ACTION_FORWARD')]
+    )
+    assert ok, str(extra)
