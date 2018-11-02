3657e7e3069739231d702f7f8564dd232a13ff39
yangbashuang <37615000+yangbashuang@users.noreply.github.com>
2018-11-02 08:58:36 +0800

    Update portsorch.h

--- orchagent/portsorch.h
+++ orchagent/portsorch.h
@@ -159,9 +159,9 @@ private:
     bool setPortSpeed(sai_object_id_t port_id, sai_uint32_t speed);
     bool getPortSpeed(sai_object_id_t port_id, sai_uint32_t &speed);
 
-    bool getQueueTypeAndIndex(sai_object_id_t queue_id, string &type, uint8_t &index);
-
     bool setPortAdvSpeed(sai_object_id_t port_id, sai_uint32_t speed);
+    
+    bool getQueueTypeAndIndex(sai_object_id_t queue_id, string &type, uint8_t &index);
 
     bool m_isQueueMapGenerated = false;
     void generateQueueMapPerPort(const Port& port);
