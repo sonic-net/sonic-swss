bf1f84271c175d8e7bba4e30eb1eab21c29e86b6
Shu0T1an ChenG <shuche@microsoft.com>
2019-05-14 15:49:09 -0700

    [vxlanorch]: Add extra info into NOTICE logs

--- orchagent/vnetorch.cpp
+++ orchagent/vnetorch.cpp
@@ -1423,7 +1423,7 @@ bool VNetOrch::addOperation(const Request& request)
                                 vnet_name.c_str(), tunnel.c_str());
             }
 
-            SWSS_LOG_INFO("VNET '%s' was added ", vnet_name.c_str());
+            SWSS_LOG_NOTICE("VNET '%s' was added ", vnet_name.c_str());
         }
         else
         {
