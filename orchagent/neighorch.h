edb334df8fba86cc73da8376d2815c5f1bba88a7
Shu0t1an Cheng <shuche@microsoft.com>
2019-09-18 19:45:59 +0000

    [mirrororch]: Freeze doTask() function instead of update() function
    With this update, we could fix potential orchagent issues before
    the warm reboot when the monitor port was wrongly calculated.
    
    Signed-off-by: Shu0T1an ChenG <shuche@microsoft.com>

--- orchagent/neighorch.h
+++ orchagent/neighorch.h
@@ -65,7 +65,6 @@ public:
     void decreaseNextHopRefCount(const IpAddress&);
 
     bool getNeighborEntry(const IpAddress&, NeighborEntry&, MacAddress&);
-    bool getNeighborEntry(const string&, NeighborEntry&, MacAddress&);
 
     bool ifChangeInformNextHop(const string &, bool);
     bool isNextHopFlagSet(const IpAddress &, const uint32_t);
