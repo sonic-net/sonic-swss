84b6d6752dae902d98e1ea6927d146553e6c88ee
Qi Luo <qiluo-msft@users.noreply.github.com>
2020-04-23 19:56:37 +0000

    Merge master

--- orchagent/vnetorch.cpp
+++ orchagent/vnetorch.cpp
@@ -429,6 +429,10 @@ VnetBridgeInfo VNetBitmapObject::getBridgeInfoByVni(uint32_t vni, string tunnelN
     attr.value.oid = info.bridge_id;
     rif_attrs.push_back(attr);
 
+    attr.id = SAI_ROUTER_INTERFACE_ATTR_MTU;
+    attr.value.u32 = VNET_BITMAP_RIF_MTU;
+    rif_attrs.push_back(attr);
+
     status = sai_router_intfs_api->create_router_interface(
             &info.rif_id,
             gSwitchId,
@@ -791,6 +795,12 @@ bool VNetBitmapObject::addTunnelRoute(IpPrefix& ipPrefix, tunnelEndpoint& endp)
     sai_ip_address_t underlayAddr;
     copy(underlayAddr, endp.ip);
 
+    if (tunnelRouteMap_.find(ipPrefix) != tunnelRouteMap_.end())
+    {
+        SWSS_LOG_WARN("VNET tunnel route %s exists", ipPrefix.to_string().c_str());
+        return true;
+    }
+
     VNetOrch* vnet_orch = gDirectory.get<VNetOrch*>();
     for (auto peer : peer_list)
     {
@@ -1092,6 +1102,12 @@ bool VNetBitmapObject::addRoute(IpPrefix& ipPrefix, nextHop& nh)
     uint32_t peerBitmap = vnet_id_;
     Port port;
     RouteInfo routeInfo;
+
+    if (routeMap_.find(ipPrefix) != routeMap_.end())
+    {
+        SWSS_LOG_WARN("VNET route %s exists", ipPrefix.to_string().c_str());
+        return true;
+    }
 
     bool is_subnet = (!nh.ips.getSize() || nh.ips.contains("0.0.0.0")) ? true : false;
 
