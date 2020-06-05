57613ce3bcb4f58cefcaef7d1509c4b831b21b24
Mykola Faryma <mykolaf@mellanox.com>
2020-06-05 15:14:38 +0300

    remove speed calc from lua scripts
    Signed-off-by: Mykola Faryma <mykolaf@mellanox.com>

--- orchagent/port_rates.lua
+++ orchagent/port_rates.lua
@@ -2,8 +2,6 @@
 -- ARGV[1] - counters db index
 -- ARGV[2] - counters table name
 -- ARGV[3] - poll time interval
--- ARGV[4] - asic db index
--- ARGV[5] - config db index
 -- return log
 
 local logtable = {}
@@ -12,34 +10,20 @@ local function logit(msg)
   logtable[#logtable+1] = tostring(msg)
 end
 
-local asic_db = ARGV[4]
 local counters_db = ARGV[1]
-local config_db = ARGV[5]
 local counters_table_name = ARGV[2] 
-local asic_table_name = "ASIC_STATE:SAI_OBJECT_TYPE_PORT" 
 local rates_table_name = "RATES"
 
 -- Get configuration
-redis.call('SELECT', config_db)
-local smooth_interval = redis.call('HGET', rates_table_name .. '|' .. 'PORT', 'PORT_SMOOTH_INTERVAL')
-local alpha = redis.call('HGET', rates_table_name .. '|' .. 'PORT', 'PORT_ALPHA')
+redis.call('SELECT', counters_db)
+local smooth_interval = redis.call('HGET', rates_table_name .. ':' .. 'PORT', 'PORT_SMOOTH_INTERVAL')
+local alpha = redis.call('HGET', rates_table_name .. ':' .. 'PORT', 'PORT_ALPHA')
 local one_minus_alpha = 1.0 - alpha
 local delta = tonumber(ARGV[3])
 
 logit(alpha)
 logit(one_minus_alpha)
 logit(delta)
-
--- Get speeds
-local speeds = {}
-redis.call('SELECT', asic_db)
-local n = table.getn(KEYS)
-for i = 1, n do
-   speeds[i] = redis.call('HGET', asic_table_name .. ':' .. KEYS[i], 'SAI_PORT_ATTR_SPEED')
-   logit(speeds[i])
-end
-
-redis.call('SELECT', counters_db)
 
 local initialized = redis.call('HGET', rates_table_name, 'INIT_DONE')
 
@@ -68,8 +52,6 @@ for i = 1, n do
         local tx_bps_new = (out_octets - out_octets_last)/delta
         local rx_pps_new = ((in_ucast_pkts + in_non_ucast_pkts) - (in_ucast_pkts_last + in_non_ucast_pkts_last))/delta
         local tx_pps_new = ((out_ucast_pkts + out_non_ucast_pkts) - (out_ucast_pkts_last + out_non_ucast_pkts_last))/delta
-        local rx_util_new = rx_bps_new/speeds[i]
-        local tx_util_new = tx_bps_new/speeds[i]
 
         if initialized == "DONE" then
             -- Get old rates values
@@ -77,29 +59,21 @@ for i = 1, n do
             local rx_pps_old = redis.call('HGET', rates_table_name .. ':' .. KEYS[i], 'RX_PPS')
             local tx_bps_old = redis.call('HGET', rates_table_name .. ':' .. KEYS[i], 'TX_BPS')
             local tx_pps_old = redis.call('HGET', rates_table_name .. ':' .. KEYS[i], 'TX_PPS')
-            local rx_util_old = redis.call('HGET', rates_table_name .. ':' .. KEYS[i], 'RX_UTIL')
-            local tx_util_old = redis.call('HGET', rates_table_name .. ':' .. KEYS[i], 'TX_UTIL')
 
             -- Smooth the rates values and store them in DB
             redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'RX_BPS', alpha*rx_bps_new + one_minus_alpha*rx_bps_old)
             redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'RX_PPS', alpha*rx_pps_new + one_minus_alpha*rx_pps_old)
             redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'TX_BPS', alpha*tx_bps_new + one_minus_alpha*tx_bps_old)
-            redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'TX_PPS', alpha*tx_pps_new + one_minus_alpha*tx_pps_old)
-            redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'RX_UTIL', alpha*rx_util_new + one_minus_alpha*rx_util_old)
-            redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'TX_UTIL', alpha*tx_util_new + one_minus_alpha*tx_util_old)    
+            redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'TX_PPS', alpha*tx_pps_new + one_minus_alpha*tx_pps_old)  
         else
             -- Store unsmoothed initial rates values in DB
             redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'RX_BPS', rx_bps_new)
             redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'RX_PPS', rx_pps_new)
             redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'TX_BPS', tx_bps_new)
             redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'TX_PPS', tx_pps_new)
-            redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'RX_UTIL', rx_util_new)
-            redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'TX_UTIL', tx_util_new)
             redis.call('HSET', rates_table_name, 'INIT_DONE', 'DONE')
         end        
-    
     else
-
         -- Set old COUNTERS values
         redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'SAI_PORT_STAT_IF_IN_UCAST_PKTS_last', in_ucast_pkts)
         redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'SAI_PORT_STAT_IF_IN_NON_UCAST_PKTS_last', in_non_ucast_pkts)
@@ -107,9 +81,7 @@ for i = 1, n do
         redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'SAI_PORT_STAT_IF_OUT_NON_UCAST_PKTS_last', out_non_ucast_pkts)
         redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'SAI_PORT_STAT_IF_IN_OCTETS_last', in_octets)
         redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'SAI_PORT_STAT_IF_OUT_OCTETS_last', out_octets)        
-
         redis.call('HSET', rates_table_name, 'INIT_DONE', 'COUNTERS_LAST')
-
     end
 end
 
--- orchagent/rif_rates.lua
+++ orchagent/rif_rates.lua
@@ -2,8 +2,6 @@
 -- ARGV[1] - counters db index
 -- ARGV[2] - counters table name
 -- ARGV[3] - poll time interval
--- ARGV[4] - asic db index
--- ARGV[5] - config db index
 -- return log
 
 local logtable = {}
@@ -12,28 +10,16 @@ local function logit(msg)
   logtable[#logtable+1] = tostring(msg)
 end
 
-local asic_db = ARGV[4]
 local counters_db = ARGV[1]
-local config_db = ARGV[5]
 local counters_table_name = ARGV[2] 
-
 local rates_table_name = "RATES"
 
 -- Get configuration
-redis.call('SELECT', config_db)
-local smooth_interval = redis.call('HGET', rates_table_name .. '|' .. 'RIF', 'RIF_SMOOTH_INTERVAL')
-local alpha = redis.call('HGET', rates_table_name .. '|' .. 'RIF', 'RIF_ALPHA')
+redis.call('SELECT', counters_db)
+local smooth_interval = redis.call('HGET', rates_table_name .. ':' .. 'RIF', 'RIF_SMOOTH_INTERVAL')
+local alpha = redis.call('HGET', rates_table_name .. ':' .. 'RIF', 'RIF_ALPHA')
 local one_minus_alpha = 1.0 - alpha
 local delta = tonumber(ARGV[3])
-
--- Get speeds
-local speeds = {}
-redis.call('SELECT', asic_db)
-local n = table.getn(KEYS)
-for i = 1, n do
-    speeds[i] = 40000 
-
-redis.call('SELECT', counters_db)
 
 local initialized = redis.call('HGET', rates_table_name, 'INIT_DONE')
 logit(initialized)
@@ -57,8 +43,6 @@ for i = 1, n do
         local tx_bps_new = (out_octets - out_octets_last)/delta
         local rx_pps_new = (in_pkts - in_pkts_last)/delta
         local tx_pps_new = (out_pkts - out_pkts_last)/delta
-        local rx_util_new = rx_bps_new/speeds[i]
-        local tx_util_new = tx_bps_new/speeds[i]
 
         if initialized == "DONE" then
             -- Get old rates values
@@ -66,35 +50,26 @@ for i = 1, n do
             local rx_pps_old = redis.call('HGET', rates_table_name .. ':' .. KEYS[i], 'RX_PPS')
             local tx_bps_old = redis.call('HGET', rates_table_name .. ':' .. KEYS[i], 'TX_BPS')
             local tx_pps_old = redis.call('HGET', rates_table_name .. ':' .. KEYS[i], 'TX_PPS')
-            local rx_util_old = redis.call('HGET', rates_table_name .. ':' .. KEYS[i], 'RX_UTIL')
-            local tx_util_old = redis.call('HGET', rates_table_name .. ':' .. KEYS[i], 'TX_UTIL')
 
             -- Smooth the rates values and store them in DB
             redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'RX_BPS', alpha*rx_bps_new + one_minus_alpha*rx_bps_old)
             redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'RX_PPS', alpha*rx_pps_new + one_minus_alpha*rx_pps_old)
             redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'TX_BPS', alpha*tx_bps_new + one_minus_alpha*tx_bps_old)
-            redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'TX_PPS', alpha*tx_pps_new + one_minus_alpha*tx_pps_old)
-            redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'RX_UTIL', alpha*rx_util_new + one_minus_alpha*rx_util_old)
-            redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'TX_UTIL', alpha*tx_util_new + one_minus_alpha*tx_util_old)    
+            redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'TX_PPS', alpha*tx_pps_new + one_minus_alpha*tx_pps_old)   
         else
             -- Store unsmoothed initial rates values in DB
             redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'RX_BPS', rx_bps_new)
             redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'RX_PPS', rx_pps_new)
             redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'TX_BPS', tx_bps_new)
             redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'TX_PPS', tx_pps_new)
-            redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'RX_UTIL', rx_util_new)
-            redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'TX_UTIL', tx_util_new)
             redis.call('HSET', rates_table_name, 'INIT_DONE', 'DONE')
         end        
-    
     else
-
         -- Set old COUNTERS values
         redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'SAI_ROUTER_INTERFACE_STAT_IN_OCTETS_last', in_octets)
         redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'SAI_ROUTER_INTERFACE_STAT_IN_PACKETS_last', in_pkts)
         redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'SAI_ROUTER_INTERFACE_STAT_OUT_OCTETS_last', out_octets)
         redis.call('HSET', rates_table_name .. ':' .. KEYS[i], 'SAI_ROUTER_INTERFACE_STAT_OUT_PACKETS_last', out_pkts)      
-
         redis.call('HSET', rates_table_name, 'INIT_DONE', 'COUNTERS_LAST')
     end
 end
