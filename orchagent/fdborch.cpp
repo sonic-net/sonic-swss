cfe96ef8dc4341f36694258822258543b17637d5
Rajkumar Pennadam Ramamoorthy <rpennadamram@marvell.com>
2020-07-29 08:37:41 +0530

    [fdborch] Fixed Orchagent crash in FDB flush on port disable.
    Signed-off-by: Rajkumar Pennadam Ramamoorthy <rpennadamram@marvell.com>

--- orchagent/fdborch.cpp
+++ orchagent/fdborch.cpp
@@ -232,13 +232,14 @@ void FdbOrch::update(sai_fdb_event_t        type,
                            update.entry.mac.to_string().c_str(),
                            vlanName.c_str(), update.port.m_alias.c_str());
 
-            for (const auto& itr : m_entries)
+            for (auto itr = m_entries.begin(); itr != m_entries.end();)
             {
-                if (itr.port_name == update.port.m_alias)
+                if (itr->port_name == update.port.m_alias)
                 {
-                    update.entry.mac = itr.mac;
-                    update.entry.bv_id = itr.bv_id;
+                    update.entry.mac = itr->mac;
+                    update.entry.bv_id = itr->bv_id;
                     update.add = false;
+                    itr++;
 
                     storeFdbEntryState(update);
 
@@ -247,6 +248,10 @@ void FdbOrch::update(sai_fdb_event_t        type,
                         observer->update(SUBJECT_TYPE_FDB_CHANGE, &update);
                     }
                 } 
+                else
+                {
+                    itr++;
+                }
             }
         }
         else if (bridge_port_id == SAI_NULL_OBJECT_ID)
