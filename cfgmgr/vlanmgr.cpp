d40b5380cb460d6d233a435caa2f7702fa64b5fb
anilkpan <47642449+anilkpan@users.noreply.github.com>
2020-10-08 19:22:32 -0700

    vlanmgr changes related to EVPN VxLan warmboot

--- cfgmgr/vlanmgr.cpp
+++ cfgmgr/vlanmgr.cpp
@@ -29,12 +29,32 @@ VlanMgr::VlanMgr(DBConnector *cfgDb, DBConnector *appDb, DBConnector *stateDb, c
         m_stateVlanTable(stateDb, STATE_VLAN_TABLE_NAME),
         m_stateVlanMemberTable(stateDb, STATE_VLAN_MEMBER_TABLE_NAME),
         m_appVlanTableProducer(appDb, APP_VLAN_TABLE_NAME),
-        m_appVlanMemberTableProducer(appDb, APP_VLAN_MEMBER_TABLE_NAME)
+        m_appVlanMemberTableProducer(appDb, APP_VLAN_MEMBER_TABLE_NAME),
+        replayDone(false)
 {
     SWSS_LOG_ENTER();
 
     if (WarmStart::isWarmStart())
     {
+        vector<string> vlanKeys, vlanMemberKeys;
+
+        /* cache all vlan and vlan member config */
+        m_cfgVlanTable.getKeys(vlanKeys);
+        m_cfgVlanMemberTable.getKeys(vlanMemberKeys);
+        for (auto k : vlanKeys)
+        {
+            m_vlanReplay.insert(k);
+        }
+        for (auto k : vlanMemberKeys)
+        {
+            m_vlanMemberReplay.insert(k);
+        }
+        if (m_vlanReplay.empty())
+        {
+            replayDone = true;
+            WarmStart::setWarmStartState("vlanmgrd", WarmStart::REPLAYED);
+            SWSS_LOG_NOTICE("vlanmgr warmstart state set to REPLAYED");
+        }
         const std::string cmds = std::string("")
           + IP_CMD + " link show " + DOT1Q_BRIDGE_NAME + " 2>/dev/null";
 
@@ -280,6 +300,7 @@ void VlanMgr::doVlanTask(Consumer &consumer)
             if (isVlanStateOk(key) && m_vlans.find(key) == m_vlans.end())
             {
                 m_vlans.insert(key);
+                m_vlanReplay.erase(kfvKey(t));
                 it = consumer.m_toSync.erase(it);
                 SWSS_LOG_DEBUG("%s already created", kfvKey(t).c_str());
                 continue;
@@ -290,6 +311,7 @@ void VlanMgr::doVlanTask(Consumer &consumer)
             {
                 addHostVlan(vlan_id);
             }
+            m_vlanReplay.erase(kfvKey(t));
 
             /* set up host env .... */
             for (auto i : kfvFieldsValues(t))
@@ -367,6 +389,14 @@ void VlanMgr::doVlanTask(Consumer &consumer)
             SWSS_LOG_DEBUG("%s", (dumpTuple(consumer, t)).c_str());
             it = consumer.m_toSync.erase(it);
         }
+    }
+    if (!replayDone && m_vlanReplay.empty() &&
+        m_vlanMemberReplay.empty() &&
+        WarmStart::isWarmStart())
+    {
+        replayDone = true;
+        WarmStart::setWarmStartState("vlanmgrd", WarmStart::REPLAYED);
+        SWSS_LOG_NOTICE("vlanmgr warmstart state set to REPLAYED");
     }
 }
 
@@ -510,6 +540,7 @@ void VlanMgr::doVlanMemberTask(Consumer &consumer)
              if (isVlanMemberStateOk(kfvKey(t)))
              {
                 SWSS_LOG_DEBUG("%s already set", kfvKey(t).c_str());
+                m_vlanMemberReplay.erase(kfvKey(t));
                 it = consumer.m_toSync.erase(it);
                 continue;
              }
@@ -551,6 +582,8 @@ void VlanMgr::doVlanMemberTask(Consumer &consumer)
                 FieldValueTuple s("state", "ok");
                 fvVector.push_back(s);
                 m_stateVlanMemberTable.set(kfvKey(t), fvVector);
+
+                m_vlanMemberReplay.erase(kfvKey(t));
             }
         }
         else if (op == DEL_COMMAND)
@@ -576,6 +609,13 @@ void VlanMgr::doVlanMemberTask(Consumer &consumer)
         }
         /* Other than the case of member port/lag is not ready, no retry will be performed */
         it = consumer.m_toSync.erase(it);
+    }
+    if (!replayDone && m_vlanMemberReplay.empty() &&
+        WarmStart::isWarmStart())
+    {
+        replayDone = true;
+        WarmStart::setWarmStartState("vlanmgrd", WarmStart::REPLAYED);
+        SWSS_LOG_NOTICE("vlanmgr warmstart state set to REPLAYED");
     }
 }
 
