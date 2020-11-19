12e5b1e76319e708897b3eb81f53d0977ede9325
Prabhu Sreenivasan <ps944501@lvnvde0930.lvn.broadcom.net>
2020-11-19 03:41:20 -0800

    [crm] Add support for snat, dnat and ipmc crm resources
    Signed-off-by: Prabhu Sreenivasan <prabhu.sreenivasan@broadcom.com>

--- tests/test_crm.py
+++ tests/test_crm.py
@@ -905,6 +905,51 @@ class TestCrm(object):
         threshold_type = getCrmConfigStr(dvs, 'Config', 'fdb_entry_threshold_type')
         assert threshold_type == 'percentage'
 
+    def test_Configure_snat(self, dvs, testlog):
+
+        #thresholds snat low/high threshold/type
+        dvs.runcmd("crm config thresholds snat low 50")
+        dvs.runcmd("crm config thresholds snat high 90")
+        dvs.runcmd("crm config thresholds snat type percentage")
+
+        time.sleep(2)
+        threshold_low = getCrmConfigValue(dvs, 'Config', 'snat_entry_low_threshold')
+        assert threshold_low == 50
+        threshold_high = getCrmConfigValue(dvs, 'Config', 'snat_entry_high_threshold')
+        assert threshold_high == 90
+        threshold_type = getCrmConfigStr(dvs, 'Config', 'snat_entry_threshold_type')
+        assert threshold_type == 'percentage'
+
+    def test_Configure_dnat(self, dvs, testlog):
+
+        #thresholds dnat low/high threshold/type
+        dvs.runcmd("crm config thresholds dnat low 50")
+        dvs.runcmd("crm config thresholds dnat high 90")
+        dvs.runcmd("crm config thresholds dnat type percentage")
+
+        time.sleep(2)
+        threshold_low = getCrmConfigValue(dvs, 'Config', 'dnat_entry_low_threshold')
+        assert threshold_low == 50
+        threshold_high = getCrmConfigValue(dvs, 'Config', 'dnat_entry_high_threshold')
+        assert threshold_high == 90
+        threshold_type = getCrmConfigStr(dvs, 'Config', 'dnat_entry_threshold_type')
+        assert threshold_type == 'percentage'
+
+    def test_Configure_ipmc(self, dvs, testlog):
+
+        #thresholds ipmc low/high threshold/type
+        dvs.runcmd("crm config thresholds ipmc low 50")
+        dvs.runcmd("crm config thresholds ipmc high 90")
+        dvs.runcmd("crm config thresholds ipmc type percentage")
+
+        time.sleep(2)
+        threshold_low = getCrmConfigValue(dvs, 'Config', 'snat_entry_low_threshold')
+        assert threshold_low == 50
+        threshold_high = getCrmConfigValue(dvs, 'Config', 'snat_entry_high_threshold')
+        assert threshold_high == 90
+        threshold_type = getCrmConfigStr(dvs, 'Config', 'snat_entry_threshold_type')
+        assert threshold_type == 'percentage'
+
 
 # Add Dummy always-pass test at end as workaroud
 # for issue when Flaky fail on final test it invokes module tear-down before retrying
