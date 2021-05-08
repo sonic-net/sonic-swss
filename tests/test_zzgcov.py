5ddfc5694c7db917b184ab16551f00f4b1c42db8
pettershao-ragilenetworks <pettershao@ragilenetworks.com>
2021-05-08 15:50:58 +0800

    [gcov] swss support gcov
    Signed-off-by: pettershao-ragilenetworks <pettershao@ragilenetworks.com>

---
+++ tests/test_zzgcov.py
@@ -0,0 +1,16 @@
+import time
+import os
+import pytest
+
+from swsscommon import swsscommon
+
+
+
+def test_gcda_collection(dvs):
+    dvs.runcmd("./gcov_support.sh collect_gcda")
+
+def test_gcda_generate_all(dvs):
+    dvs.runcmd("./gcov_support.sh generate all")
+
+def test_nonflaky_dummy():
+    pass
