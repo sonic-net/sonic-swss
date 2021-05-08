f9c08ea865dcb4360a9c259bf57da62c5cf34d87
pettershao-ragilenetworks <pettershao@ragilenetworks.com>
2021-05-08 09:37:26 +0800

    [gcov] swss support gcov
    Signed-off-by: pettershao-ragilenetworks <pettershao@ragilenetworks.com>

--- tests/test_buffer_mode.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-class TestBufferModel(object):
-    def test_bufferModel(self, dvs, testlog):
-        config_db = dvs.get_config_db()
-        metadata = config_db.get_entry("DEVICE_METADATA", "localhost")
-        assert metadata["buffer_model"] == "traditional"
