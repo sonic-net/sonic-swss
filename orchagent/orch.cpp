e8557980d639ba0d5b9bc5038374a1aac76f43d4
Shi Su <ssu@microsoft.com>
2021-07-02 19:11:33 +0000

    Avoid fall through

--- orchagent/orch.cpp
+++ orchagent/orch.cpp
@@ -704,6 +704,9 @@ task_process_status Orch::handleSaiCreateStatus(sai_api_t api, sai_status_t stat
         case SAI_API_FDB:
             switch (status)
             {
+                case SAI_STATUS_SUCCESS:
+                    SWSS_LOG_WARN("SAI_STATUS_SUCCESS is not expected in handleSaiCreateStatus");
+                    return task_success;
                 case SAI_STATUS_ITEM_ALREADY_EXISTS:
                     /*
                     *  In FDB creation, there are scenarios where the hardware learns an FDB entry before orchagent.
@@ -711,7 +714,12 @@ task_process_status Orch::handleSaiCreateStatus(sai_api_t api, sai_status_t stat
                     *  and orchagent should ignore the error and treat it as entry was explicitly created.
                     */
                     return task_success;
+                default:
+                    SWSS_LOG_ERROR("Encountered failure in create operation, exiting orchagent, SAI API: %s, status: %s",
+                                sai_serialize_api(api).c_str(), sai_serialize_status(status).c_str());
+                    exit(EXIT_FAILURE);
             }
+            break;
         default:
             switch (status)
             {
