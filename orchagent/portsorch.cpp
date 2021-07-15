b89a5758dd5c56d003e85ea541d72fa63253103b
Alexander Allen <arallen@nvidia.com>
2021-07-15 01:09:49 +0000

    Add inc and dec for lag membership for port refs

--- orchagent/portsorch.cpp
+++ orchagent/portsorch.cpp
@@ -4806,6 +4806,8 @@ bool PortsOrch::addLagMember(Port &lag, Port &port, bool enableForwarding)
         }
     }
 
+    increasePortRefCount(port.m_alias);
+
     LagMemberUpdate update = { lag, port, true };
     notify(SUBJECT_TYPE_LAG_MEMBER_CHANGE, static_cast<void *>(&update));
 
@@ -4851,6 +4853,9 @@ bool PortsOrch::removeLagMember(Port &lag, Port &port)
             return false;
         }
     }
+
+    decreasePortRefCount(port.m_alias);
+
     LagMemberUpdate update = { lag, port, false };
     notify(SUBJECT_TYPE_LAG_MEMBER_CHANGE, static_cast<void *>(&update));
 
