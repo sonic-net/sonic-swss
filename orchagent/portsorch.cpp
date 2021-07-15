c2530bebe77f7ffff5ab9dc589b93073fbd6eec0
Alexander Allen <arallen@nvidia.com>
2021-07-15 00:21:24 +0000

    Fix pointer bug

--- orchagent/portsorch.cpp
+++ orchagent/portsorch.cpp
@@ -3221,11 +3221,11 @@ void PortsOrch::doPortTask(Consumer &consumer)
                 {
                     throw runtime_error("Remove hostif for the port failed");
                 }
+		m_portList[alias].m_hif_id = SAI_NULL_OBJECT_ID;
 
                 Port p;
                 if (getPort(port_id, p))
                 {
-		    p.m_hif_id = null;
                     PortUpdate update = {p, false};
                     notify(SUBJECT_TYPE_PORT_CHANGE, static_cast<void *>(&update));
                 }
@@ -4844,7 +4844,7 @@ bool PortsOrch::removeLagMember(Port &lag, Port &port)
 
     if (lag.m_bridge_port_id > 0)
     {
-        if (port.m_hif_id && !setHostIntfsStripTag(port, SAI_HOSTIF_VLAN_TAG_STRIP))
+        if (port.m_hif_id != SAI_NULL_OBJECT_ID && !setHostIntfsStripTag(port, SAI_HOSTIF_VLAN_TAG_STRIP))
         {
             SWSS_LOG_ERROR("Failed to set %s for hostif of port %s which is leaving LAG %s",
                     hostif_vlan_tag[SAI_HOSTIF_VLAN_TAG_STRIP], port.m_alias.c_str(), lag.m_alias.c_str());
