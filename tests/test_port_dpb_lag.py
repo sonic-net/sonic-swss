79c2e0d5f3b8ba67966b0337aa2ea5a68f93a3c3
Alexander Allen <arallen@nvidia.com>
2021-07-26 19:42:05 +0000

    Fix delays

--- tests/test_port_dpb_lag.py
+++ tests/test_port_dpb_lag.py
@@ -15,16 +15,18 @@ class TestPortDPBLag(object):
         p = Port(dvs, "Ethernet0")
         p.sync_from_config_db()
 
-        # 1. Create PortChannel0001
+        # 1. Create PortChannel0001.
         self.dvs_lag.create_port_channel(lag)
+        time.sleep(2)
 
-        # 2. Add Ethernet0 to PortChannel0001
+        # 2. Add Ethernet0 to PortChannel0001.
         self.dvs_lag.create_port_channel_member(lag, p.get_name())
+        time.sleep(2)
 
         # 3. Add log marker to syslog
         marker = dvs.add_log_marker()
 
-        # 4. Delete Ethernet0 from config DB. Sleep for 5 seconds.
+        # 4. Delete Ethernet0 from config DB.
         p.delete_from_config_db()
         time.sleep(2)
 
@@ -39,6 +41,7 @@ class TestPortDPBLag(object):
 
         # 7. Remove port from LAG
         self.dvs_lag.remove_port_channel_member(lag, p.get_name())
+        time.sleep(2)
 
         # 8. Verify that port is removed from ASIC DB
         assert(p.not_exists_in_asic_db() == True)
@@ -46,12 +49,14 @@ class TestPortDPBLag(object):
         # 9. Re-create port Ethernet0 and verify that it is
         #    present in CONFIG, APPL, and ASIC DBs
         p.write_to_config_db()
+        time.sleep(2)
         p.verify_config_db()
         p.verify_app_db()
         p.verify_asic_db()
 
         # 10. Remove PortChannel0001 and verify that its removed.
         self.dvs_lag.remove_port_channel(lag)
+        time.sleep(2)
         self.dvs_lag.get_and_verify_port_channel(0)
 
 
