48c6e8743cda1b1973ab65e481c243c1b84bb19e
Alexander Allen <arallen@nvidia.com>
2021-07-26 21:03:15 -0400

    Update test_port_dpb_lag.py

--- tests/test_port_dpb_lag.py
+++ tests/test_port_dpb_lag.py
@@ -17,23 +17,21 @@ class TestPortDPBLag(object):
 
         # 1. Create PortChannel0001.
         self.dvs_lag.create_port_channel(lag)
-        time.sleep(2)
+        time.sleep(5)
 
         # 2. Add Ethernet0 to PortChannel0001.
         self.dvs_lag.create_port_channel_member(lag, p.get_name())
-        time.sleep(2)
+        time.sleep(5)
 
         # 3. Add log marker to syslog
         marker = dvs.add_log_marker()
 
         # 4. Delete Ethernet0 from config DB.
         p.delete_from_config_db()
-        time.sleep(2)
+        time.sleep(5)
 
         # 5. Verify that we are waiting in portsorch for the port
         #    to be removed from LAG, by looking at the log
-        (exitcode, output) = dvs.runcmd(['cat', '/var/log/syslog'])
-        print(output)
         self.check_syslog(dvs, marker, "Unable to remove port Ethernet0: ref count 1", 1)
 
         # 6. Also verify that port is still present in ASIC DB.
@@ -41,7 +39,7 @@ class TestPortDPBLag(object):
 
         # 7. Remove port from LAG
         self.dvs_lag.remove_port_channel_member(lag, p.get_name())
-        time.sleep(2)
+        time.sleep(5)
 
         # 8. Verify that port is removed from ASIC DB
         assert(p.not_exists_in_asic_db() == True)
@@ -49,14 +47,14 @@ class TestPortDPBLag(object):
         # 9. Re-create port Ethernet0 and verify that it is
         #    present in CONFIG, APPL, and ASIC DBs
         p.write_to_config_db()
-        time.sleep(2)
+        time.sleep(5)
         p.verify_config_db()
         p.verify_app_db()
         p.verify_asic_db()
 
         # 10. Remove PortChannel0001 and verify that its removed.
         self.dvs_lag.remove_port_channel(lag)
-        time.sleep(2)
+        time.sleep(5)
         self.dvs_lag.get_and_verify_port_channel(0)
 
 
