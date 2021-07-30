1ea446914f4143c27ba33aebd2ecdf669738382a
Alexander Allen <arallen@nvidia.com>
2021-07-29 22:01:08 -0400

    Update test_port_dpb_lag.py

--- tests/test_port_dpb_lag.py
+++ tests/test_port_dpb_lag.py
@@ -54,7 +54,7 @@ class TestPortDPBLag(object):
 
         # 10. Remove PortChannel0001 and verify that its removed.
         self.dvs_lag.remove_port_channel(lag)        
-        time.sleep(60)
+        time.sleep(30)
         self.dvs_lag.get_and_verify_port_channel(0)
 
 
