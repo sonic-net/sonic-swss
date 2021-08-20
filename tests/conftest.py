ba6b5967654c43f42fce59c96cd1444d796f8268
Lawrence Lee <lawlee@microsoft.com>
2021-08-20 03:18:28 +0000

    [pytest]: Increase DVS wait timeout
    Signed-off-by: Lawrence Lee <lawlee@microsoft.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1623,7 +1623,7 @@ def manage_dvs(request) -> str:
             # If not re-creating the DVS, reload supervisor
             # between modules to ensure a consistent start state
             dvs.runcmd("supervisorctl reload")
-            dvs.check_services_ready(timeout=60)
+            dvs.check_services_ready(timeout=120)
 
         return dvs
 
