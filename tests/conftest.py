033d6995945cb751b36589adc8e8b3c7b6aff5b4
Lawrence Lee <lawlee@microsoft.com>
2021-09-15 18:50:12 +0000

    [pytest]: Removed unused logger
    Signed-off-by: Lawrence Lee <lawlee@microsoft.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -28,8 +28,6 @@ from dvslib import dvs_policer
 
 from buffer_model import enable_dynamic_buffer
 
-logger = logging.getLogger(__name__)
-
 # FIXME: For the sake of stabilizing the PR pipeline we currently assume there are 32 front-panel
 # ports in the system (much like the rest of the test suite). This should be adjusted to accomodate
 # a dynamic number of ports. GitHub Issue: Azure/sonic-swss#1384.
