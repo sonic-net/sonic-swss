08e8cea7a668ee2b6328fbb7d2b1cec5e07a2d1f
Sambath Kumar Balasubramanian <sambath@arista.com>
2021-09-16 17:27:05 -0700

    Add support for voq counters in portsorch.
    1) Add m_voq_ids to SystemPortInfo to maintain the list of queue ids.
    2) Add two new tables COUNTERS_SYSTEM_PORT_NAME_MAP and
       COUNTERS_VOQ_NAME_MAP to enable queuestat to differentiate between
       Port Tx queues and Voqs.
    3) Add a new function initializeVoqs that retrieves the number of voqs
       for a system port and stores the voq object ids in m_voq_ids
    4) Add code to handle queue type SAI_QUEUE_TYPE_UNICAST_VOQ.
    5) Initialize voqs and populate COUNTERS_SYSTEM_PORT_NAME_MAP in
       addSystemPorts function.
    6) Update generateQueueMap to generate queue maps for both Txqs and Voq.
    For PHY ports in a voq system both Txqs and Voqs are instantiated. For
    Voqs of remote system port, only Voq counters are initialized.

--- orchagent/portsorch.h
+++ orchagent/portsorch.h
@@ -154,10 +154,12 @@ public:
 
 private:
     unique_ptr<Table> m_counterTable;
+    unique_ptr<Table> m_counterSysPortTable;
     unique_ptr<Table> m_counterLagTable;
     unique_ptr<Table> m_portTable;
     unique_ptr<Table> m_gearboxTable;
     unique_ptr<Table> m_queueTable;
+    unique_ptr<Table> m_voqTable;
     unique_ptr<Table> m_queuePortTable;
     unique_ptr<Table> m_queueIndexTable;
     unique_ptr<Table> m_queueTypeTable;
@@ -241,6 +243,7 @@ private:
     void initializePriorityGroups(Port &port);
     void initializePortMaximumHeadroom(Port &port);
     void initializeQueues(Port &port);
+    void initializeVoqs(Port &port);
 
     bool addHostIntfs(Port &port, string alias, sai_object_id_t &host_intfs_id);
     bool setHostIntfsStripTag(Port &port, sai_hostif_vlan_tag_t strip);
@@ -288,7 +291,7 @@ private:
     bool getQueueTypeAndIndex(sai_object_id_t queue_id, string &type, uint8_t &index);
 
     bool m_isQueueMapGenerated = false;
-    void generateQueueMapPerPort(const Port& port);
+    void generateQueueMapPerPort(const Port& port, bool voq);
 
     bool m_isPriorityGroupMapGenerated = false;
     void generatePriorityGroupMapPerPort(const Port& port);
