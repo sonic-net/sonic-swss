c64b8cfdf5f4aa3893f089e04685bf22520f8a50
Tom cappleman <thomas.cappleman@metaswitch.com>
2021-10-07 09:12:49 +0100

    Fix removed code

--- orchagent/routeorch.cpp
+++ orchagent/routeorch.cpp
@@ -2230,6 +2230,9 @@ bool RouteOrch::removeRoutePost(const RouteBulkContext& ctx)
                 m_neighOrch->removeMplsNextHop(nexthop);
             }
         }
+
+        RouteKey r_key = { vrf_id, ipPrefix };
+        removeNextHopRoute(nexthop, r_key);
     }
 
     SWSS_LOG_INFO("Remove route %s with next hop(s) %s",
