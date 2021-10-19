60fba0202bb442fcefa231ad3ed8252ea7a90127
Sudharsan Dhamal Gopalarathnam <sudharsand@nvidia.com>
2021-10-19 04:42:40 +0000

    Addressing code review comments

--- tests/test_buffer_mode.py
+++ tests/test_buffer_mode.py
@@ -12,9 +12,16 @@ class TestBufferModel(object):
         app_db = dvs.get_app_db()
         keys = app_db.get_keys("BUFFER_POOL_TABLE")
         num_keys =  len(keys)
-        fvs = {'buffer_model' : 'dynamic'}
-        config_db.update_entry("DEVICE_METADATA", "localhost", fvs)
-        fvs = {'mode':'dynamic', 'type':'egress'}
-        config_db.update_entry("BUFFER_POOL", "temp_pool", fvs)
-        time.sleep(2)
-        app_db.wait_for_n_keys("BUFFER_POOL_TABLE", num_keys)
+
+        try:
+            fvs = {'buffer_model' : 'dynamic'}
+            config_db.update_entry("DEVICE_METADATA", "localhost", fvs)
+            fvs = {'mode':'dynamic', 'type':'egress'}
+            config_db.update_entry("BUFFER_POOL", "temp_pool", fvs)
+            time.sleep(2)
+            app_db.wait_for_n_keys("BUFFER_POOL_TABLE", num_keys)
+
+        finally:
+            config_db.delete_entry("BUFFER_POOL", "temp_pool")
+            fvs = {'buffer_model' : 'traditional'}
+            config_db.update_entry("DEVICE_METADATA", "localhost", fvs)
