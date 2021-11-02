5e26e7e24157ea5d3b3bdaa8fde16a23defdb9fe
vedganes <vedavinayagam.ganesan@nokia.com>
2021-11-02 15:37:19 -0400

    [chassis][syncd][bcmsai] Adjusting response timeout during syncd init
    In VOQ based chassis where syncd uses BCM VOQ SAI, if there are large
    number of front panel ports, SAI takes more than 1 minutes to complete
    the switch create initialization. Because of this, the switch create
    request sent by orchagent is not getting response within the default
    response wait time of 1 minute. So the orchagent declares switch create
    failure and crashes.
    
    The number of ports need to be initialized by SAI depends on number of
    ports per asic and total number of system ports configured in the
    system. The total number of system ports in the system in turn depends
    on number of line cards supported, number of asics per line card and
    number of ports supported by each asic. Therefore in a fully populated
    system, which is an often expected scenario, this crashing will happen.
    
    To fix this, in orchagent, the syncd response time out is set to 35
    minutes before sending request for switch create and is set back to
    default wait time after the switch create. For now, since the problem
    is observed for BCM platforms, this adjustment is done only for BCM
    platform.
    
    Signed-off-by: vedganes <vedavinayagam.ganesan@nokia.com>

--- orchagent/main.cpp
+++ orchagent/main.cpp
@@ -81,6 +81,7 @@ int32_t gVoqMaxCores = 0;
 uint32_t gCfgSystemPorts = 0;
 string gMyHostName = "";
 string gMyAsicName = "";
+uint64_t gSyncdResponseTimeout = ( 35 * SAI_REDIS_DEFAULT_SYNC_OPERATION_RESPONSE_TIMEOUT);
 
 void usage()
 {
@@ -574,6 +575,29 @@ int main(int argc, char **argv)
     attr.value.u64 = gSwitchId;
     attrs.push_back(attr);
 
+
+    char *platform = getenv("platform");
+    if (platform && strstr(platform, BRCM_PLATFORM_SUBSTRING))
+    {
+        /* We set this long timeout in order for orchagent to wait enough time for
+         * response from syncd. It is needed since switch create takes more time
+         * than default time to create switch if there are lots of front panel ports
+         * and systems ports to initialize
+         */
+        attr.id = SAI_REDIS_SWITCH_ATTR_SYNC_OPERATION_RESPONSE_TIMEOUT;
+        attr.value.u64 = gSyncdResponseTimeout;
+        status = sai_switch_api->set_switch_attribute(gSwitchId, &attr);
+
+        if (status != SAI_STATUS_SUCCESS)
+        {
+            SWSS_LOG_WARN("Failed to set SAI REDIS response timeout");
+        }
+        else
+        {
+            SWSS_LOG_NOTICE("SAI REDIS response timeout set successfully to %" PRIu64 " ", attr.value.u64);
+        }
+    }
+
     status = sai_switch_api->create_switch(&gSwitchId, (uint32_t)attrs.size(), attrs.data());
     if (status != SAI_STATUS_SUCCESS)
     {
@@ -582,6 +606,22 @@ int main(int argc, char **argv)
     }
     SWSS_LOG_NOTICE("Create a switch, id:%" PRIu64, gSwitchId);
 
+    if (platform && strstr(platform, BRCM_PLATFORM_SUBSTRING))
+    {
+        /* Set syncd response timeout back to the default value */
+        attr.id = SAI_REDIS_SWITCH_ATTR_SYNC_OPERATION_RESPONSE_TIMEOUT;
+        attr.value.u64 = SAI_REDIS_DEFAULT_SYNC_OPERATION_RESPONSE_TIMEOUT;
+        status = sai_switch_api->set_switch_attribute(gSwitchId, &attr);
+
+        if (status != SAI_STATUS_SUCCESS)
+        {
+            SWSS_LOG_WARN("Failed to set SAI REDIS response timeout to default");
+        }
+        else
+        {
+            SWSS_LOG_NOTICE("SAI REDIS response timeout set successfully to default: %" PRIu64 " ", attr.value.u64);
+        }
+    }
 
     if (gMySwitchType != "fabric")
     {
