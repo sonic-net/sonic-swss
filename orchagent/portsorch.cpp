7087ed45750df0b64d321d05798f9ee727fc8928
dprital <drorp@nvidia.com>
2022-02-07 14:00:29 +0000

    Fix PR review comment

--- orchagent/portsorch.cpp
+++ orchagent/portsorch.cpp
@@ -5448,10 +5448,10 @@ void PortsOrch::removeQueueMapPerPort(const Port& port)
         }
     }
 
-    m_counter_db->hdel(COUNTERS_QUEUE_NAME_MAP, queueVector);
-    m_counter_db->hdel(COUNTERS_QUEUE_PORT_MAP, queuePortVector);
-    m_counter_db->hdel(COUNTERS_QUEUE_INDEX_MAP, queueIndexVector);
-    m_counter_db->hdel(COUNTERS_QUEUE_TYPE_MAP, queueTypeVector);
+    m_queueTable->hdel("", queueVector);
+    m_queuePortTable->hdel("", queuePortVector);
+    m_queueIndexTable->hdel("", queueIndexVector);
+    m_queueTypeTable->hdel("", queueTypeVector);
 
     for (size_t queueIndex = 0; queueIndex < port.m_queue_ids.size(); ++queueIndex)
     {
@@ -5570,9 +5570,9 @@ void PortsOrch::removePriorityGroupMapPerPort(const Port& port)
         pgIndexVector.emplace_back(id);
     }
 
-    m_counter_db->hdel(COUNTERS_PG_NAME_MAP, pgVector);
-    m_counter_db->hdel(COUNTERS_PG_PORT_MAP, pgPortVector);
-    m_counter_db->hdel(COUNTERS_PG_INDEX_MAP, pgIndexVector);
+    m_pgTable->hdel("", pgVector);
+    m_pgPortTable->hdel("", pgPortVector);
+    m_pgIndexTable->hdel("", pgIndexVector);
 
     for (size_t pgIndex = 0; pgIndex < port.m_priority_group_ids.size(); ++pgIndex)
     {
