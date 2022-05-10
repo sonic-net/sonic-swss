6ad42ce4338c9cca15b99aabb8bb1ccf4fd993e0
Saikrishna Arcot <sarcot@microsoft.com>
2022-05-09 17:08:33 -0700

    Move opening brace to newline
    Signed-off-by: Saikrishna Arcot <sarcot@microsoft.com>

--- orchagent/orchdaemon.cpp
+++ orchagent/orchdaemon.cpp
@@ -621,7 +621,8 @@ bool OrchDaemon::init()
 
     m_orchList.push_back(&CounterCheckOrch::getInstance(m_configDb));
 
-    if (platform == P4_PLATFORM_SUBSTRING) {
+    if (platform == P4_PLATFORM_SUBSTRING)
+    {
         vector<string> p4rt_tables = {APP_P4RT_TABLE_NAME};
         gP4Orch = new P4Orch(m_applDb, p4rt_tables, vrf_orch, gCoppOrch);
         m_orchList.push_back(gP4Orch);
