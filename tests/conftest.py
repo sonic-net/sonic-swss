ab63507b2155f6ed3a400c8a7762a6510f6136ed
liora <liora@nvidia.com>
2022-07-24 11:40:57 +0000

    Remove killall5 command
    Signed-off-by: liora <liora@nvidia.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1677,7 +1677,7 @@ def manage_dvs(request) -> str:
 
         else:
             # First generate GCDA files for GCov
-            dvs.runcmd('killall5 -15')
+            # dvs.runcmd('killall5 -15')
             # If not re-creating the DVS, restart container
             # between modules to ensure a consistent start state
             dvs.net_cleanup()
