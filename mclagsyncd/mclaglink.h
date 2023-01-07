94efd7b7893a54c4678171bab7289e1ebde5bdd0
skannan <skannan@celestica.com>
2023-01-07 15:40:12 +0000

    Add Isolation Support for Innovium based platforms https://github.com/sonic-net/sonic-swss/issues/2602

--- mclagsyncd/mclaglink.h
+++ mclagsyncd/mclaglink.h
@@ -54,6 +54,7 @@
 #define BRCM_PLATFORM_SUBSTRING "broadcom"
 #define BFN_PLATFORM_SUBSTRING  "barefoot"
 #define CTC_PLATFORM_SUBSTRING  "centec"
+#define INVM_PLATFORM_SUBSTRING "innovium"
 
 using namespace std;
 
