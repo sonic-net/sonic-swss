eb20b1c9a690a8bff7d3fae329ffa730b652e71e
prasanna-cls <prasannal@celestica.com>
2023-01-18 00:26:55 -0600

    support and handle default vlan creation and deletion from the operator, such that orchagent does not invoke SAI APIs to avoid errors and crash.

--- orchagent/portsorch.h
+++ orchagent/portsorch.h
@@ -233,6 +233,7 @@ private:
     // TODO: Add Bridge/Vlan class
     sai_object_id_t m_default1QBridge;
     sai_object_id_t m_defaultVlan;
+    unsigned int m_defaultVlanId;
 
     typedef enum
     {
