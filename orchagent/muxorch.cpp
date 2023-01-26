d695db2a6724bf0683d145144101bc9f1b2c7402
Devesh P Pathak <devpatha@cisco.com>
2023-01-25 17:49:02 -0800

    Remove counters for Mux ACL

--- orchagent/muxorch.cpp
+++ orchagent/muxorch.cpp
@@ -802,7 +802,7 @@ MuxAclHandler::MuxAclHandler(sai_object_id_t port, string alias)
         // First time handling of Mux Table, create ACL table, and bind
         createMuxAclTable(port, table_name);
         shared_ptr<AclRulePacket> newRule =
-                make_shared<AclRulePacket>(gAclOrch, rule_name, table_name);
+                make_shared<AclRulePacket>(gAclOrch, rule_name, table_name, false /*no counters*/);
         createMuxAclRule(newRule, table_name);
     }
     else
@@ -813,7 +813,7 @@ MuxAclHandler::MuxAclHandler(sai_object_id_t port, string alias)
         if (rule == nullptr)
         {
             shared_ptr<AclRulePacket> newRule =
-                    make_shared<AclRulePacket>(gAclOrch, rule_name, table_name);
+                    make_shared<AclRulePacket>(gAclOrch, rule_name, table_name, false /*no counters*/);
             createMuxAclRule(newRule, table_name);
         }
         else
