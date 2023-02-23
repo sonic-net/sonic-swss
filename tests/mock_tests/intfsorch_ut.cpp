55f8228d066df77cb587a30142c90cb96f61e14f
junchao <junchao@nvidia.com>
2023-02-23 14:12:33 +0800

    Fix issue: unit test failure

--- tests/mock_tests/intfsorch_ut.cpp
+++ tests/mock_tests/intfsorch_ut.cpp
@@ -49,13 +49,23 @@ namespace intfsorch_test
         //sai_remove_router_interface_fn old_remove_rif;
         void SetUp() override
         {
-            ASSERT_EQ(sai_router_intfs_api, nullptr);
             map<string, string> profile = {
                 { "SAI_VS_SWITCH_TYPE", "SAI_VS_SWITCH_TYPE_BCM56850" },
                 { "KV_DEVICE_MAC_ADDRESS", "20:03:04:05:06:00" }
             };
 
             ut_helper::initSaiApi(profile);
+            pold_sai_rif_api = sai_router_intfs_api;
+            ut_sai_rif_api = *sai_router_intfs_api;
+            sai_router_intfs_api = &ut_sai_rif_api;
+
+            sai_router_intfs_api->create_router_interface = _ut_create_router_interface;
+            sai_router_intfs_api->remove_router_interface = _ut_remove_router_interface;
+
+            m_app_db = make_shared<swss::DBConnector>("APPL_DB", 0);
+            m_config_db = make_shared<swss::DBConnector>("CONFIG_DB", 0);
+            m_state_db = make_shared<swss::DBConnector>("STATE_DB", 0);
+            m_chassis_app_db = make_shared<swss::DBConnector>("CHASSIS_APP_DB", 0);
 
             sai_attribute_t attr;
 
@@ -73,42 +83,50 @@ namespace intfsorch_test
 
             gMacAddress = attr.value.mac;
 
-            //old_create_rif = sai_router_intfs_api->create_router_interface;
-            //old_remove_rif = sai_router_intfs_api->remove_router_interface;
-            //old_sai_rif_api_ptr = sai_router_intfs_api;
-            pold_sai_rif_api = sai_router_intfs_api;
-            ut_sai_rif_api = *sai_router_intfs_api;
-            sai_router_intfs_api = &ut_sai_rif_api;
+            attr.id = SAI_SWITCH_ATTR_DEFAULT_VIRTUAL_ROUTER_ID;
+            status = sai_switch_api->get_switch_attribute(gSwitchId, 1, &attr);
 
-            sai_router_intfs_api->create_router_interface = _ut_create_router_interface;
-            sai_router_intfs_api->remove_router_interface = _ut_remove_router_interface;
+            ASSERT_EQ(status, SAI_STATUS_SUCCESS);
 
-            m_app_db = make_shared<swss::DBConnector>("APPL_DB", 0);
-            m_config_db = make_shared<swss::DBConnector>("CONFIG_DB", 0);
-            m_state_db = make_shared<swss::DBConnector>("STATE_DB", 0);
-            m_chassis_app_db = make_shared<swss::DBConnector>("CHASSIS_APP_DB", 0);
+            gVirtualRouterId = attr.value.oid;
 
+
+            ASSERT_EQ(gCrmOrch, nullptr);
             gCrmOrch = new CrmOrch(m_config_db.get(), CFG_CRM_TABLE_NAME);
 
-            const int portsorch_base_pri = 40;
-            vector<table_name_with_pri_t> ports_tables = {
-                { APP_PORT_TABLE_NAME,        portsorch_base_pri + 5 },
-                { APP_VLAN_TABLE_NAME,        portsorch_base_pri + 2 },
-                { APP_VLAN_MEMBER_TABLE_NAME, portsorch_base_pri     },
-                { APP_LAG_TABLE_NAME,         portsorch_base_pri + 4 },
-                { APP_LAG_MEMBER_TABLE_NAME,  portsorch_base_pri     }
+            TableConnector stateDbSwitchTable(m_state_db.get(), "SWITCH_CAPABILITY");
+            TableConnector conf_asic_sensors(m_config_db.get(), CFG_ASIC_SENSORS_TABLE_NAME);
+            TableConnector app_switch_table(m_app_db.get(),  APP_SWITCH_TABLE_NAME);
+
+            vector<TableConnector> switch_tables = {
+                conf_asic_sensors,
+                app_switch_table
             };
 
+            ASSERT_EQ(gSwitchOrch, nullptr);
+            gSwitchOrch = new SwitchOrch(m_app_db.get(), switch_tables, stateDbSwitchTable);
+
+            // Create dependencies ...
             TableConnector stateDbBfdSessionTable(m_state_db.get(), STATE_BFD_SESSION_TABLE_NAME);
             gBfdOrch = new BfdOrch(m_app_db.get(), APP_BFD_SESSION_TABLE_NAME, stateDbBfdSessionTable);
 
-            gPortsOrch = new PortsOrch(m_app_db.get(), m_state_db.get(), ports_tables, m_chassis_app_db.get());
-
-            static const vector<string> route_pattern_tables = {
-                CFG_FLOW_COUNTER_ROUTE_PATTERN_TABLE_NAME,
+            const int portsorch_base_pri = 40;
+            vector<table_name_with_pri_t> ports_tables = {
+                { APP_PORT_TABLE_NAME, portsorch_base_pri + 5 },
+                { APP_VLAN_TABLE_NAME, portsorch_base_pri + 2 },
+                { APP_VLAN_MEMBER_TABLE_NAME, portsorch_base_pri },
+                { APP_LAG_TABLE_NAME, portsorch_base_pri + 4 },
+                { APP_LAG_MEMBER_TABLE_NAME, portsorch_base_pri }
             };
-            gFlowCounterRouteOrch = new FlowCounterRouteOrch(m_config_db.get(), route_pattern_tables);
-            gDirectory.set(gFlowCounterRouteOrch);
+
+            vector<string> flex_counter_tables = {
+                CFG_FLEX_COUNTER_TABLE_NAME
+            };
+            auto* flexCounterOrch = new FlexCounterOrch(m_config_db.get(), flex_counter_tables);
+            gDirectory.set(flexCounterOrch);
+
+            ASSERT_EQ(gPortsOrch, nullptr);
+            gPortsOrch = new PortsOrch(m_app_db.get(), m_state_db.get(), ports_tables, m_chassis_app_db.get());
 
             vector<string> vnet_tables = {
                 APP_VNET_RT_TABLE_NAME,
@@ -116,21 +134,23 @@ namespace intfsorch_test
             };
 
             vector<string> cfg_vnet_tables = {
-                    CFG_VNET_RT_TABLE_NAME,
-                    CFG_VNET_RT_TUNNEL_TABLE_NAME
+                CFG_VNET_RT_TABLE_NAME,
+                CFG_VNET_RT_TUNNEL_TABLE_NAME
             };
-            VNetOrch *vnet_orch;
-            vnet_orch = new VNetOrch(m_app_db.get(), APP_VNET_TABLE_NAME);
-            gDirectory.set(vnet_orch);
-            VNetCfgRouteOrch *cfg_vnet_rt_orch = new VNetCfgRouteOrch(m_config_db.get(), m_app_db.get(), cfg_vnet_tables);
-            gDirectory.set(cfg_vnet_rt_orch);
-            VNetRouteOrch *vnet_rt_orch = new VNetRouteOrch(m_app_db.get(), vnet_tables, vnet_orch);
-            gDirectory.set(vnet_rt_orch);
-            VRFOrch *vrf_orch = new VRFOrch(m_app_db.get(), APP_VRF_TABLE_NAME, m_state_db.get(), STATE_VRF_OBJECT_TABLE_NAME);
-            gDirectory.set(vrf_orch);
 
-            gIntfsOrch = new IntfsOrch(m_app_db.get(), APP_INTF_TABLE_NAME, vrf_orch, m_chassis_app_db.get());
-            
+            auto* vnet_orch = new VNetOrch(m_app_db.get(), APP_VNET_TABLE_NAME);
+            gDirectory.set(vnet_orch);
+            auto* cfg_vnet_rt_orch = new VNetCfgRouteOrch(m_config_db.get(), m_app_db.get(), cfg_vnet_tables);
+            gDirectory.set(cfg_vnet_rt_orch);
+            auto* vnet_rt_orch = new VNetRouteOrch(m_app_db.get(), vnet_tables, vnet_orch);
+            gDirectory.set(vnet_rt_orch);
+            ASSERT_EQ(gVrfOrch, nullptr);
+            gVrfOrch = new VRFOrch(m_app_db.get(), APP_VRF_TABLE_NAME, m_state_db.get(), STATE_VRF_OBJECT_TABLE_NAME);
+            gDirectory.set(gVrfOrch);
+
+            ASSERT_EQ(gIntfsOrch, nullptr);
+            gIntfsOrch = new IntfsOrch(m_app_db.get(), APP_INTF_TABLE_NAME, gVrfOrch, m_chassis_app_db.get());
+
             const int fdborch_pri = 20;
 
             vector<table_name_with_pri_t> app_fdb_tables = {
@@ -141,28 +161,61 @@ namespace intfsorch_test
 
             TableConnector stateDbFdb(m_state_db.get(), STATE_FDB_TABLE_NAME);
             TableConnector stateMclagDbFdb(m_state_db.get(), STATE_MCLAG_REMOTE_FDB_TABLE_NAME);
+            ASSERT_EQ(gFdbOrch, nullptr);
             gFdbOrch = new FdbOrch(m_app_db.get(), app_fdb_tables, stateDbFdb, stateMclagDbFdb, gPortsOrch);
 
+            ASSERT_EQ(gNeighOrch, nullptr);
             gNeighOrch = new NeighOrch(m_app_db.get(), APP_NEIGH_TABLE_NAME, gIntfsOrch, gFdbOrch, gPortsOrch, m_chassis_app_db.get());
 
-            vector<string> buffer_tables = {
-                APP_BUFFER_POOL_TABLE_NAME,
-                APP_BUFFER_PROFILE_TABLE_NAME,
-                APP_BUFFER_QUEUE_TABLE_NAME,
-                APP_BUFFER_PG_TABLE_NAME,
-                APP_BUFFER_PORT_INGRESS_PROFILE_LIST_NAME,
-                APP_BUFFER_PORT_EGRESS_PROFILE_LIST_NAME
+            auto* tunnel_decap_orch = new TunnelDecapOrch(m_app_db.get(), APP_TUNNEL_DECAP_TABLE_NAME);
+            vector<string> mux_tables = {
+                CFG_MUX_CABLE_TABLE_NAME,
+                CFG_PEER_SWITCH_TABLE_NAME
             };
+            auto* mux_orch = new MuxOrch(m_config_db.get(), mux_tables, tunnel_decap_orch, gNeighOrch, gFdbOrch);
+            gDirectory.set(mux_orch);
+
+            ASSERT_EQ(gFgNhgOrch, nullptr);
+            const int fgnhgorch_pri = 15;
+
+            vector<table_name_with_pri_t> fgnhg_tables = {
+                { CFG_FG_NHG,                 fgnhgorch_pri },
+                { CFG_FG_NHG_PREFIX,          fgnhgorch_pri },
+                { CFG_FG_NHG_MEMBER,          fgnhgorch_pri }
+            };
+            gFgNhgOrch = new FgNhgOrch(m_config_db.get(), m_app_db.get(), m_state_db.get(), fgnhg_tables, gNeighOrch, gIntfsOrch, gVrfOrch);
+
+            ASSERT_EQ(gSrv6Orch, nullptr);
+            vector<string> srv6_tables = {
+                APP_SRV6_SID_LIST_TABLE_NAME,
+                APP_SRV6_MY_SID_TABLE_NAME
+            };
+            gSrv6Orch = new Srv6Orch(m_app_db.get(), srv6_tables, gSwitchOrch, gVrfOrch, gNeighOrch);
+
+            // Start FlowCounterRouteOrch
+            static const  vector<string> route_pattern_tables = {
+                CFG_FLOW_COUNTER_ROUTE_PATTERN_TABLE_NAME,
+            };
+            gFlowCounterRouteOrch = new FlowCounterRouteOrch(m_config_db.get(), route_pattern_tables);
+
+            ASSERT_EQ(gRouteOrch, nullptr);
+            const int routeorch_pri = 5;
+            vector<table_name_with_pri_t> route_tables = {
+                { APP_ROUTE_TABLE_NAME,        routeorch_pri },
+                { APP_LABEL_ROUTE_TABLE_NAME,  routeorch_pri }
+            };
+            gRouteOrch = new RouteOrch(m_app_db.get(), route_tables, gSwitchOrch, gNeighOrch, gIntfsOrch, gVrfOrch, gFgNhgOrch, gSrv6Orch);
+            gNhgOrch = new NhgOrch(m_app_db.get(), APP_NEXTHOP_GROUP_TABLE_NAME);
+
+            // Recreate buffer orch to read populated data
+            vector<string> buffer_tables = { APP_BUFFER_POOL_TABLE_NAME,
+                                             APP_BUFFER_PROFILE_TABLE_NAME,
+                                             APP_BUFFER_QUEUE_TABLE_NAME,
+                                             APP_BUFFER_PG_TABLE_NAME,
+                                             APP_BUFFER_PORT_INGRESS_PROFILE_LIST_NAME,
+                                             APP_BUFFER_PORT_EGRESS_PROFILE_LIST_NAME };
+
             gBufferOrch = new BufferOrch(m_app_db.get(), m_config_db.get(), m_state_db.get(), buffer_tables);
-
-            vector<string> flex_counter_tables = {
-                CFG_FLEX_COUNTER_TABLE_NAME
-            };
-
-            auto* flexCounterOrch = new FlexCounterOrch(m_config_db.get(), flex_counter_tables);
-
-            gDirectory.set(flexCounterOrch);
-            gDirectory.set(gPortsOrch);
 
             Table portTable = Table(m_app_db.get(), APP_PORT_TABLE_NAME);
 
@@ -185,8 +238,22 @@ namespace intfsorch_test
 
         void TearDown() override
         {
+            gDirectory.m_values.clear();
+
             delete gCrmOrch;
             gCrmOrch = nullptr;
+
+            delete gSwitchOrch;
+            gSwitchOrch = nullptr;
+
+            delete gBfdOrch;
+            gBfdOrch = nullptr;
+
+            delete gNeighOrch;
+            gNeighOrch = nullptr;
+
+            delete gFdbOrch;
+            gFdbOrch = nullptr;
 
             delete gPortsOrch;
             gPortsOrch = nullptr;
@@ -194,13 +261,27 @@ namespace intfsorch_test
             delete gIntfsOrch;
             gIntfsOrch = nullptr;
 
-            delete gNeighOrch;
-            gNeighOrch = nullptr;
+            delete gFgNhgOrch;
+            gFgNhgOrch = nullptr;
+
+            delete gSrv6Orch;
+            gSrv6Orch = nullptr;
+
+            delete gRouteOrch;
+            gRouteOrch = nullptr;
+
+            delete gNhgOrch;
+            gNhgOrch = nullptr;
 
             delete gBufferOrch;
             gBufferOrch = nullptr;
 
-            gDirectory.m_values.clear();
+            delete gVrfOrch;
+            gVrfOrch = nullptr;
+
+            delete gFlowCounterRouteOrch;
+            gFlowCounterRouteOrch = nullptr;
+
             sai_router_intfs_api = pold_sai_rif_api;
             ut_helper::uninitSaiApi();
         }
