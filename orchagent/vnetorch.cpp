c562a44daa8bf5b020ebdc41c33b6102c1864af7
siqbal1486 <shahzad.iqbal@microsoft.com>
2023-04-04 17:15:14 -0700

    utalized hte swss::MacAddr intenral !operator implementation.
    Signed-off-by: siqbal1486 <shahzad.iqbal@microsoft.com>

--- orchagent/vnetorch.cpp
+++ orchagent/vnetorch.cpp
@@ -425,7 +425,7 @@ bool VNetOrch::addOperation(const Request& request)
     sai_attribute_t attr;
     vector<sai_attribute_t> attrs;
     set<string> peer_list = {};
-    bool peer = false, create = false, advertise_prefix = false, od_mac = false;
+    bool peer = false, create = false, advertise_prefix = false;
     uint32_t vni=0;
     string tunnel;
     string scope;
@@ -464,7 +464,6 @@ bool VNetOrch::addOperation(const Request& request)
         else if (name == "overlay_dmac")
         {
             overlay_dmac = request.getAttrMacAddress("overlay_dmac");
-            od_mac = true;
         }
         else
         {
@@ -510,7 +509,7 @@ bool VNetOrch::addOperation(const Request& request)
             else
             {
                 SWSS_LOG_NOTICE("VNET '%s' already exists ", vnet_name.c_str());
-                if (od_mac && overlay_dmac != it->second->getOverlayDMac())
+                if (!!overlay_dmac && overlay_dmac != it->second->getOverlayDMac())
                 {
                     it->second->setOverlayDMac(overlay_dmac);
                     VNetRouteOrch* vnet_route_orch = gDirectory.get<VNetRouteOrch*>();
