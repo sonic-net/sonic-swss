629f264b88057139fcad97df9ea9f417de801fff
Aman Singhal <amans@cisco.com>
2023-05-09 13:48:15 -0700

    Media-based NPU setting: admin-disable port before setPortSerdesAttribute - toggle host_tx_ready flag to trigger CMIS reinit
    Signed-off-by: Aman Singhal <amans@cisco.com>

--- orchagent/portsorch.cpp
+++ orchagent/portsorch.cpp
@@ -3792,17 +3792,32 @@ void PortsOrch::doPortTask(Consumer &consumer)
                         p.m_preemphasis = serdes_attr;
                         m_portList[alias] = p;
                     }
-                    else if (setPortSerdesAttribute(p.m_port_id, gSwitchId, serdes_attr))
-                    {
-                        SWSS_LOG_NOTICE("Set port %s preemphasis is success", alias.c_str());
-                        p.m_preemphasis = serdes_attr;
-                        m_portList[alias] = p;
-                    }
                     else
                     {
-                        SWSS_LOG_ERROR("Failed to set port %s pre-emphasis", alias.c_str());
-                        it++;
-                        continue;
+                        if (p.m_admin_state_up)
+                        {
+                                /* Bring port down before applying serdes attribute*/
+                                if (!setPortAdminStatus(p, false))
+                                {
+                                    SWSS_LOG_ERROR("Failed to set port %s admin status DOWN to set serdes attr", alias.c_str());
+                                    it++;
+                                    continue;
+                                }
+
+                                p.m_admin_state_up = false;
+                        }
+
+                        if (setPortSerdesAttribute(p.m_port_id, gSwitchId, serdes_attr)) {
+                            SWSS_LOG_NOTICE("Set port %s preemphasis is success", alias.c_str());
+                            p.m_preemphasis = serdes_attr;
+                            m_portList[alias] = p;
+                        }
+                        else
+                        {
+                            SWSS_LOG_ERROR("Failed to set port %s pre-emphasis", alias.c_str());
+                            it++;
+                            continue;
+                        }
                     }
                 }
 
