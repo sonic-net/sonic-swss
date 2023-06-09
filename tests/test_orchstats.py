125943daab4815d36e11f0bbdff17fac73d4286e
Ze Gan <ganze718@gmail.com>
2023-06-09 10:49:55 +0800

    Fix vstest
    Signed-off-by: Ze Gan <ganze718@gmail.com>

--- tests/test_orchstats.py
+++ tests/test_orchstats.py
@@ -3,5 +3,5 @@ import conftest
 
 
 def test_orchstats(dvs: conftest.DockerVirtualSwitch,):
-    tb=swsscommon.Table(dvs.get_counters_db(), "ORCH_STATS_TABLE")
-    assert(tb.get("PORT")[0])
+    stats = dict(dvs.get_counters_db().get_entry("ORCH_STATS_TABLE", "PORT_TABLE"))
+    assert(int(stats["SET"]) > 0)
