bc8edfbb94e8414ea850540d7beb146ebcaa32a2
judyjoseph <judy.joseph@microsoft.com>
2023-09-04 14:37:38 -0400

    Again Remove the Error log added earlier for debugging

--- cfgmgr/macsecmgr.cpp
+++ cfgmgr/macsecmgr.cpp
@@ -143,7 +143,6 @@ static std::string decodeKey(const std::string &cipher_str, const MACsecMgr::MAC
         decodedPassword += (char)(hex_int ^ salts[saltIdx++ % (sizeof(salts)/sizeof(salts[0]))]);
     }
 
-    SWSS_LOG_ERROR("\n INPUT (%s) OUTPUT (%s)", cipher_str.c_str(), decodedPassword.c_str());
     return decodedPassword;
 }
 
