5531a65f77f6724c40642be49e7328824a436694
liuh-80 <liuh@microsoft.com>
2023-11-29 02:37:38 +0000

    Imporve code

--- tests/create_appliance.py
+++ tests/create_appliance.py
@@ -14,28 +14,21 @@ def to_string(value):
         return value
     return str(value)
 
-class ZmqProduceStateTable(object):
-    def __init__(self, table_name: str):
-        self.db_connection = swsscommon.DBConnector("APPL_DB", 0)
-        self.zmq_client = swsscommon.ZmqClient("tcp://127.0.0.1:8100")
-        self.zmq_table = swsscommon.ZmqProducerStateTable(
-            self.db_connection,
-            table_name,
-            self.zmq_client,
-            True)
-    def __setitem__(self, key: str, pairs: typing.Union[dict, list, tuple]):
-        pairs_str = []
-        if isinstance(pairs, dict):
-            pairs = pairs.items()
-        for k, v in pairs:
-            pairs_str.append((to_string(k), to_string(v)))
-        self.zmq_table.set(key, pairs_str)
-    def __delitem__(self, key: str):
-        self.zmq_table.delete(str(key))
+# connect to Dash ZMQ endpoint
+db_connection = swsscommon.DBConnector("APPL_DB", 0)
+zmq_client = swsscommon.ZmqClient("tcp://127.0.0.1:8100")
+app_dash_appliance_table = swsscommon.ZmqProducerStateTable(
+                db_connection,
+                "DASH_APPLIANCE_TABLE",
+                zmq_client,
+                True)
 
-vni = sys.argv[1]
-app_dash_appliance_table = ZmqProduceStateTable("DASH_APPLIANCE_TABLE")
+# prepare create appliance request
+pairs_str = []
 pb = Appliance()
 pb.sip.ipv4 = socket.htonl(int(ipaddress.ip_address("10.0.0.1")))
-pb.vm_vni = int(vni)
-app_dash_appliance_table["100"] = {"pb": pb.SerializeToString()}
+pb.vm_vni = int(sys.argv[1])
+pairs_str.append(("pb", pb.SerializeToString()))
+
+# send create appliance request via ZMQ
+app_dash_appliance_table.set("100", pairs_str)
