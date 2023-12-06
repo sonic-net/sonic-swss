4b6fec6258cf2790c77b75a70192c5dbe290e653
Lawrence Lee <lawlee@microsoft.com>
2023-12-06 19:14:02 +0000

    Revert "[REVERT BEFORE MERGE] Dummy changes to test diff"
    This reverts commit f65656d1ebe2088624903017f99eb34734c62435.

--- orchagent/main.cpp
+++ orchagent/main.cpp
@@ -434,7 +434,7 @@ int main(int argc, char **argv)
         }
     }
 
-    SWSS_LOG_NOTICE("--- this line changedStarting Orchestration Agent ---");
+    SWSS_LOG_NOTICE("--- Starting Orchestration Agent ---");
 
     /* Initialize sairedis recording parameters */
     Recorder::Instance().sairedis.setRecord(
