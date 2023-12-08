c2da263c995b79989a8dfd3730d8bdd7176387d0
Lawrence Lee <lawlee@microsoft.com>
2023-12-08 07:10:34 +0000

    Parallelize tracefile generation
    - Update gcov_support.sh to follow shell script conventions
    
    Signed-off-by: Lawrence Lee <lawlee@microsoft.com>

--- tests/gcov_support.sh
+++ tests/gcov_support.sh
@@ -1,40 +1,32 @@
 #!/bin/bash
 ## This script is to enable the gcov support of the SONiC source codes
 work_dir=$(pwd)
-env_home=$HOME
 
 GCNO_LIST_FILE="gcno_file_list.txt"
-GCDA_DIR_LIST="gcda_dir_list.txt"
 TMP_GCDA_FILE_LIST="tmp_gcda_file_list.txt"
-GCNO_ALL_TAR_GZ="gcno.tar.gz"
-
-INFO_DIR=info
-HTML_DIR=html
 ALLMERGE_DIR=AllMergeReport
-
 GCOV_OUTPUT=${work_dir}/gcov_output
-GCOV_INFO_OUTPUT=${GCOV_OUTPUT}/${INFO_DIR}
-GCOV_HTML_OUTPUT=${GCOV_OUTPUT}/${HTML_DIR}
-GCOV_MERGE_REPORT_OUTPUT=${GCOV_OUTPUT}/${ALLMERGE_DIR}
-
 HTML_FILE_PREFIX="GCOVHTML_"
-HTML_FILE_LIST=${GCOV_OUTPUT}/htmllist
 INFO_FILE_PREFIX="GCOVINFO_"
-INFO_FILE_LIST=${GCOV_OUTPUT}/infolist
-INFO_ERR_LIST=${work_dir}/info_err_list
-CONTAINER_LIST=${work_dir}/containerlist
-ALL_INFO_FILES=${work_dir}/allinfofileslist
-REPE_INFO_LIST=${work_dir}/tmpinfolist
-SINGLE_INFO_LIST=${work_dir}/singleinfolist
+
+# overload pushd and popd to reduce log output
+pushd()
+{
+    command pushd "$@" > /dev/null
+}
+popd()
+{
+    command popd > /dev/null
+}
 
 # reset compiling environment
 gcov_support_clean()
 {
-    find /tmp/gcov -name $INFO_FILE_PREFIX* | xargs rm -rf
-    find /tmp/gcov -name $HTML_FILE_PREFIX* | xargs rm -rf
-    find /tmp/gcov -name *.gcno | xargs rm -rf
-    find /tmp/gcov -name *.gcda | xargs rm -rf
-    find /tmp/gcov -name $TMP_GCDA_FILE_LIST | xargs rm -rf
+    find /tmp/gcov -name "$INFO_FILE_PREFIX*" -exec "rm" "-rf" "{}" ";"
+    find /tmp/gcov -name "$HTML_FILE_PREFIX*" -exec "rm" "-rf" "{}" ";"
+    find /tmp/gcov -name "*.gcno" -exec "rm" "-rf" "{}" ";"
+    find /tmp/gcov -name "*.gcda" -exec "rm" "-rf" "{}" ";"
+    find /tmp/gcov -name $TMP_GCDA_FILE_LIST -exec "rm" "-rf" "{}" ";"
     rm /tmp/gcov/info_err_list
     rm /tmp/gcov/gcda_dir_list.txt
 }
@@ -43,211 +35,193 @@ gcov_support_clean()
 verify_info_file()
 {
     local file=$1
-    local path=$2
-    local FILE_OK=`grep "FN:" ${file} | wc -l`
-    if [ $FILE_OK -lt 1 ] ;then
-        echo ${path}/${file} >> /tmp/gcov/info_err_list
-        rm ${file}
+    local valid_line_count
+    valid_line_count=$(grep -c "FN:" "${file}")
+
+    if [ "$valid_line_count" -lt 1 ] ;then
+        echo "${file}" >> info_err_list
+        rm "${file}"
     fi
 }
 
-# search and save the dir where the lcov should be implemented
-list_lcov_path()
+generate_single_tracefile()
 {
-    local find_gcda_file
-    local find_gcno_file
-    local gcdastr=".gcda"
     local gcda_dir=$1
-
-    echo ${gcda_dir}
-
-    TMP_FILE=${gcda_dir}/tmpgcdalist
-    echo "Start searching .gcda files..."
-    exec 4>$TMP_FILE
-    find_gcda_file=`find ${gcda_dir} -name *.gcda`
-    RESULT=${find_gcda_file}
-    echo "$RESULT" >&4
-    exec 4>&-
-
-    cat ${TMP_FILE} | xargs dirname | uniq > ${gcda_dir}/gcda_dir_list.txt
+    local output_file=${gcda_dir}/$2
+    lcov -c -d "${gcda_dir}" -o "${output_file}" &>/dev/null
+    verify_info_file "${output_file}"
 }
 
 # generate gcov base info and html report for specified range files
-lcov_genhtml_report()
+generate_archive_tracefiles()
 {
     local gcda_file_range=$1
-    list_lcov_path ${gcda_file_range}
 
-    while read line
-    do
+    # find all directories that contain GCDA files
+    local gcda_dir_list
+    gcda_dir_list=$(find "${gcda_file_range}" -name "*.gcda" -exec "dirname" "{}" ";" | uniq)
+
+    # for each directory containg GCDA files, use lcov to generate a tracefile
+    while IFS= read -r line; do
         local fullpath=$line
         local infoname=${INFO_FILE_PREFIX}${fullpath##*/}.info
 
-        echo ${fullpath}
-
-        pushd ${fullpath} > /dev/null
-        GCDA_COUNT=`find -name "*.gcda" | wc -l`
-        echo "gcda count: $GCDA_COUNT"
-        if [ $GCDA_COUNT -ge 1 ]; then
-            if [ -f "$infoname" ]; then
-                echo "Executing lcov -c -d . -o temp.info"
-                lcov -c -d . -o temp.info &>/dev/null
-                echo "Executing lcov -a temp.info -a ${infoname} -o ${infoname}"
-                lcov -a ${infoname} -a temp.info -o ${infoname} &>/dev/null
-                rm temp.info
-            else
-                echo "Executing lcov -c -d . -o ${infoname}"
-                lcov -c -d . -o ${infoname} &>/dev/null
-            fi
-
-            if [ "$?" != "0" ]; then
-                echo "lcov fail!"
-                rm ${infoname}
-            fi
+        GCDA_COUNT=$(find "${fullpath}" -name "*.gcda" | wc -l)
+        if [ "$GCDA_COUNT" -ge 1 ]; then
+            generate_single_tracefile "${fullpath}" "${infoname}" &
         fi
-        popd > /dev/null
-    done < ${gcda_file_range}/gcda_dir_list.txt
+        # limit max # of parallel jobs to half the # of CPU cores
+        [ "$( jobs | wc -l )" -ge "$(( $(nproc) / 2 ))" ] && wait 
+    done <<< "${gcda_dir_list}"
+    wait
 }
 
 lcov_merge_all()
 {
-    cp -rf common_work $1/
-    find . -name *.info > infolist
-    while read line
-    do
+    info_files=$(find "${GCOV_OUTPUT}" -name "*.info")
+    while IFS= read -r info_file; do
         if [ ! -f "total.info" ]; then
-            lcov -o total.info -a ${line}
+            lcov -o total.info -a "${info_file}"
         else
-            lcov -o total.info -a total.info -a ${line}
+            lcov -o total.info -a total.info -a "${info_file}"
         fi
-    done < infolist
+    done <<< "$info_files"
 
-    # Remove unit test files.
+    # Remove unit test files and system libraries
     lcov -o total.info -r total.info "*tests/*"
     lcov -o total.info -r total.info "/usr/*"
 
-    cp $1/lcov_cobertura.py $1/common_work/gcov/
-    python $1/common_work/gcov/lcov_cobertura.py total.info -o coverage.xml
+    python lcov_cobertura.py total.info -o coverage.xml
 
     sed -i "s#../../__w/1/s/##" coverage.xml
     sed -i "s#......__w.1.s.##" coverage.xml
 
-    cd gcov_output/
+    pushd gcov_output/
     if [ ! -d ${ALLMERGE_DIR} ]; then
         mkdir -p ${ALLMERGE_DIR}
     fi
 
     cp ../coverage.xml ${ALLMERGE_DIR}
-
-    cd ../
+    popd
 }
 
 gcov_set_environment()
 {
-    local build_dir
+    local build_dir containers
 
     build_dir=$1
-    mkdir -p ${build_dir}/gcov_tmp
-    mkdir -p ${build_dir}/gcov_tmp/sonic-gcov
+    mkdir -p "${build_dir}"/gcov_tmp
+    mkdir -p "${build_dir}"/gcov_tmp/sonic-gcov
 
-    docker ps -q > ${CONTAINER_LIST}
+    containers=$(docker ps -q)
 
-    echo "### Start collecting info files from existed containers"
+    echo "### Start collecting info files from existed containers:"
+    echo "${containers}"
 
-    for line in $(cat ${CONTAINER_LIST})
-    do
+    while IFS= read -r line; do
         local container_id=${line}
-        echo ${container_id}
+        echo "${container_id}"
         echo "script_count"
-        script_count=`docker exec -i ${container_id} find / -name gcov_support.sh | wc -l`
-        echo ${script_count}
-        if [ ${script_count} -gt 0 ]; then
-            docker exec -i ${container_id} killall5 -15
-            docker exec -i ${container_id} /tmp/gcov/gcov_support.sh collect_gcda
+        script_count=$(docker exec -i "${container_id}" find / -name gcov_support.sh | wc -l)
+        echo "${script_count}"
+        if [ "${script_count}" -gt 0 ]; then
+            docker exec -i "${container_id}" killall5 -15
+            docker exec -i "${container_id}" /tmp/gcov/gcov_support.sh collect_gcda
         fi
-        gcda_count=`docker exec -i ${container_id} find / -name *.gcda | wc -l`
-        if [ ${gcda_count} -gt 0 ]; then
+        gcda_count=$(docker exec -i "${container_id}" find / -name *.gcda | wc -l)
+        if [ "${gcda_count}" -gt 0 ]; then
             echo "find gcda in "
-            echo ${container_id}
-            mkdir -p ${build_dir}/gcov_tmp/sonic-gcov/${container_id}
-            pushd ${build_dir}/gcov_tmp/sonic-gcov/${container_id}
-            docker cp ${container_id}:/tmp/gcov/ .
-            cp gcov/gcov_support.sh ${build_dir}/gcov_tmp/sonic-gcov
-            cp gcov/lcov_cobertura.py ${build_dir}/gcov_tmp/sonic-gcov
+            echo "${container_id}"
+            mkdir -p "${build_dir}/gcov_tmp/sonic-gcov/${container_id}"
+            pushd "${build_dir}/gcov_tmp/sonic-gcov/${container_id}"
+            docker cp "${container_id}":/tmp/gcov/ .
+            cp gcov/gcov_support.sh "${build_dir}"/gcov_tmp/sonic-gcov
+            cp gcov/lcov_cobertura.py "${build_dir}"/gcov_tmp/sonic-gcov
             popd
         fi
-    done
-
-    echo "cat list"
-    cat ${CONTAINER_LIST}
+    done <<< "${containers}"
 }
 
-gcov_merge_info()
+process_gcda_archive()
 {
-    lcov_merge_all $1
+    local archive_path=$1
+    echo "Generating tracefiles from ${archive_path}"
+
+    local src_archive src_dir dst_dir
+    src_archive=$(basename "${archive_path}")
+    src_dir=$(dirname "${archive_path}")
+    dst_dir=${src_archive//".tar.gz"/}
+
+    # create a temp working directory for the GCDA archive so that GCDA files from other archives aren't overwritten
+    mkdir -p "${dst_dir}"
+    cp -r "${src_dir}/." "${dst_dir}"
+    tar -C "${dst_dir}" -zxf "${dst_dir}/${src_archive}"
+
+    generate_archive_tracefiles "${dst_dir}"
+    echo "Done generating tracefiles from ${archive_path}"
 }
 
-gcov_support_generate_report()
+combine_tracefiles()
 {
-    ls -F | grep "/$" > container_dir_list
-    sed -i '/gcov_output/d' container_dir_list
-    sed -i "s#\/##g" container_dir_list
+    local archive src_dir tracefiles dst_file
+    archive=$(basename "${1}")
+    src_dir=${archive//".tar.gz"/}
+    tracefiles=$(find "${src_dir}" -name "*.info")
 
-    mkdir -p gcov_output/info
-
-    #for same code path
-    mkdir -p common_work/gcov
-    tar -zxf swss.tar.gz -C common_work/gcov
-
-    while read line
-    do
-        local container_id=${line}
-        echo "Processing container ${container_id}"
-
-        cp -rf ${container_id}/* common_work
-        pushd common_work/gcov
-
-        find -name gcno*.tar.gz > tmp_gcno.txt
-        echo "Found GCNO archives:"
-        cat tmp_gcno.txt
-        while read LINE ; do
-            tar -zxf ${LINE}
-        done < tmp_gcno.txt
-        rm tmp_gcno.txt
-
-        find -name 'gcda*.tar.gz' > tmp_gcda.txt
-        echo "Found GCDA archives:"
-        cat tmp_gcda.txt
-        while read LINE ; do
-            echo "Generating tracefiles from ${LINE}..."
-            tar -zxf ${LINE}
-            lcov_genhtml_report .
-            echo "Done generating tracefiles from ${LINE}"
-        done < tmp_gcda.txt
-        rm tmp_gcda.txt
-        popd
-
-        if [ "$?" != "0" ]; then
-            echo "###lcov operation fail.."
-            return 0
+    while IFS= read -r src_file; do
+        dst_file=${src_file//${src_dir}/"gcov"}
+        if [ -f "${dst_file}" ]; then
+            lcov -o "${dst_file}" -a "${dst_file}" -a "${src_file}"
+        else
+            lcov -o "${dst_file}" -a "${src_file}"
         fi
-        mkdir -p gcov_output/${container_id}
-        cp -rf common_work/*  gcov_output/${container_id}/
-        pushd gcov_output/${container_id}
-        find . -name "*.gcda" -o -name "*.gcno" -o -name "*.gz" -o -name "*.cpp" -o -name "*.h" | xargs rm -rf
-        popd
-        pushd common_work
-        find . -name "*.gcda" -o -name "*.gz" -o -name "*.info" | xargs rm -rf
-        popd
-        rm -rf ${container_id}
-    done < container_dir_list
+    done <<< "$tracefiles"
+}
 
-    # generate report with code
-    pushd common_work/gcov
-    find . -name "*.gcno" | xargs rm -rf
-    popd
+generate_tracefiles()
+{
+    local container_dir_list
+    container_dir_list=$(find . -maxdepth 1 -mindepth 1 -type d) 
 
-    echo "### Make info generating completed !!"
+    while IFS= read -r container_id; do
+        echo "Processing container ${container_id}"
+        pushd "${container_id}"
+
+        # untar any GCNO archives
+        local gcno_archives gcda_archives
+        gcno_archives=$(find . -name "gcno*.tar.gz")
+        if [ -z "${gcno_archives}" ]; then
+            echo "No GCNO files found!"
+        else
+            echo "Found GCNO archives:"
+            echo "${gcno_archives}"
+            while IFS= read -r gcno_archive; do
+                tar -C "$(dirname "${gcno_archive}")" -zxf "${gcno_archive}"
+            done <<< "$gcno_archives"
+        fi
+
+        gcda_archives=$(find . -name 'gcda*.tar.gz')
+        if [ -z "${gcda_archives}" ]; then
+            echo "No GCDA files found!"
+        else
+            echo "Found GCDA archives:"
+            echo "${gcda_archives}"
+            # process each GCDA archive in parallel. since each archive is extracted to a separate directory, there is no risk of overwriting files
+            while IFS= read -r gcda_archive; do
+                process_gcda_archive "${gcda_archive}" &
+                # limit max # of parallel jobs to half the # of CPU cores
+                [ "$( jobs | wc -l )" -ge "$(( $(nproc) / 2 ))" ] && wait 
+            done <<< "$gcda_archives"
+            wait
+        fi
+
+        mkdir -p "gcov_output/${container_id}"
+        find "${container_id}" -name "*.info" -exec "cp" "-r" "--parents" "{}" "gcov_output/" ";"
+
+        rm -rf "${container_id}"
+        echo "Done processing container ${container_id}"
+    done <<< "$container_dir_list" 
+    echo "Tracefile generation completed"
 }
 
 # list and save the generated .gcda files
@@ -259,8 +233,8 @@ gcov_support_collect_gcda()
 
     pushd /
     # check whether .gcda files exist
-    gcda_files_count=`find \. -name "*\.gcda" 2>/dev/null | wc -l`
-    if [ ${gcda_files_count} -lt 1 ]; then
+    gcda_files_count=$(find \. -name "*\.gcda" 2>/dev/null | wc -l)
+    if [ "${gcda_files_count}" -lt 1 ]; then
         echo "### no gcda files found!"
         return 0
     fi
@@ -270,14 +244,14 @@ gcov_support_collect_gcda()
     gcov_support_clean
 
     pushd /tmp/gcov
-    gcno_count=`find -name gcno*.tar.gz 2>/dev/null | wc -l`
-    if [ ${gcno_count} -lt 1 ]; then
+    gcno_count=$(find . -name "gcno*.tar.gz" 2>/dev/null | wc -l)
+    if [ "${gcno_count}" -lt 1 ]; then
         echo "### Fail! Cannot find any gcno files, please check."
-        return -1
+        return 1
     fi
 
-    gcda_count=`find -name gcda*.tar.gz 2>/dev/null | wc -l`
-    if [ ${gcda_count} -lt 1 ]; then
+    gcda_count=$(find . -name "gcda*.tar.gz" 2>/dev/null | wc -l)
+    if [ "${gcda_count}" -lt 1 ]; then
         echo "### Cannot find any gcda files, please check."
         return 0
     fi
@@ -297,36 +271,38 @@ gcov_support_collect_gcno()
     local tar_command
     local submodule_name
 
-    find gcno*.tar.gz > tmp_gcno.txt
-    while read LINE ; do
-        rm -f ${LINE}
+    find "gcno*.tar.gz" > tmp_gcno.txt
+    while read -r LINE ; do
+        rm -f "${LINE}"
     done < tmp_gcno.txt
     rm tmp_gcno.txt
 
     # rename .tmp*_gcno files generated
-    for tmp_gcno in `find -name .tmp_*.gcno`
+    tmp_gcno_files=$(find . -name ".tmp_*.gcno")
+    while IFS= read -r tmp_gcno;
     do
-        new_gcno=`echo ${tmp_gcno} | sed 's/.tmp_//g'`
-        echo ${new_gcno}
-        mv ${tmp_gcno} ${new_gcno}
-    done
+        new_gcno=${tmp_gcno//".tmp_"/}
+        echo "${new_gcno}"
+        mv "${tmp_gcno}" "${new_gcno}"
+    done <<< "$tmp_gcno_files"
 
     echo " === Start collecting .gcno files... === "
     submodule_name=$1
     exec 3>$GCNO_LIST_FILE
-    find_command=`find -name "*.gcno" -o -name "*.gcda"`
+    find_command=$(find . -name "*.gcno" -o -name "*.gcda")
     echo "${find_command}"
     if [ -z "${find_command}" ]; then
         echo "### Error! no gcno files found!"
-        return -1
+        return 1
     fi
     RESULT=${find_command}
     echo "$RESULT" >&3
     exec 3>&-
     
-    local filesize=`ls -l $GCNO_LIST_FILE | awk '{print $5}'`
+    local filesize
+    filesize=$(ls -l $GCNO_LIST_FILE | awk '{print $5}')
     # Empty gcno_file_list indicates the non-gcov compling mode
-    if [ ${filesize} -le 1 ]; then
+    if [ "${filesize}" -le 1 ]; then
         echo "empty gcno_file_list.txt"
         rm $GCNO_LIST_FILE
     else
@@ -335,14 +311,14 @@ gcov_support_collect_gcno()
         echo "${tar_command}"
         ${tar_command}
         # temporarily using fixed dir
-        cp gcno_$submodule_name.tar.gz ${work_dir}/debian/$submodule_name/tmp/gcov
-        cp ./tests/gcov_support.sh ${work_dir}/debian/$submodule_name/tmp/gcov
-        cp ./tests/gcov_support.sh ${work_dir}/tests
-        cp ./gcovpreload/lcov_cobertura.py ${work_dir}/debian/$submodule_name/tmp/gcov
-        mkdir -p ${work_dir}/debian/$submodule_name/usr
-        mkdir -p ${work_dir}/debian/$submodule_name/usr/lib
-        cp ./gcovpreload/libgcovpreload.so ${work_dir}/debian/$submodule_name/usr/lib
-        sudo chmod 777 -R /${work_dir}/debian/$submodule_name/usr/lib/libgcovpreload.so
+        cp gcno_"${submodule_name}".tar.gz "${work_dir}/debian/${submodule_name}/tmp/gcov"
+        cp ./tests/gcov_support.sh "${work_dir}/debian/${submodule_name}/tmp/gcov"
+        cp ./tests/gcov_support.sh "${work_dir}/tests"
+        cp ./gcovpreload/lcov_cobertura.py "${work_dir}/debian/${submodule_name}/tmp/gcov"
+        mkdir -p "${work_dir}/debian/${submodule_name}/usr"
+        mkdir -p "${work_dir}/debian/${submodule_name}/usr/lib"
+        cp ./gcovpreload/libgcovpreload.so "${work_dir}/debian/${submodule_name}/usr/lib"
+        sudo chmod 777 -R "/${work_dir}/debian/${submodule_name}/usr/lib/libgcovpreload.so"
         rm $GCNO_LIST_FILE
         echo " === Collect finished... === "
     fi
@@ -352,19 +328,19 @@ main()
 {
     case $1 in
         collect)
-            gcov_support_collect_gcno $2
+            gcov_support_collect_gcno "$2"
             ;;
         collect_gcda)
             gcov_support_collect_gcda
             ;;
         generate)
-            gcov_support_generate_report
+            generate_tracefiles
             ;;
         merge_container_info)
-            gcov_merge_info $2
+            lcov_merge_all
             ;;
         set_environment)
-            gcov_set_environment $2
+            gcov_set_environment "$2"
             ;;
         *)
             echo "Usage:"
@@ -376,6 +352,6 @@ main()
     esac
 }
 
-main $1 $2
+main "$1" "$2"
 exit
 
