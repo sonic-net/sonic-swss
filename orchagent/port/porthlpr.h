4c3cfd2d11f5f3933a51efc539ca3628e75eb9ce
Rida Hanif <rida.hanif@xflowresearch.com>
2024-05-10 06:52:10 +0000

    Fixing for dhcp

--- orchagent/port/porthlpr.h
+++ orchagent/port/porthlpr.h
@@ -53,6 +53,5 @@ private:
     bool parsePortRole(PortConfig &port, const std::string &field, const std::string &value) const;
     bool parsePortAdminStatus(PortConfig &port, const std::string &field, const std::string &value) const;
     bool parsePortDescription(PortConfig &port, const std::string &field, const std::string &value) const;
-    bool parsePortDhcpRateLimit(PortConfig &port, const std::string &field, const std::string &value) const;
 
 };
