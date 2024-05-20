3c7139aa1f7d4e356455da2aafca83b1b6dd8b32
Muhammad Ali Hussnain <91005947+muhammadalihussnain@users.noreply.github.com>
2024-05-20 08:43:18 +0000

    std::str(fvValue(i))

--- cfgmgr/portmgr.cpp
+++ cfgmgr/portmgr.cpp
@@ -226,7 +226,7 @@ void PortMgr::doTask(Consumer &consumer)
                 }
                 else if (fvField(i) == "dhcp_rate_limit")
                 {
-                    dhcp_rate_limit = str(fvValue(i));
+                    dhcp_rate_limit = std::str(fvValue(i));
                 }
                 else
                 {
