5b850dbc781334274e894d76dd818cebbf60ada5
Muhammad Ali Hussnain <91005947+muhammadalihussnain@users.noreply.github.com>
2024-05-20 06:58:51 +0000

    removed shellquote(aliase)

--- cfgmgr/portmgr.cpp
+++ cfgmgr/portmgr.cpp
@@ -89,8 +89,8 @@ bool PortMgr::setPortDHCPMitigationRate(const string &alias, const string &dhcp_
         // &&
         // tc filter add dev <port_name> protocol ip parent ffff: prio 1 u32 match ip protocol 17 0xff match ip dport 67 0xffff police rate <byte_rate>bps burst <byte_rate>b conform-exceed drop
           
-        cmd << "sudo "<< TC_CMD << " qdisc add dev " << shellquote(alias) << " handle ffff: ingress" << " && " \
-            <<" sudo "<< TC_CMD << " filter add dev " << shellquote(alias) << " protocol ip parent ffff: prio 1 u32 match ip protocol 17 0xff match ip dport 67 0xffff police rate " << byte_rate << "bps burst " << byte_rate << "b conform-exceed drop";
+        cmd << "sudo "<< TC_CMD << " qdisc add dev " << alias << " handle ffff: ingress" << " && " \
+            <<" sudo "<< TC_CMD << " filter add dev " << alias << " protocol ip parent ffff: prio 1 u32 match ip protocol 17 0xff match ip dport 67 0xffff police rate " << byte_rate << "bps burst " << byte_rate << "b conform-exceed drop";
         cmd_str = cmd.str();
         ret = swss::exec(cmd_str, res);
     }
