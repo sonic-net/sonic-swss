48ced1913822bd05fd02dbc4a6f1c9eef3ac5acf
muhammadalihussnain <ali_hussnain@live.com>
2024-05-28 10:48:46 +0500

    changed order for commands and values

--- tests/mock_tests/portmgr_ut.cpp
+++ tests/mock_tests/portmgr_ut.cpp
@@ -52,17 +52,23 @@ namespace portmgr_ut
         m_portMgr->addExistingData(&cfg_port_table);
         m_portMgr->doTask();
         ASSERT_TRUE(mockCallArgs.empty());
+
         std::vector<FieldValueTuple> values;
         app_port_table.get("Ethernet0", values);
+
         auto value_opt = swss::fvsGetValue(values, "mtu", true);
         ASSERT_TRUE(value_opt);
         ASSERT_EQ(DEFAULT_MTU_STR, value_opt.get());
+
         value_opt = swss::fvsGetValue(values, "admin_status", true);
         ASSERT_TRUE(value_opt);
-        ASSERT_EQ(DEFAULT_DHCP_RATE_LIMIT_STR, value_opt.get());
+        ASSERT_EQ(DEFAULT_ADMIN_STATUS_STR, value_opt.get());
+
+        
         value_opt = swss::fvsGetValue(values, "dhcp_rate_limit", true);
         ASSERT_TRUE(value_opt);
         ASSERT_EQ(DEFAULT_ADMIN_STATUS_STR, value_opt.get());
+        
         value_opt = swss::fvsGetValue(values, "speed", true);
         ASSERT_TRUE(value_opt);
         ASSERT_EQ("100000", value_opt.get());
@@ -78,8 +84,8 @@ namespace portmgr_ut
         m_portMgr->doTask();
         ASSERT_EQ(size_t(2), mockCallArgs.size());
         ASSERT_EQ("/sbin/ip link set dev \"Ethernet0\" mtu \"9100\"", mockCallArgs[0]);
-        ASSERT_EQ("/sbin/ip link set dev \"Ethernet0\" down", mockCallArgs[1]);
-        ASSERT_EQ("/sbin/tc qdisc add dev \"Ethernet0\" handle ffff: ingress && /sbin/tc filter add dev \"Ethernet0\" protocol ip parent ffff: prio 1 u32 match ip protocol 17 0xff match ip dport 67 0xffff police rate 406000bps burst 406000b conform-exceed drop", mockCallArgs[2]) << "Unexpected command: " << mockCallArgs[2];
+        ASSERT_EQ("/sbin/tc qdisc add dev \"Ethernet0\" handle ffff: ingress && /sbin/tc filter add dev \"Ethernet0\" protocol ip parent ffff: prio 1 u32 match ip protocol 17 0xff match ip dport 67 0xffff police rate 406000bps burst 406000b conform-exceed drop", mockCallArgs[1]) << "Unexpected command: " << mockCallArgs[1];
+        ASSERT_EQ("/sbin/ip link set dev \"Ethernet0\" down", mockCallArgs[2]);
         
         // Set port admin_status, verify that it could override the default value
         cfg_port_table.set("Ethernet0", {
