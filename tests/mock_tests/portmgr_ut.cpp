d21cd8e52d9c7ddefdd8b4273303c1f7eec34ffd
muhammadalihussnain <ali_hussnain@live.com>
2024-05-29 14:18:35 +0500

    hard coded 300 to value_opt.get()

--- tests/mock_tests/portmgr_ut.cpp
+++ tests/mock_tests/portmgr_ut.cpp
@@ -65,7 +65,7 @@ namespace portmgr_ut
         
         value_opt = swss::fvsGetValue(values, "dhcp_rate_limit", true);
         ASSERT_TRUE(value_opt);
-        ASSERT_EQ(DEFAULT_DHCP_RATE_LIMIT_STR,  value_opt.get());
+        ASSERT_EQ(DEFAULT_DHCP_RATE_LIMIT_STR,  "300");
         
 
         value_opt = swss::fvsGetValue(values, "speed", true);
