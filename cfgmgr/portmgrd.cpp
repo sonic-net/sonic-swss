1980e65afc11d8e39eb8738724064bc04dec8e82
Muhammad Ali Hussnain <91005947+muhammadalihussnain@users.noreply.github.com>
2024-07-02 04:55:19 +0000

    Removed Logs from Portmgrd.cpp

--- cfgmgr/portmgrd.cpp
+++ cfgmgr/portmgrd.cpp
@@ -3,78 +3,53 @@
 #include <mutex>
 #include <unistd.h>
 #include <vector>
-
 #include "exec.h"
 #include "portmgr.h"
 #include "schema.h"
 #include "select.h"
-
 using namespace std;
 using namespace swss;
-
 /* select() function timeout retry time, in millisecond */
 #define SELECT_TIMEOUT 1000
-
 int main(int argc, char **argv)
 {
     Logger::linkToDbNative("portmgrd");
     SWSS_LOG_ENTER();
-
     SWSS_LOG_NOTICE("--- Starting portmgrd ---");
-
     try
     {
         vector<string> cfg_port_tables = {
             CFG_PORT_TABLE_NAME,
             CFG_SEND_TO_INGRESS_PORT_TABLE_NAME,
         };
-
         DBConnector cfgDb("CONFIG_DB", 0);
         DBConnector appDb("APPL_DB", 0);
         DBConnector stateDb("STATE_DB", 0);
-
         PortMgr portmgr(&cfgDb, &appDb, &stateDb, cfg_port_tables);
         vector<Orch *> cfgOrchList = {&portmgr};
-
         swss::Select s;
         for (Orch *o : cfgOrchList)
         {
             s.addSelectables(o->getSelectables());
         }
-
         while (true)
         {
             Selectable *sel;
             int ret;
-
             ret = s.select(&sel, SELECT_TIMEOUT);
             if (ret == Select::ERROR)
             {
-                SWSS_LOG_ERROR("Select error: %s!", strerror(errno));
+                SWSS_LOG_NOTICE("Error: %s!", strerror(errno));
                 continue;
             }
             if (ret == Select::TIMEOUT)
             {
-                try
-                {
-                    portmgr.doTask();
-                }
-                catch (const exception &e)
-                {
-                    SWSS_LOG_ERROR("Exception during doTask: %s", e.what());
-                }
+                portmgr.doTask();
                 continue;
             }
 
-            try
-            {
-                auto *c = (Executor *)sel;
-                c->execute();
-            }
-            catch (const exception &e)
-            {
-                SWSS_LOG_ERROR("Exception during execute: %s", e.what());
-            }
+            auto *c = (Executor *)sel;
+            c->execute();
         }
     }
     catch (const exception &e)
