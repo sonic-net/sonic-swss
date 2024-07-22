f96de39b9e629e870df67fefb42de7ad4642de86
Yijiao Qin <y.qin@alibaba-inc.com>
2024-07-22 14:22:16 -0700

    [orchagent] implement ring buffer feature with a flag

--- orchagent/orchdaemon.h
+++ orchagent/orchdaemon.h
@@ -84,7 +84,21 @@ public:
         m_fabricQueueStatEnabled = enabled;
     }
     void logRotate();
+
+    // Two required API to support ring buffer feature
+    /**
+     * This method is used by a ring buffer consumer [Orchdaemon] to initialzie its ring,
+     * and populate this ring's pointer to the producers [Orch, Consumer], to make sure that
+     * they are connected to the same ring.
+     */
+    void enableRingBuffer();
+    /**
+     * This method describes how the ring consumer consumes this ring.
+     */
+    void popRingBuffer();
+
 private:
+    std::thread ring_thread;
     DBConnector *m_applDb;
     DBConnector *m_configDb;
     DBConnector *m_stateDb;
@@ -97,7 +111,7 @@ private:
 
     std::vector<Orch *> m_orchList;
     Select *m_select;
-    
+
     std::chrono::time_point<std::chrono::high_resolution_clock> m_lastHeartBeat;
 
     void flush();
@@ -105,6 +119,11 @@ private:
     void heartBeat(std::chrono::time_point<std::chrono::high_resolution_clock> tcurrent, long interval);
 
     void freezeAndHeartBeat(unsigned int duration, long interval);
+
+protected:
+    /* Orchdaemon instance points to the same ring buffer during its lifetime */
+    OrchRing* gRingBuffer = nullptr;
+    std::atomic<bool> ring_thread_exited{false};
 };
 
 class FabricOrchDaemon : public OrchDaemon
