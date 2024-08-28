0b0d443914183a3772c9c2cc651b45bcacb177be
Lawrence Lee <lawlee@microsoft.com>
2024-08-28 01:00:00 +0000

    remove old code
    Signed-off-by: Lawrence Lee <lawlee@microsoft.com>

--- tests/test_dash_pl.py
+++ tests/test_dash_pl.py
@@ -25,24 +25,17 @@ def apply_base_pl_route(dash_db: DashDB):
     
     dash_db.set_app_db_entry(APP_DASH_ROUTE_TABLE_NAME, ROUTE_GROUP1, OUTBOUND_ROUTE_PREFIX, ROUTE_PL_CONFIG)
     dash_db.set_app_db_entry(APP_DASH_ENI_ROUTE_TABLE_NAME, ENI_ID, ENI_ROUTE_CONFIG)
-    #create_route(dash_db, ROUTE_GROUP1, OUTBOUND_ROUTE_PREFIX, ROUTE_PL_CONFIG)
     yield
     dash_db.remove_app_db_entry(APP_DASH_ENI_ROUTE_TABLE_NAME, ENI_ID )
     dash_db.remove_app_db_entry(APP_DASH_ROUTE_TABLE_NAME, ROUTE_GROUP1, OUTBOUND_ROUTE_PREFIX)
-    #remove_route(dash_db, ROUTE_GROUP1, OUTBOUND_ROUTE_PREFIX)
 
 @pytest.fixture
 def apply_pl_route_with_underlay_sip(dash_db: DashDB):
     dash_db.set_app_db_entry(APP_DASH_ROUTE_TABLE_NAME, ROUTE_GROUP1, OUTBOUND_ROUTE_PREFIX, ROUTE_PL_CONFIG_WITH_UNDERLAY_SIP)
     dash_db.set_app_db_entry(APP_DASH_ENI_ROUTE_TABLE_NAME, ENI_ID, ENI_ROUTE_CONFIG)
-    #create_route(dash_db, ROUTE_GROUP1, OUTBOUND_ROUTE_PREFIX, ROUTE_PL_CONFIG_WITH_UNDERLAY_SIP)
-    #create_eni_route(dash_db, ENI_ID, ENI_ROUTE_CONFIG)
     yield
-
     dash_db.remove_app_db_entry(APP_DASH_ENI_ROUTE_TABLE_NAME, ENI_ID)
     dash_db.remove_app_db_entry(APP_DASH_ROUTE_TABLE_NAME, ROUTE_GROUP1, OUTBOUND_ROUTE_PREFIX)
-    #remove_eni_route(dash_db, ENI_ID)
-    #remove_route(dash_db, ROUTE_GROUP1, OUTBOUND_ROUTE_PREFIX)
 
 
 @pytest.fixture(scope='module', autouse=True)
@@ -54,13 +47,6 @@ def common_setup_teardown(dash_db: DashDB):
     dash_db.set_app_db_entry(APP_DASH_VNET_MAPPING_TABLE_NAME, VNET1, VNET_MAP_IP1, VNET_MAPPING_CONFIG)
     dash_db.set_app_db_entry(APP_DASH_ROUTE_GROUP_TABLE_NAME, ROUTE_GROUP1, ROUTE_GROUP_CONFIG)
 
-    # create_routing_type(dash_db, PRIVATELINK, ROUTING_TYPE_PL_CONFIG)
-    # create_appliance(dash_db, APPLIANCE_ID, APPLIANCE_CONFIG)
-    # create_vnet(dash_db, VNET1, VNET_CONFIG)
-    # create_eni(dash_db, ENI_ID, ENI_CONFIG)
-    # create_vnet_mapping(dash_db, VNET1, VNET_MAP_IP1, VNET_MAPPING_CONFIG)
-    # create_route_group(dash_db, ROUTE_GROUP1, ROUTE_GROUP_CONFIG)
-
     yield
 
     dash_db.remove_app_db_entry(APP_DASH_ROUTE_GROUP_TABLE_NAME, ROUTE_GROUP1)
@@ -70,19 +56,11 @@ def common_setup_teardown(dash_db: DashDB):
     dash_db.remove_app_db_entry(APP_DASH_APPLIANCE_TABLE_NAME, APPLIANCE_ID)
     dash_db.remove_app_db_entry(APP_DASH_ROUTING_TYPE_TABLE_NAME, PRIVATELINK)
 
-    # remove_route_group(dash_db, ROUTE_GROUP1)
-    # remove_vnet_mapping(dash_db, VNET1, VNET_MAP_IP1)
-    # remove_eni(dash_db, ENI_ID)
-    # remove_vnet(dash_db, VNET1)
-    # remove_appliance(dash_db, APPLIANCE_ID)
-    # remove_routing_type(dash_db, PRIVATELINK)
-
 
 def test_pl_eni_attrs(dash_db: DashDB, apply_base_pl_route):
     enis= dash_db.wait_for_asic_db_keys("ASIC_STATE:SAI_OBJECT_TYPE_ENI")
     assert enis
     eni_attrs = dash_db.get_asic_db_entry("ASIC_STATE:SAI_OBJECT_TYPE_ENI", enis[0])
-    # eni_attrs = dash_db.asic_eni_table[enis[0]]
     assert SAI_ENI_ATTR_PL_UNDERLAY_SIP in eni_attrs
     assert eni_attrs[SAI_ENI_ATTR_PL_UNDERLAY_SIP] == PL_UNDERLAY_SIP1
 
