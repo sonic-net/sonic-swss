3552d823e5f5592d235106466f46cd813cb0eb2c
Muhammad Ali Hussnain <91005947+muhammadalihussnain@users.noreply.github.com>
2024-09-05 11:50:04 +0000

    typo at 117 portmgr.cpp

--- cfgmgr/portmgr.cpp
+++ cfgmgr/portmgr.cpp
@@ -97,17 +97,18 @@ bool PortMgr::setPortDHCPMitigationRate(const string &alias, const string &dhcp_
         SWSS_LOG_WARN("ret Value in setPortDHCPMitigationRate is  ret:%d,", ret);
 
         if (!ret)
-        {   SWSS_LOG_INFO("writing dhcp_rate_limit to appl_db");
-            return writeConfigToAppDb(alias, "dhcp_rate_limit", dhcp_rate_limit);
-        }
+            {   
+                SWSS_LOG_INFO("writing dhcp_rate_limit to appl_db");
+                return writeConfigToAppDb(alias, "dhcp_rate_limit", dhcp_rate_limit);
+            }
         else if (!isPortStateOk(alias))
-    {
-        // Can happen when a DEL notification is sent by portmgrd immediately followed by a new SET notif
-        SWSS_LOG_WARN("Setting dhcp_rate_limit to alias:%s netdev failed with cmd:%s, rc:%d, error:%s", alias.c_str(), cmd_str.c_str(), ret, res.c_str());
-        return false;
+            {
+            // Can happen when a DEL notification is sent by portmgrd immediately followed by a new SET notif
+            SWSS_LOG_WARN("Setting dhcp_rate_limit to alias:%s netdev failed with cmd:%s, rc:%d, error:%s", alias.c_str(), cmd_str.c_str(), ret, res.c_str());
+            return false;
+            }
     }
-    }
-    else
+    else if 
     {
         // tc qdisc del dev <port_name> handle ffff: ingress
         cmd << TC_CMD << " qdisc del dev " << shellquote(alias) << " handle ffff: ingress";
