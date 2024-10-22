7b6fc536551453092da459168c60a7a3afb5556c
Saikrishna Arcot <sarcot@microsoft.com>
2024-10-21 17:46:22 -0700

    Update tests to test LAG cleanup and to test with the new code
    This requires overriding some libc functions and capturing information
    about kill signals sent or intercepting file open operations.
    
    Signe -off-by: Saikrishna Arcot <sarcot@microsoft.com>

--- tests/mock_tests/teammgrd/teammgr_ut.cpp
+++ tests/mock_tests/teammgrd/teammgr_ut.cpp
@@ -1,20 +1,87 @@
 #include "gtest/gtest.h"
 #include "../mock_table.h"
 #include "teammgr.h"
+#include <dlfcn.h>
 
 extern int (*callback)(const std::string &cmd, std::string &stdout);
 extern std::vector<std::string> mockCallArgs;
+static std::vector< std::pair<pid_t, int> > mockKillCommands;
+static std::map<std::string, std::FILE*> pidFiles;
+
+static int (*callback_kill)(pid_t pid, int sig) = NULL;
+static FILE* (*callback_fopen)(const char *pathname, const char *mode) = NULL;
+
+static int cb_kill(pid_t pid, int sig)
+{
+    mockKillCommands.push_back(std::make_pair(pid, sig));
+    return 0;
+}
+
+int kill(pid_t pid, int sig)
+{
+    if (callback_kill) {
+        return callback_kill(pid, sig);
+    }
+    int (*realfunc)(pid_t, int) =
+        (int(*)(pid_t, int))(dlsym (RTLD_NEXT, "kill"));
+    return realfunc(pid, sig);
+}
+
+static FILE* cb_fopen(const char *pathname, const char *mode)
+{
+    auto pidFileSearch = pidFiles.find(pathname);
+    if (pidFileSearch != pidFiles.end()) {
+        return pidFileSearch->second;
+    } else {
+        return NULL;
+    }
+}
+
+FILE* fopen(const char *pathname, const char *mode)
+{
+    if (callback_fopen) {
+        FILE *fd = callback_fopen(pathname, mode);
+        if (fd) {
+            return fd;
+        }
+    }
+    FILE* (*realfunc)(const char *, const char *) =
+        (FILE*  (*)(const char *, const char *))(dlsym (RTLD_NEXT, "fopen"));
+    return realfunc(pathname, mode);
+}
+
+FILE* fopen64(const char *pathname, const char *mode)
+{
+    if (callback_fopen) {
+        FILE *fd = callback_fopen(pathname, mode);
+        if (fd) {
+            return fd;
+        }
+    }
+    FILE* (*realfunc)(const char *, const char *) =
+        (FILE*  (*)(const char *, const char *))(dlsym (RTLD_NEXT, "fopen64"));
+    return realfunc(pathname, mode);
+}
 
 int cb(const std::string &cmd, std::string &stdout)
 {
     mockCallArgs.push_back(cmd);
-    if (cmd.find("/usr/bin/teamd -r -t PortChannel1") != std::string::npos)
+    if (cmd.find("/usr/bin/teamd -r -t PortChannel382") != std::string::npos)
     {
+        mkdir("/var/run/teamd", 0755);
+        std::FILE* pidFile = std::tmpfile();
+        std::fputs("1234", pidFile);
+        std::rewind(pidFile);
+        pidFiles["/var/run/teamd/PortChannel382.pid"] = pidFile;
         return 1;
     }
-    else if (cmd.find("cat \"/var/run/teamd/PortChannel1.pid\"") != std::string::npos)
+    else if (cmd.find("/usr/bin/teamd -r -t PortChannel495") != std::string::npos)
     {
-        stdout = "1234";
+        mkdir("/var/run/teamd", 0755);
+        std::FILE* pidFile = std::tmpfile();
+        std::fputs("5678", pidFile);
+        std::rewind(pidFile);
+        pidFiles["/var/run/teamd/PortChannel495.pid"] = pidFile;
         return 0;
     }
     return 0;
@@ -53,7 +120,18 @@ namespace teammgr_ut
 
             cfg_lag_tables = tables;
             mockCallArgs.clear();
+            mockKillCommands.clear();
+            pidFiles.clear();
             callback = cb;
+            callback_kill = cb_kill;
+            callback_fopen = cb_fopen;
+        }
+
+        virtual void TearDown() override
+        {
+            callback = NULL;
+            callback_kill = NULL;
+            callback_fopen = NULL;
         }
     };
 
@@ -61,15 +139,49 @@ namespace teammgr_ut
     {
         swss::TeamMgr teammgr(m_config_db.get(), m_app_db.get(), m_state_db.get(), cfg_lag_tables);
         swss::Table cfg_lag_table = swss::Table(m_config_db.get(), CFG_LAG_TABLE_NAME);
-        cfg_lag_table.set("PortChannel1", { { "admin_status", "up" },
+        cfg_lag_table.set("PortChannel382", { { "admin_status", "up" },
                                             { "mtu", "9100" },
                                             { "lacp_key", "auto" },
                                             { "min_links", "2" } });
         teammgr.addExistingData(&cfg_lag_table);
         teammgr.doTask();
-        int kill_cmd_called = 0;
+        int exec_cmd_called = 0;
         for (auto cmd : mockCallArgs){
-            if (cmd.find("kill -TERM 1234") != std::string::npos){
+            if (cmd.find("/usr/bin/teamd -r -t PortChannel382") != std::string::npos) {
+                exec_cmd_called++;
+            }
+        }
+        ASSERT_EQ(exec_cmd_called, 1);
+        int kill_cmd_called = 0;
+        for (auto killedProcess : mockKillCommands) {
+            if (killedProcess.first == 1234) {
+                kill_cmd_called++;
+            }
+        }
+        ASSERT_EQ(kill_cmd_called, 1);
+    }
+
+    TEST_F(TeamMgrTest, testProcessCleanupAfterAddLag)
+    {
+        swss::TeamMgr teammgr(m_config_db.get(), m_app_db.get(), m_state_db.get(), cfg_lag_tables);
+        swss::Table cfg_lag_table = swss::Table(m_config_db.get(), CFG_LAG_TABLE_NAME);
+        cfg_lag_table.set("PortChannel495", { { "admin_status", "up" },
+                                            { "mtu", "9100" },
+                                            { "lacp_key", "auto" },
+                                            { "min_links", "2" } });
+        teammgr.addExistingData(&cfg_lag_table);
+        teammgr.doTask();
+        int exec_cmd_called = 0;
+        for (auto cmd : mockCallArgs){
+            if (cmd.find("/usr/bin/teamd -r -t PortChannel495") != std::string::npos) {
+                exec_cmd_called++;
+            }
+        }
+        ASSERT_EQ(exec_cmd_called, 1);
+        teammgr.cleanTeamProcesses();
+        int kill_cmd_called = 0;
+        for (auto killedProcess : mockKillCommands) {
+            if (killedProcess.first == 5678) {
                 kill_cmd_called++;
             }
         }
