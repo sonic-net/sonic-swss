1465f0556066808da97348c28223f2754f498d94
Abhishek Dosi <abdosi@microsoft.com>
2024-11-05 02:03:27 +0000

    Added support to compile and load code to read yaml file in  routesync.cpp. In routesyncd we read tag from constants.yml and take action of not to send route to appdb and also fallback to default route.
    Signed-off-by: Abhishek Dosi <abdosi@microsoft.com>

--- fpmsyncd/routesync.h
+++ fpmsyncd/routesync.h
@@ -77,6 +77,8 @@ private:
     ProducerStateTable  m_vnet_tunnelTable; 
     struct nl_cache    *m_link_cache;
     struct nl_sock     *m_nl_sock;
+    uint32_t route_tag_not_to_appdb;
+    uint32_t route_tag_fallback_to_default_route;
 
     bool                m_isSuppressionEnabled{false};
     FpmInterface*       m_fpmInterface {nullptr};
