86a3be9f66343d61bfe8eb493373cfbc0716e8d4
Yijiao Qin <y.qin@alibaba-inc.com>
2024-11-27 02:25:06 -0800

    add some cases

--- tests/mock_tests/fpmsyncd/test_routesync.cpp
+++ tests/mock_tests/fpmsyncd/test_routesync.cpp
@@ -16,6 +16,8 @@
 #include <sstream>
 
 using namespace swss;
+using namespace testing;
+
 #define MAX_PAYLOAD 1024
 
 using ::testing::_;
@@ -244,14 +246,11 @@ TEST_F(FpmSyncdResponseTest, testEvpn)
 
 }
 
-TEST_F(FpmSyncdResponseTest, testNewNextHopMsg)
-{
-    uint32_t test_id = 10;
-    const char* test_gateway = "192.168.1.1";
-    int32_t test_ifindex = 5;
+struct nlmsghdr* createNewNextHopMsgHdr(int32_t ifindex, const char* gateway, uint32_t id) {
 
     struct nlmsghdr *nlh = (struct nlmsghdr *)malloc(NLMSG_SPACE(MAX_PAYLOAD));
     memset(nlh, 0, NLMSG_SPACE(MAX_PAYLOAD));
+
     nlh->nlmsg_type = RTM_NEWNEXTHOP;
     nlh->nlmsg_flags = NLM_F_REQUEST | NLM_F_CREATE | NLM_F_REPLACE;
 
@@ -263,19 +262,107 @@ TEST_F(FpmSyncdResponseTest, testNewNextHopMsg)
     nhm->nh_family = AF_INET;
 
     // set NHA_ID
-    ut_fpmsyncd::nl_attr_put32(nlh, NLMSG_SPACE(MAX_PAYLOAD), NHA_ID, test_id);
+    ut_fpmsyncd::nl_attr_put32(nlh, NLMSG_SPACE(MAX_PAYLOAD), NHA_ID, id);
 
     // set NHA_GATEWAY
     struct rtattr *rta = reinterpret_cast<struct rtattr *>(static_cast<void *>(((char *)nlh) + NLMSG_ALIGN(nlh->nlmsg_len)));
     struct in_addr gw_addr;
-    inet_pton(AF_INET, test_gateway, &gw_addr);
+    inet_pton(AF_INET, gateway, &gw_addr);
     rta->rta_type = NHA_GATEWAY;
     rta->rta_len = RTA_LENGTH(sizeof(struct in_addr));
     memcpy(RTA_DATA(rta), &gw_addr, sizeof(struct in_addr));
     nlh->nlmsg_len = NLMSG_ALIGN(nlh->nlmsg_len) + RTA_ALIGN(rta->rta_len);
 
     // set NHA_OIF attribute
-    ut_fpmsyncd::nl_attr_put32(nlh, NLMSG_SPACE(MAX_PAYLOAD), NHA_OIF, test_ifindex);
+    ut_fpmsyncd::nl_attr_put32(nlh, NLMSG_SPACE(MAX_PAYLOAD), NHA_OIF, ifindex);
+    return nlh;
+}
+
+TEST_F(FpmSyncdResponseTest, TestNoNHAId)
+{
+    struct nlmsghdr *nlh = (struct nlmsghdr *)malloc(NLMSG_SPACE(MAX_PAYLOAD));
+    memset(nlh, 0, NLMSG_SPACE(MAX_PAYLOAD));
+
+    nlh->nlmsg_type = RTM_NEWNEXTHOP;
+    nlh->nlmsg_flags = NLM_F_REQUEST | NLM_F_CREATE | NLM_F_REPLACE;
+    nlh->nlmsg_len = NLMSG_LENGTH(sizeof(struct nhmsg));
+    struct nhmsg *nhm = (struct nhmsg *)NLMSG_DATA(nlh);
+    nhm->nh_family = AF_INET;
+
+    EXPECT_CALL(m_mockRouteSync, getIfName(_, _, _))
+    .Times(0);
+
+    m_mockRouteSync.RouteSync::onNextHopMsg(nlh, 0);
+
+    free(nlh);
+}
+
+// TEST_F(FpmSyncdResponseTest, TestSingleNextHopAdd)
+// {
+//     uint32_t test_id = 10;
+//     const char* test_gateway = "192.168.1.1";
+//     int32_t test_ifindex = 5;
+
+//     struct nlmsghdr* nlh = createNewNextHopMsgHdr(test_ifindex, test_gateway, test_id);
+//     int expected_length = (int)(nlh->nlmsg_len - NLMSG_LENGTH(sizeof(struct nhmsg)));
+
+//     EXPECT_CALL(m_mockRouteSync, getIfName(test_ifindex, _, _))
+//     .WillOnce(DoAll(
+//         [](int32_t, char* ifname, size_t size) {
+//             strncpy(ifname, "Ethernet1", size);
+//             ifname[size-1] = '\0';
+//         },
+//         Return(true)
+//     ));
+
+//     m_mockRouteSync.RouteSync::onNextHopMsg(nlh, expected_length);
+
+//     auto it = m_mockRouteSync.m_nh_groups.find(test_id);
+//     ASSERT_NE(it, m_mockRouteSync.m_nh_groups.end()) << "Failed to add new nexthop";
+
+//     free(nlh);
+// }
+
+TEST_F(FpmSyncdResponseTest, TestSkipSpecialInterfaces)
+{
+    uint32_t test_id = 11;
+    const char* test_gateway = "192.168.1.1";
+    int32_t test_ifindex = 6;
+
+    EXPECT_CALL(m_mockRouteSync, getIfName(test_ifindex, _, _))
+    .WillOnce(DoAll(
+        [](int32_t ifidx, char* ifname, size_t size) {
+            std::cout << "getIfName called with ifindex: " << ifidx << ", size: " << size << std::endl;
+            strncpy(ifname, "eth0", size);
+            std::cout << "ifname set to: " << ifname << std::endl;
+        },
+        Return(true)
+    ));
+
+    std::cout << "create nexthopmsg" << std::endl;
+    struct nlmsghdr* nlh = createNewNextHopMsgHdr(test_ifindex, test_gateway, test_id);
+    int expected_length = (int)(nlh->nlmsg_len - NLMSG_LENGTH(sizeof(struct nhmsg)));
+
+    std::cout << "nlh pointer: " << nlh << std::endl;
+    std::cout << "nlmsg_len: " << nlh->nlmsg_len << std::endl;
+    std::cout << "nlmsg_type: " << nlh->nlmsg_type << std::endl;
+    std::cout << "expected_length: " << expected_length << std::endl;
+
+    m_mockRouteSync.RouteSync::onNextHopMsg(nlh, expected_length);
+
+    auto it = m_mockRouteSync.m_nh_groups.find(test_id);
+    EXPECT_EQ(it, m_mockRouteSync.m_nh_groups.end()) << "Should skip eth0 interface";
+    
+    free(nlh);
+}
+
+TEST_F(FpmSyncdResponseTest, testNewNextHopMsg)
+{
+    uint32_t test_id = 10;
+    const char* test_gateway = "192.168.1.1";
+    int32_t test_ifindex = 5;
+
+    struct nlmsghdr* nlh = createNewNextHopMsgHdr(test_ifindex, test_gateway, test_id);
 
     int expected_length = (int)(nlh->nlmsg_len - NLMSG_LENGTH(sizeof(struct nhmsg)));
 
@@ -288,17 +375,5 @@ TEST_F(FpmSyncdResponseTest, testNewNextHopMsg)
         });
 
     m_mockRouteSync.onMsgRaw(nlh);
-
-    auto it = m_mockRouteSync.m_nh_groups.find(test_id);
-    if (it == m_mockRouteSync.m_nh_groups.end())
-    {
-        std::cout << "fail to add new nh" << std::endl;
-    } 
-    else
-        std::cout << it->second.id << it->second.nexthop << it->second.intf << std::endl;
-    // EXPECT_EQ(it->second.id, test_id);
-    // EXPECT_EQ(it->second.nexthop, "192.168.1.1");
-    // EXPECT_EQ(it->second.intf, "Ethernet0");
-
     free(nlh);
 }
