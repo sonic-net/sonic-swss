7915cd65663ff1ca81a3c1d646340a5acf4e6489
Joy <ejiaoq@gmail.com>
2024-12-04 22:59:00 +0000

    add testcases for deleteNextHopGroup updateNextHopGroup getNextHopGroupFields

--- tests/mock_tests/fpmsyncd/test_routesync.cpp
+++ tests/mock_tests/fpmsyncd/test_routesync.cpp
@@ -394,3 +394,174 @@ TEST_F(FpmSyncdResponseTest, TestSkipSpecialInterfaces)
 
     free(nlh);
 }
+
+TEST_F(FpmSyncdResponseTest, TestNextHopGroupKeyString)
+{
+    EXPECT_EQ(m_mockRouteSync.getNextHopGroupKeyAsString(1), "ID1");
+    EXPECT_EQ(m_mockRouteSync.getNextHopGroupKeyAsString(1234), "ID1234");
+}
+
+TEST_F(FpmSyncdResponseTest, TestGetNextHopGroupFields)
+{
+    // Test single next hop case
+    {
+        NextHopGroup nhg(1, "192.168.1.1", "Ethernet0");
+        m_mockRouteSync.m_nh_groups.insert({1, nhg});
+        
+        string nexthops, ifnames, weights;
+        m_mockRouteSync.getNextHopGroupFields(nhg, nexthops, ifnames, weights);
+        
+        EXPECT_EQ(nexthops, "192.168.1.1");
+        EXPECT_EQ(ifnames, "Ethernet0");
+        EXPECT_TRUE(weights.empty());
+    }
+
+    // Test multiple next hops with weights
+    {
+        // Create the component next hops first
+        NextHopGroup nhg1(1, "192.168.1.1", "Ethernet0");
+        NextHopGroup nhg2(2, "192.168.1.2", "Ethernet1");
+        m_mockRouteSync.m_nh_groups.insert({1, nhg1});
+        m_mockRouteSync.m_nh_groups.insert({2, nhg2});
+        
+        // Create the group with multiple next hops
+        vector<pair<uint32_t,uint8_t>> group_members;
+        group_members.push_back(make_pair(1, 1));  // id=1, weight=1
+        group_members.push_back(make_pair(2, 2));  // id=2, weight=2
+        
+        NextHopGroup nhg(3, group_members);
+        m_mockRouteSync.m_nh_groups.insert({3, nhg});
+        
+        string nexthops, ifnames, weights;
+        m_mockRouteSync.getNextHopGroupFields(nhg, nexthops, ifnames, weights);
+        
+        EXPECT_EQ(nexthops, "192.168.1.1,192.168.1.2");
+        EXPECT_EQ(ifnames, "Ethernet0,Ethernet1");
+        EXPECT_EQ(weights, "1,2");
+    }
+
+    // Test IPv6 default case
+    {
+        NextHopGroup nhg(4, "", "Ethernet0");
+        m_mockRouteSync.m_nh_groups.insert({4, nhg});
+        
+        string nexthops, ifnames, weights;
+        m_mockRouteSync.getNextHopGroupFields(nhg, nexthops, ifnames, weights, AF_INET6);
+        
+        EXPECT_EQ(nexthops, "::");
+        EXPECT_EQ(ifnames, "Ethernet0");
+        EXPECT_TRUE(weights.empty());
+    }
+
+     // Both empty
+    {
+        NextHopGroup nhg(5, "", "");
+        string nexthops, ifnames, weights;
+        m_mockRouteSync.getNextHopGroupFields(nhg, nexthops, ifnames, weights, AF_INET);
+        
+        EXPECT_EQ(nexthops, "0.0.0.0");
+        EXPECT_TRUE(ifnames.empty());
+        EXPECT_TRUE(weights.empty());
+    }
+}
+
+TEST_F(FpmSyncdResponseTest, TestUpdateNextHopGroupDb)
+{
+    Table nexthop_group_table(m_db.get(), APP_NEXTHOP_GROUP_TABLE_NAME);
+
+    // Test single next hop group
+    {
+        NextHopGroup nhg(1, "192.168.1.1", "Ethernet0");
+        m_mockRouteSync.updateNextHopGroupDb(nhg);
+
+        vector<FieldValueTuple> fieldValues;
+        nexthop_group_table.get("ID1", fieldValues);
+        
+        EXPECT_EQ(fieldValues.size(), 2);
+        EXPECT_EQ(fvField(fieldValues[0]), "nexthop");
+        EXPECT_EQ(fvValue(fieldValues[0]), "192.168.1.1");
+        EXPECT_EQ(fvField(fieldValues[1]), "ifname");
+        EXPECT_EQ(fvValue(fieldValues[1]), "Ethernet0");
+    }
+
+    // Test group with multiple next hops
+    {
+        vector<pair<uint32_t,uint8_t>> group_members;
+        group_members.push_back(make_pair(1, 1));
+        group_members.push_back(make_pair(2, 2));
+        
+        NextHopGroup nhg1(1, "192.168.1.1", "Ethernet0");
+        NextHopGroup nhg2(2, "192.168.1.2", "Ethernet1");
+        NextHopGroup group(3, group_members);
+        
+        m_mockRouteSync.m_nh_groups.insert({1, nhg1});
+        m_mockRouteSync.m_nh_groups.insert({2, nhg2});
+        m_mockRouteSync.m_nh_groups.insert({3, group});
+        
+        m_mockRouteSync.updateNextHopGroup(3);
+        
+        auto it = m_mockRouteSync.m_nh_groups.find(3);
+        ASSERT_NE(it, m_mockRouteSync.m_nh_groups.end());
+        EXPECT_TRUE(it->second.installed);
+        vector<FieldValueTuple> fieldValues;
+        nexthop_group_table.get("ID3", fieldValues);
+        EXPECT_EQ(fieldValues.size(), 3);
+        EXPECT_EQ(fvField(fieldValues[0]), "nexthop");
+        EXPECT_EQ(fvValue(fieldValues[0]), "192.168.1.1,192.168.1.2");
+        EXPECT_EQ(fvField(fieldValues[1]), "ifname");
+        EXPECT_EQ(fvValue(fieldValues[1]), "Ethernet0,Ethernet1");
+        EXPECT_EQ(fvField(fieldValues[2]), "weight");
+        EXPECT_EQ(fvValue(fieldValues[2]), "1,2");
+    }
+
+    // Empty nexthop (default route case)
+    {
+        NextHopGroup nhg(4, "", "Ethernet0");
+        m_mockRouteSync.updateNextHopGroupDb(nhg);
+        
+        vector<FieldValueTuple> fieldValues;
+        nexthop_group_table.get("ID4", fieldValues);
+        
+        EXPECT_EQ(fieldValues.size(), 2);
+        EXPECT_EQ(fvField(fieldValues[0]), "nexthop");
+        EXPECT_EQ(fvValue(fieldValues[0]), "0.0.0.0");
+        EXPECT_EQ(fvField(fieldValues[1]), "ifname");
+        EXPECT_EQ(fvValue(fieldValues[1]), "Ethernet0");
+    }
+
+    // Empty interface name
+    {
+        NextHopGroup nhg(5, "192.168.1.1", "");
+        m_mockRouteSync.updateNextHopGroupDb(nhg);
+        
+        vector<FieldValueTuple> fieldValues;
+        nexthop_group_table.get("ID5", fieldValues);
+        
+        EXPECT_EQ(fieldValues.size(), 2);
+        EXPECT_EQ(fvField(fieldValues[0]), "nexthop");
+        EXPECT_EQ(fvValue(fieldValues[0]), "192.168.1.1");
+        EXPECT_EQ(fvField(fieldValues[1]), "ifname");
+        EXPECT_EQ(fvValue(fieldValues[1]), "");
+    }
+}
+
+TEST_F(FpmSyncdResponseTest, TestDeleteNextHopGroup)
+{
+    // Setup test groups
+    NextHopGroup nhg1(1, "192.168.1.1", "Ethernet0");
+    NextHopGroup nhg2(2, "192.168.1.2", "Ethernet1");
+    nhg1.installed = true;
+    nhg2.installed = true;
+    
+    m_mockRouteSync.m_nh_groups.insert({1, nhg1});
+    m_mockRouteSync.m_nh_groups.insert({2, nhg2});
+    
+    // Test deletion
+    m_mockRouteSync.deleteNextHopGroup(1);
+    EXPECT_EQ(m_mockRouteSync.m_nh_groups.find(1), m_mockRouteSync.m_nh_groups.end());
+    EXPECT_NE(m_mockRouteSync.m_nh_groups.find(2), m_mockRouteSync.m_nh_groups.end());
+
+    // Test deleting non-existent group
+    m_mockRouteSync.deleteNextHopGroup(999);
+    EXPECT_EQ(m_mockRouteSync.m_nh_groups.find(999), m_mockRouteSync.m_nh_groups.end());
+}
