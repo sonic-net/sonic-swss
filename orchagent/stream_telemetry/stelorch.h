494af39870cb0d781a3c80716e993920147ec17e
Ze Gan <ganze718@gmail.com>
2024-12-04 17:00:36 +0800

    Add state machine
    Signed-off-by: Ze Gan <ganze718@gmail.com>

--- orchagent/stream_telemetry/stelorch.h
+++ orchagent/stream_telemetry/stelorch.h
@@ -16,6 +16,8 @@
 #define CFG_STREAM_TELEMETRY_GROUP_TABLE_NAME "STREAM_TELEMETRY_GROUP"
 #define STREAM_TELEMETRY_SESSION "STREAM_TELEMETRY_SESSION"
 
+#define SAI_SWITCH_NOTIFICATION_NAME_TAM_TEL_TYPE_CONFIG_CHANGE "tam_tel_type_config_change"
+
 class STelOrch : public Orch
 {
 public:
@@ -34,19 +36,21 @@ public:
     void locallyNotify(const CounterNameMapUpdater::Message &msg);
 
 private:
-    swss::Table m_cfg_stream_telemetry_group;
     swss::Table m_state_telemetry_session;
+    swss::DBConnector m_asic_db;
+    std::shared_ptr<swss::NotificationConsumer> m_asic_notification_consumer;
 
     std::unordered_map<std::string, std::shared_ptr<STelProfile>> m_name_profile_mapping;
     std::unordered_map<sai_object_type_t, std::unordered_set<std::shared_ptr<STelProfile>>> m_type_profile_mapping;
     CounterNameCache m_counter_name_cache;
 
-    void profileTableSet(const std::string &profile_name, const std::vector<swss::FieldValueTuple> &values);
-    void profileTableDel(const std::string &profile_name);
-    void groupTableSet(const std::string &profile_name, const std::string &group_name, const std::vector<swss::FieldValueTuple> &values);
-    void groupTableDel(const std::string &profile_name, const std::string &group_name);
+    task_process_status profileTableSet(const std::string &profile_name, const std::vector<swss::FieldValueTuple> &values);
+    task_process_status profileTableDel(const std::string &profile_name);
+    task_process_status groupTableSet(const std::string &profile_name, const std::string &group_name, const std::vector<swss::FieldValueTuple> &values);
+    task_process_status groupTableDel(const std::string &profile_name, const std::string &group_name);
     std::shared_ptr<STelProfile> getProfile(const std::string &profile_name);
 
+    void doTask(swss::NotificationConsumer &consumer);
     void doTask(Consumer &consumer);
 
     // SAI objects
