5ac86141edc1f3b39c500a440d12f052671e747d
Muhammad Ali Hussnain <91005947+muhammadalihussnain@users.noreply.github.com>
2025-01-03 10:31:53 +0000

    reverted to pvst code

--- cfgmgr/stpmgr.h
+++ cfgmgr/stpmgr.h
@@ -219,45 +219,45 @@ typedef struct STP_VLAN_MEM_CONFIG_MSG {
 // } __attribute__((packed)) STP_MST_PORT_CONFIG_MSG;
 
 
-typedef struct STP_MST_GLOBAL_CONFIG_MSG {
-    uint8_t     opcode; // enable/disable
-    uint16_t    revision_number;
-    char        name[STP_SYNC_MSTP_NAME_LEN];
-    int         forward_delay;
-    int         hello_time;
-    int         max_age;
-    int         max_hop;
-}_attribute_ ((packed))STP_MST_GLOBAL_CONFIG_MSG;
+// typedef struct STP_MST_GLOBAL_CONFIG_MSG {
+//     uint8_t     opcode; // enable/disable
+//     uint16_t    revision_number;
+//     char        name[STP_SYNC_MSTP_NAME_LEN];
+//     int         forward_delay;
+//     int         hello_time;
+//     int         max_age;
+//     int         max_hop;
+// }_attribute_ ((packed))STP_MST_GLOBAL_CONFIG_MSG;
 
-typedef struct VLAN_LIST{
-    uint16_t    vlan_id;
-} _attribute_ ((packed))VLAN_LIST;
+// typedef struct VLAN_LIST{
+//     uint16_t    vlan_id;
+// } _attribute_ ((packed))VLAN_LIST;
 
-typedef struct MST_INST_CONFIG_MSG {
-    uint8_t     opcode; // enable/disable
-    uint16_t    mst_id;
-    int         priority;
-    uint16_t    vlan_count;
-    VLAN_LIST   vlan_list[0];
-}_attribute_ ((packed))MST_INST_CONFIG_MSG;
+// typedef struct MST_INST_CONFIG_MSG {
+//     uint8_t     opcode; // enable/disable
+//     uint16_t    mst_id;
+//     int         priority;
+//     uint16_t    vlan_count;
+//     VLAN_LIST   vlan_list[0];
+// }_attribute_ ((packed))MST_INST_CONFIG_MSG;
 
-typedef struct STP_MST_INSTANCE_CONFIG_MSG {
-    uint8_t    mst_count;
-    MST_INST_CONFIG_MSG mst_list[0];
-}_attribute_ ((packed))STP_MST_INSTANCE_CONFIG_MSG; 
+// typedef struct STP_MST_INSTANCE_CONFIG_MSG {
+//     uint8_t    mst_count;
+//     MST_INST_CONFIG_MSG mst_list[0];
+// }_attribute_ ((packed))STP_MST_INSTANCE_CONFIG_MSG; 
 
-typedef struct STP_MST_INST_PORT_CONFIG_MSG {
-    uint8_t     opcode; // enable/disable
-    char        intf_name[STP_IFALIASZ];
-    uint16_t    mst_id;
-    int         path_cost;
-    int         priority;
-}_attribute_ ((packed))STP_MST_INST_PORT_CONFIG_MSG;
+// typedef struct STP_MST_INST_PORT_CONFIG_MSG {
+//     uint8_t     opcode; // enable/disable
+//     char        intf_name[STP_IFALIASZ];
+//     uint16_t    mst_id;
+//     int         path_cost;
+//     int         priority;
+// }_attribute_ ((packed))STP_MST_INST_PORT_CONFIG_MSG;
 
-typedef struct PORT_LIST{
-    char        intf_name[STP_IFALIASZ];
-    int8_t      tagging_mode;
-}PORT_LIST;
+// typedef struct PORT_LIST{
+//     char        intf_name[STP_IFALIASZ];
+//     int8_t      tagging_mode;
+// }PORT_LIST;
 
 namespace swss {
 
