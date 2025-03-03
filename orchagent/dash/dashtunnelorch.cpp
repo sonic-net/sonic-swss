ca3d88c9a45f104ea755f47b987b027a70032d7f
Lawrence Lee <lawlee@microsoft.com>
2025-03-03 08:47:38 +0000

    fix bulker creation
    Signed-off-by: Lawrence Lee <lawlee@microsoft.com>

--- orchagent/dash/dashtunnelorch.cpp
+++ orchagent/dash/dashtunnelorch.cpp
@@ -16,9 +16,9 @@ DashTunnelOrch::DashTunnelOrch(
     swss::DBConnector *db,
     std::vector<std::string> &tables,
     swss::ZmqServer *zmqServer) :
-    tunnel_bulker_(sai_dash_tunnel_api, gMaxBulkSize, SAI_OBJECT_TYPE_DASH_TUNNEL),
-    tunnel_member_bulker_(sai_dash_tunnel_api, gMaxBulkSize, SAI_OBJECT_TYPE_DASH_TUNNEL_MEMBER),
-    tunnel_nhop_bulker_(sai_dash_tunnel_api, gMaxBulkSize, SAI_OBJECT_TYPE_DASH_TUNNEL_NEXT_HOP),
+    tunnel_bulker_(sai_dash_tunnel_api, gSwitchId, gMaxBulkSize, SAI_OBJECT_TYPE_DASH_TUNNEL),
+    tunnel_member_bulker_(sai_dash_tunnel_api, gSwitchId, gMaxBulkSize, SAI_OBJECT_TYPE_DASH_TUNNEL_MEMBER),
+    tunnel_nhop_bulker_(sai_dash_tunnel_api, gSwitchId, gMaxBulkSize, SAI_OBJECT_TYPE_DASH_TUNNEL_NEXT_HOP),
     ZmqOrch(db, tables, zmqServer)
 {
     SWSS_LOG_ENTER();
