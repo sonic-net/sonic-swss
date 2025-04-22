634697eeb56329f4cb48db7ff12ccf3294e9f33d
liuh-80 <liuh@microsoft.com>
2025-04-22 09:15:13 +0000

    Add test case

---
+++ tests/mock_tests/zmq_orch_ut.cpp
@@ -0,0 +1,32 @@
+#define private public
+#include "directory.h"
+#undef private
+#define protected public
+#include "orch.h"
+#include "zmqorch.h"
+#undef protected
+#include "ut_helper.h"
+#include "gtest/gtest.h"
+#include <string>
+
+EXTERN_MOCK_FNS
+
+namespace zmq_orch_test
+{
+    using namespace mock_orch_test;
+    class DashOrchTest : public MockDashOrchTest {};
+
+    TEST_F(ZmqOrchTest, CreateZmqOrchWitTableNames)
+    {   
+        vector<table_name_with_pri_t> tables = {
+            { "TABLE_1", 1 },
+            { "TABLE_2", 2 },
+            { "TABLE_3", 3 }
+        };
+    
+        auto app_db = make_shared<swss::DBConnector>("APPL_DB", 0);
+        auto zmq_orch = make_shared<ZmqOrch>(app_db.get(), tables, nullptr);
+        
+        EXPECT_EQ(zmq_orch->getSelectables().size(), tables.size());
+    }
+}
