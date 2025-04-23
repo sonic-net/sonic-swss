12d2d00ff8b804b27510012f0150cb1c8f7b2094
Hua Liu <58683130+liuh-80@users.noreply.github.com>
2025-04-23 10:22:31 +0800

    Update zmq_orch_ut.cpp

--- tests/mock_tests/zmq_orch_ut.cpp
+++ tests/mock_tests/zmq_orch_ut.cpp
@@ -9,22 +9,18 @@
 #include "gtest/gtest.h"
 #include <string>
 
-namespace zmq_orch_test
-{
-    using namespace mock_orch_test;
-    class DashOrchTest : public MockDashOrchTest {};
+using namespace std;
 
-    TEST_F(ZmqOrchTest, CreateZmqOrchWitTableNames)
-    {   
-        vector<table_name_with_pri_t> tables = {
-            { "TABLE_1", 1 },
-            { "TABLE_2", 2 },
-            { "TABLE_3", 3 }
-        };
+TEST_F(ZmqOrchTest, CreateZmqOrchWitTableNames)
+{   
+    vector<table_name_with_pri_t> tables = {
+        { "TABLE_1", 1 },
+        { "TABLE_2", 2 },
+        { "TABLE_3", 3 }
+    };
+
+    auto app_db = make_shared<swss::DBConnector>("APPL_DB", 0);
+    auto zmq_orch = make_shared<ZmqOrch>(app_db.get(), tables, nullptr);
     
-        auto app_db = make_shared<swss::DBConnector>("APPL_DB", 0);
-        auto zmq_orch = make_shared<ZmqOrch>(app_db.get(), tables, nullptr);
-        
-        EXPECT_EQ(zmq_orch->getSelectables().size(), tables.size());
-    }
+    EXPECT_EQ(zmq_orch->getSelectables().size(), tables.size());
 }
