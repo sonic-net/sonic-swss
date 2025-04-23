c54bc6325473ea32264fc620ca865e9fb3152154
Hua Liu <58683130+liuh-80@users.noreply.github.com>
2025-04-23 10:58:13 +0800

    Update zmq_orch_ut.cpp

--- tests/mock_tests/zmq_orch_ut.cpp
+++ tests/mock_tests/zmq_orch_ut.cpp
@@ -1,17 +1,16 @@
-#define private public
-#include "directory.h"
-#undef private
+#include "gtest/gtest.h"
+#include <string>
+#include "schema.h"
+#include "ut_helper.h"
+
 #define protected public
 #include "orch.h"
 #include "zmqorch.h"
 #undef protected
-#include "ut_helper.h"
-#include "gtest/gtest.h"
-#include <string>
 
 using namespace std;
 
-TEST_F(ZmqOrchTest, CreateZmqOrchWitTableNames)
+TEST(ZmqOrchTest, CreateZmqOrchWitTableNames)
 {   
     vector<table_name_with_pri_t> tables = {
         { "TABLE_1", 1 },
