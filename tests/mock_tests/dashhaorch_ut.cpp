ffc0104731aef93ea995a24cb85a976fa7210885
Jing Zhang <zhangjing@microsoft.com>
2025-05-01 08:25:19 +0000

    even more coverage

--- tests/mock_tests/dashhaorch_ut.cpp
+++ tests/mock_tests/dashhaorch_ut.cpp
@@ -41,7 +41,8 @@ namespace dashhaorch_ut
             swss::IpAddress vip_v6("::1");
             swss::IpAddress npu_ip("2.2.2.2");
             swss::IpAddress local_ip("3.3.3.3");
-            swss::IpAddress peer_ip("4.4.4.4");
+            // swss::IpAddress peer_ip("4.4.4.4");
+            swss::IpAddress peer_ip("::2");
 
             ha_set.set_version("1");
             ha_set.set_scope(dash::types::SCOPE_DPU);
@@ -49,7 +50,8 @@ namespace dashhaorch_ut
             ha_set.mutable_vip_v6()->set_ipv6(reinterpret_cast<const char*>(vip_v6.getV6Addr()));
             ha_set.mutable_local_npu_ip()->set_ipv4(npu_ip.getV4Addr());
             ha_set.mutable_local_ip()->set_ipv4(local_ip.getV4Addr());
-            ha_set.mutable_peer_ip()->set_ipv4(peer_ip.getV4Addr());
+            // ha_set.mutable_peer_ip()->set_ipv4(peer_ip.getV4Addr());
+            ha_set.mutable_peer_ip()->set_ipv6(reinterpret_cast<const char*>(peer_ip.getV6Addr()));
             ha_set.set_cp_data_channel_port(100);
             ha_set.set_dp_channel_dst_port(200);
             ha_set.set_dp_channel_src_port_min(0);
@@ -217,6 +219,98 @@ namespace dashhaorch_ut
             );
             static_cast<Orch *>(m_dashHaOrch)->doTask(*consumer.get());
         }
+
+        void RandomTable()
+        {
+            auto consumer = unique_ptr<Consumer>(new Consumer(
+                new swss::ConsumerStateTable(m_dpu_app_db.get(), "RANDOM_TABLE", 1, 1),
+                m_dashHaOrch, "RANDOM_TABLE"));
+
+            consumer->addToSync(
+                deque<KeyOpFieldsValuesTuple>(
+                    {
+                        {
+                            "random_key",
+                            SET_COMMAND,
+                            {
+                                { "pb", "random" }
+                            }
+                        }
+                    }
+                )
+            );
+            static_cast<Orch *>(m_dashHaOrch)->doTask(*consumer.get());
+        }
+
+        void InvalidHaScopePbString()
+        {
+            auto consumer = unique_ptr<Consumer>(new Consumer(
+                new swss::ConsumerStateTable(m_dpu_app_db.get(), APP_DASH_HA_SCOPE_TABLE_NAME, 1, 1),
+                m_dashHaOrch, APP_DASH_HA_SCOPE_TABLE_NAME));
+
+            consumer->addToSync(
+                deque<KeyOpFieldsValuesTuple>(
+                    {
+                        {
+                            "HA_SET_1",
+                            SET_COMMAND,
+                            {
+                                { "pb", "invalid" }
+                            }
+                        }
+                    }
+                )
+            );
+            static_cast<Orch *>(m_dashHaOrch)->doTask(*consumer.get());
+        }
+
+        void InvalidHaSetPbString()
+        {
+            auto consumer = unique_ptr<Consumer>(new Consumer(
+                new swss::ConsumerStateTable(m_dpu_app_db.get(), APP_DASH_HA_SET_TABLE_NAME, 1, 1),
+                m_dashHaOrch, APP_DASH_HA_SET_TABLE_NAME));
+
+            consumer->addToSync(
+                deque<KeyOpFieldsValuesTuple>(
+                    {
+                        {
+                            "HA_SET_1",
+                            SET_COMMAND,
+                            {
+                                { "pb", "invalid" }
+                            }
+                        }
+                    }
+                )
+            );
+            static_cast<Orch *>(m_dashHaOrch)->doTask(*consumer.get());
+        }
+
+        void HaSetScopeUnspecified()
+        {
+            auto consumer = unique_ptr<Consumer>(new Consumer(
+                new swss::ConsumerStateTable(m_dpu_app_db.get(), APP_DASH_HA_SET_TABLE_NAME, 1, 1),
+                m_dashHaOrch, APP_DASH_HA_SET_TABLE_NAME));
+
+            dash::ha_set::HaSet ha_set = dash::ha_set::HaSet();
+            ha_set.set_version("1");
+            ha_set.set_scope(dash::types::SCOPE_UNSPECIFIED);
+
+            consumer->addToSync(
+                deque<KeyOpFieldsValuesTuple>(
+                    {
+                        {
+                            "HA_SET_1",
+                            SET_COMMAND,
+                            {
+                                { "pb", ha_set.SerializeAsString() }
+                            }
+                        }
+                    }
+                )
+            );
+            static_cast<Orch *>(m_dashHaOrch)->doTask(*consumer.get());
+        }
     };
 
     TEST_F(DashHaOrchTest, AddRemoveHaSet)
@@ -260,6 +354,11 @@ namespace dashhaorch_ut
 
         CreateHaScope();
 
+        // HA Scope already exists
+        EXPECT_CALL(*mock_sai_dash_ha_api, create_ha_scope)
+        .Times(0);
+        CreateHaScope();
+
         EXPECT_CALL(*mock_sai_dash_ha_api, remove_ha_scope)
         .Times(1)
         .WillOnce(Return(SAI_STATUS_SUCCESS));
@@ -278,6 +377,11 @@ namespace dashhaorch_ut
         .Times(0);
 
         RemoveHaScope();
+
+        EXPECT_CALL(*mock_sai_dash_ha_api, remove_ha_set)
+        .Times(0);
+
+        RemoveHaSet();
     }
 
     TEST_F(DashHaOrchTest, SetHaScopeHaRole)
@@ -324,5 +428,17 @@ namespace dashhaorch_ut
 
         RemoveHaScope();
         RemoveHaSet();
+    }
+
+    TEST_F(DashHaOrchTest, InvalidInput)
+    {
+        RandomTable();
+        InvalidHaScopePbString();
+        InvalidHaSetPbString();
+
+        EXPECT_EQ(m_dashHaOrch->getHaScopeEntries().find("HA_SET_1"), m_dashHaOrch->getHaScopeEntries().end());
+        EXPECT_EQ(m_dashHaOrch->getHaSetEntries().find("HA_SET_1"), m_dashHaOrch->getHaSetEntries().end());
+
+        HaSetScopeUnspecified();
     }
 }
