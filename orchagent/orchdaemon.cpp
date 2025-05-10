b5d1f660bee9c1ee6621e0d92cbe81f8d0eac1af
Hua Liu <58683130+liuh-80@users.noreply.github.com>
2025-05-10 17:27:14 +0800

    Update orchdaemon.cpp

--- orchagent/orchdaemon.cpp
+++ orchagent/orchdaemon.cpp
@@ -277,7 +277,7 @@ bool OrchDaemon::init()
         { CFG_FG_NHG_MEMBER,          fgnhgorch_pri }
     };
 
-    gFgNhgOrch = new FgNhgOrch(m_configDb, m_applDb, m_stateDb, fgnhg_tables, gNeighOrch, gIntfsOrch, vrf_orch, enable_route_zmq);
+    gFgNhgOrch = new FgNhgOrch(m_configDb, m_applDb, m_stateDb, fgnhg_tables, gNeighOrch, gIntfsOrch, vrf_orch, false);
     gDirectory.set(gFgNhgOrch);
 
     TableConnector srv6_sid_list_table(m_applDb, APP_SRV6_SID_LIST_TABLE_NAME);
@@ -300,7 +300,7 @@ bool OrchDaemon::init()
         { APP_ROUTE_TABLE_NAME,        routeorch_pri },
         { APP_LABEL_ROUTE_TABLE_NAME,  routeorch_pri }
     };
-    gRouteOrch = new RouteOrch(m_applDb, route_tables, gSwitchOrch, gNeighOrch, gIntfsOrch, vrf_orch, gFgNhgOrch, gSrv6Orch, enable_route_zmq ? m_zmqServer : nullptr);
+    gRouteOrch = new RouteOrch(m_applDb, route_tables, gSwitchOrch, gNeighOrch, gIntfsOrch, vrf_orch, gFgNhgOrch, gSrv6Orch, nullptr);
     gNhgOrch = new NhgOrch(m_applDb, APP_NEXTHOP_GROUP_TABLE_NAME);
     gCbfNhgOrch = new CbfNhgOrch(m_applDb, APP_CLASS_BASED_NEXT_HOP_GROUP_TABLE_NAME);
 
