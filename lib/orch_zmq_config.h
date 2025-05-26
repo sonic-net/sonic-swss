6483a84c5fecaf28ca11523733555b243b4be7f0
Hua Liu <58683130+liuh-80@users.noreply.github.com>
2025-05-26 15:41:35 +0800

    Update orch_zmq_config.h

--- lib/orch_zmq_config.h
+++ lib/orch_zmq_config.h
@@ -1,12 +1,13 @@
 #ifndef SWSS_ORCH_ZMQ_CONFIG_H
 #define SWSS_ORCH_ZMQ_CONFIG_H
 
+#include <memory>
 #include <string.h>
 #include <set>
 
 #include "dbconnector.h"
 #include "zmqclient.h"
-#include "zmqserver.h"
+#include "zmqproducerstatetable.h"
 
 /*
  * swssconfig will only connect to local orchagent ZMQ endpoint.
@@ -17,14 +18,15 @@ namespace swss {
 
 std::set<std::string> load_zmq_tables();
 
-int get_zmq_port();
-
-std::shared_ptr<ZmqClient> create_zmq_client(std::string zmq_address, std::string vrf="");
-
-std::shared_ptr<ZmqServer> create_zmq_server(std::string zmq_address, std::string vrf="");
+std::shared_ptr<ZmqClient> create_zmq_client(std::string zmq_address);
 
 bool get_feature_status(std::string feature, bool default_value);
 
+std::shared_ptr<swss::ZmqClient> create_zmq_client(std::string feature, bool default_value);
+
+std::shared_ptr<swss::ProducerStateTable> createProducerStateTable(DBConnector *db, const std::string &tableName, std::shared_ptr<swss::ZmqClient> zmqClient);
+
+std::shared_ptr<swss::ProducerStateTable> createProducerStateTable(RedisPipeline *pipeline, const std::string &tableName, bool buffered, std::shared_ptr<swss::ZmqClient> zmqClient);
 }
 
 #endif /* SWSS_ORCH_ZMQ_CONFIG_H */
