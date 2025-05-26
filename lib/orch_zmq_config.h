a45c1be5cf6b2ec958fb649b2fb2494d1f7e1d52
Hua Liu <58683130+liuh-80@users.noreply.github.com>
2025-05-26 15:49:39 +0800

    Update orch_zmq_config.h

--- lib/orch_zmq_config.h
+++ lib/orch_zmq_config.h
@@ -7,18 +7,22 @@
 
 #include "dbconnector.h"
 #include "zmqclient.h"
+#include "zmqserver.h"
 #include "zmqproducerstatetable.h"
 
 /*
  * swssconfig will only connect to local orchagent ZMQ endpoint.
  */
 #define ZMQ_LOCAL_ADDRESS               "tcp://localhost"
-
 namespace swss {
 
 std::set<std::string> load_zmq_tables();
 
-std::shared_ptr<ZmqClient> create_zmq_client(std::string zmq_address);
+int get_zmq_port();
+
+std::shared_ptr<ZmqClient> create_zmq_client(std::string zmq_address, std::string vrf="");
+
+std::shared_ptr<ZmqServer> create_zmq_server(std::string zmq_address, std::string vrf="");
 
 bool get_feature_status(std::string feature, bool default_value);
 
