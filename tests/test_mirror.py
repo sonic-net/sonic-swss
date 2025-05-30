c9f0e41ab0997c2ece5e3afb83cc27e9e3e8bc74
Carmine Scarpitta <cscarpit@cisco.com>
2025-05-30 16:47:35 -0500

    Remove pytest skip marks

--- tests/test_mirror.py
+++ tests/test_mirror.py
@@ -779,7 +779,6 @@ class TestMirror(object):
         # remove mirror session
         self.remove_mirror_session(session)
 
-    @pytest.mark.skip(reason="This test is failing consistently")
     def test_MirrorDestMoveLag(self, dvs, testlog):
         self.setup_db(dvs)
 
@@ -820,7 +819,6 @@ class TestMirror(object):
         tbl._del(table + "|" + rule)
         time.sleep(1)
 
-    @pytest.mark.skip(reason="This test is failing consistently")
     def test_AclBindMirrorPerStage(self, dvs, testlog):
         """
         This test configures mirror rules with specifying explicitely
@@ -989,7 +987,6 @@ class TestMirror(object):
         self.remove_ip_address("Ethernet32", "20.0.0.0/31")
         self.set_interface_status(dvs, "Ethernet32", "down")
 
-    @pytest.mark.skip(reason="This test is failing consistently")
     def test_AclBindMirror(self, dvs, testlog):
         self.setup_db(dvs)
 
