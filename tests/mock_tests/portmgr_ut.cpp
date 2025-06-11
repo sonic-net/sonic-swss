2a5c3b24af908772119f919cfa00bca886064377
Muhammad Ali Hussnain <91005947+muhammadalihussnain@users.noreply.github.com>
2025-06-11 10:13:37 +0000

    test

--- tests/mock_tests/portmgr_ut.cpp
+++ tests/mock_tests/portmgr_ut.cpp
@@ -267,9 +267,9 @@ namespace portmgr_ut
         value_opt = swss::fvsGetValue(values, "admin_status", true);
         ASSERT_TRUE(value_opt);
         ASSERT_EQ(DEFAULT_ADMIN_STATUS_STR, value_opt.get());
-        value_opt = swss::fvsGetValue(values, "dhcp_rate_limit", true);
-        //ASSERT_TRUE(value_opt);
-        ASSERT_EQ(DEFAULT_DHCP_RATE_LIMIT_STR, value_opt.get());
+        // value_opt = swss::fvsGetValue(values, "dhcp_rate_limit", true);
+        // //ASSERT_TRUE(value_opt);
+        // ASSERT_EQ(DEFAULT_DHCP_RATE_LIMIT_STR, value_opt.get());
         value_opt = swss::fvsGetValue(values, "speed", true);
         ASSERT_TRUE(value_opt);
         ASSERT_EQ("100000", value_opt.get());
