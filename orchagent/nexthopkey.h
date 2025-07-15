5000af914168682857c34f2fbd9b984530e2580f
sonicbld <sonicbld@microsoft.com>
2025-07-15 02:45:54 +0000

    Support boost hash

--- orchagent/nexthopkey.h
+++ orchagent/nexthopkey.h
@@ -210,25 +210,20 @@ struct NextHopKey
     }
 };
 
-namespace std {
-    template <>
-    struct hash<NextHopKey> {
-        size_t operator()(const NextHopKey& obj) const {
-            size_t nh_hash = 0;
+std::size_t hash_value(const NextHopKey& obj) {
+    std::size_t nh_hash = 0;
 
-            boost::hash_combine(nh_hash, obj.ip_address.to_string());
-            boost::hash_combine(nh_hash, obj.alias);
-            boost::hash_combine(nh_hash, obj.vni);
-            boost::hash_combine(nh_hash, obj.mac_address.to_string());
-            boost::hash_combine(nh_hash, obj.label_stack.to_string());
-            boost::hash_combine(nh_hash, obj.weight);
-            boost::hash_combine(nh_hash, obj.srv6_segment);
-            boost::hash_combine(nh_hash, obj.srv6_source);
-            boost::hash_combine(nh_hash, obj.srv6_vpn_sid);
+    boost::hash_combine(nh_hash, obj.ip_address.to_string());
+    boost::hash_combine(nh_hash, obj.alias);
+    boost::hash_combine(nh_hash, obj.vni);
+    boost::hash_combine(nh_hash, obj.mac_address.to_string());
+    boost::hash_combine(nh_hash, obj.label_stack.to_string());
+    boost::hash_combine(nh_hash, obj.weight);
+    boost::hash_combine(nh_hash, obj.srv6_segment);
+    boost::hash_combine(nh_hash, obj.srv6_source);
+    boost::hash_combine(nh_hash, obj.srv6_vpn_sid);
 
-            return nh_hash;
-        }
-    };
+    return nh_hash;
 }
 
 #endif /* SWSS_NEXTHOPKEY_H */
