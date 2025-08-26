d409c662e7210e36947d56ae2fa0856f7b5fd4f3
Muhammad Ali Hussnain <91005947+muhammadalihussnain@users.noreply.github.com>
2025-08-26 06:50:16 +0000

    test

--- cfgmgr/portmgr.cpp
+++ cfgmgr/portmgr.cpp
@@ -291,7 +291,7 @@ void PortMgr::doTask(Consumer &consumer)
                 SWSS_LOG_NOTICE("Configure %s admin status to %s", alias.c_str(), admin_status.c_str());
             }
 
-            if (!dhcp_rate_limit.empty)
+            if (!dhcp_rate_limit.empty())
             {
                 setPortDHCPMitigationRate(alias, dhcp_rate_limit);
                 SWSS_LOG_NOTICE("Configure %s DHCP rate limit to %s", alias.c_str(), dhcp_rate_limit.c_str());
