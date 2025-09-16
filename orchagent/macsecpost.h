e4b0676868ad13f2dcfe0b7b1234a2a9038a030c
syuan <syuan@arista.com>
2025-09-16 20:59:39 +0000

    Remove copyright.

--- orchagent/macsecpost.h
+++ orchagent/macsecpost.h
@@ -1,6 +1,3 @@
-// Copyright (c) 2025 Arista Networks, Inc.  All rights reserved.
-// Arista Networks, Inc. Confidential and Proprietary.
-
 #ifndef ORCHAGENT_MACSECPOST_H
 #define ORCHAGENT_MACSECPOST_H
 
