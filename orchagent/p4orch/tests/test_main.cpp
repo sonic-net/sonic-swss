86aa4284cdd406f11cf5294ec966ec61f2513730
mint570 <runmingwu@google.com>
2025-10-03 10:36:47 +0000

    [P4Orch] Add l3 multicast support in p4orch.

--- orchagent/p4orch/tests/test_main.cpp
+++ orchagent/p4orch/tests/test_main.cpp
@@ -77,6 +77,7 @@ sai_tunnel_api_t *sai_tunnel_api;
 sai_my_mac_api_t *sai_my_mac_api;
 sai_counter_api_t *sai_counter_api;
 sai_ipmc_api_t* sai_ipmc_api;
+sai_ipmc_group_api_t* sai_ipmc_group_api;
 sai_rpf_group_api_t* sai_rpf_group_api;
 sai_bridge_api_t* sai_bridge_api;
 sai_generic_programmable_api_t *sai_generic_programmable_api;
@@ -205,6 +206,7 @@ int main(int argc, char *argv[])
     sai_tunnel_api_t tunnel_api;
     sai_counter_api_t counter_api;
     sai_ipmc_api_t ipmc_api;
+    sai_ipmc_group_api_t ipmc_group_api;
     sai_rpf_group_api_t rpf_group_api;
     sai_bridge_api_t bridge_api;
     sai_generic_programmable_api_t generic_programmable_api;
@@ -225,6 +227,7 @@ int main(int argc, char *argv[])
     sai_tunnel_api = &tunnel_api;
     sai_counter_api = &counter_api;
     sai_ipmc_api = &ipmc_api;
+    sai_ipmc_group_api = &ipmc_group_api;
     sai_rpf_group_api = &rpf_group_api;
     sai_bridge_api = &bridge_api;
     sai_generic_programmable_api = &generic_programmable_api;
