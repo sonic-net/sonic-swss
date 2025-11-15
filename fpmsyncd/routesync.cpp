efbf82f140b6163eb06f8700dc52551af4facb7a
Venkit Kasiviswanathan <venkit@nexthop.ai>
2025-11-15 05:33:23 +0000

    Fix breakage for smartswitch usecase
    Refer to https://github.com/sonic-net/sonic-buildimage/issues/19638
    The previous PR (https://github.com/sonic-net/sonic-swss/pull/3979)
    inadvertantly caused the above issue.
    This commit fixes that by connecting to zmq from fpmsyncd using the mgmt
    vrf as well (if one is configured)

--- fpmsyncd/routesync.cpp
+++ fpmsyncd/routesync.cpp
@@ -145,9 +145,9 @@ static decltype(auto) makeNlAddr(const T& ip)
 }
 
 
-RouteSync::RouteSync(RedisPipeline *pipeline) :
+RouteSync::RouteSync(RedisPipeline *pipeline, const std::string& zmqVrf) :
     // When the feature ORCH_NORTHBOND_ROUTE_ZMQ_ENABLED is enabled, route events must be sent to orchagent via the ZMQ channel.
-    m_zmqClient(create_local_zmq_client(ORCH_NORTHBOND_ROUTE_ZMQ_ENABLED, false)),
+    m_zmqClient(create_local_zmq_client(ORCH_NORTHBOND_ROUTE_ZMQ_ENABLED, false, zmqVrf)),
     m_routeTable(createProducerStateTable(pipeline, APP_ROUTE_TABLE_NAME, true, m_zmqClient)),
     m_nexthop_groupTable(pipeline, APP_NEXTHOP_GROUP_TABLE_NAME, true),
     m_label_routeTable(createProducerStateTable(pipeline, APP_LABEL_ROUTE_TABLE_NAME, true, m_zmqClient)),
