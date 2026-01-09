89ec5cc5e37b52fe958c7615dcff2edc7ec48543
Venkit Kasiviswanathan <venkit@nexthop.ai>
2026-01-09 08:39:29 +0000

    Incorporate review comments for the new test(scope): title
    Signed-off-by: Venkit Kasiviswanathan <venkit@nexthop.ai>

--- tests/test_zmq_mgmt_vrf.py
+++ tests/test_zmq_mgmt_vrf.py
@@ -1,156 +1,10 @@
 import time
-import json
 
 from swsscommon import swsscommon
-from dvslib.dvs_common import wait_for_result
+from test_route import TestRouteBase
 
 
-class TestRouteBase(object):
-    def setup_db(self, dvs):
-        self.pdb = dvs.get_app_db()
-        self.adb = dvs.get_asic_db()
-        self.cdb = dvs.get_config_db()
-        self.sdb = dvs.get_state_db()
-
-    def set_admin_status(self, interface, status):
-        self.cdb.update_entry("PORT", interface, {"admin_status": status})
-
-    def create_vrf(self, vrf_name):
-        initial_entries = set(self.adb.get_keys(
-            "ASIC_STATE:SAI_OBJECT_TYPE_VIRTUAL_ROUTER"))
-
-        self.cdb.create_entry("VRF", vrf_name, {"empty": "empty"})
-        self.adb.wait_for_n_keys("ASIC_STATE:SAI_OBJECT_TYPE_VIRTUAL_ROUTER",
-                                 len(initial_entries) + 1)
-
-        current_entries = set(self.adb.get_keys(
-            "ASIC_STATE:SAI_OBJECT_TYPE_VIRTUAL_ROUTER"))
-        assert len(current_entries - initial_entries) == 1
-        return list(current_entries - initial_entries)[0]
-
-    def remove_vrf(self, vrf_name):
-        self.cdb.delete_entry("VRF", vrf_name)
-
-    def create_l3_intf(self, interface, vrf_name):
-        if len(vrf_name) == 0:
-            self.cdb.create_entry("INTERFACE", interface, {"NULL": "NULL"})
-        else:
-            self.cdb.create_entry("INTERFACE", interface,
-                                  {"vrf_name": vrf_name})
-
-    def remove_l3_intf(self, interface):
-        self.cdb.delete_entry("INTERFACE", interface)
-
-    def add_ip_address(self, interface, ip):
-        self.cdb.create_entry("INTERFACE", interface + "|" + ip,
-                              {"NULL": "NULL"})
-
-    def remove_ip_address(self, interface, ip):
-        self.cdb.delete_entry("INTERFACE", interface + "|" + ip)
-
-    def create_route_entry(self, key, pairs):
-        tbl = swsscommon.ProducerStateTable(self.pdb.db_connection,
-                                            "ROUTE_TABLE")
-        fvs = swsscommon.FieldValuePairs(list(pairs.items()))
-        tbl.set(key, fvs)
-
-    def remove_route_entry(self, key):
-        tbl = swsscommon.ProducerStateTable(self.pdb.db_connection,
-                                            "ROUTE_TABLE")
-        tbl._del(key)
-
-    def check_route_entries(self, destinations):
-        def _access_function():
-            route_entries = self.adb.get_keys(
-                "ASIC_STATE:SAI_OBJECT_TYPE_ROUTE_ENTRY")
-            route_destinations = [json.loads(route_entry)["dest"]
-                                  for route_entry in route_entries]
-            return (all(destination in route_destinations for destination in destinations), None)
-
-        wait_for_result(_access_function)
-
-    def check_route_state(self, prefix, value):
-        found = False
-        fvs = {}
-
-        for _ in range(5):  # Try for up to ~5 seconds
-            route_entries = self.sdb.get_keys("ROUTE_TABLE")
-            for key in route_entries:
-                if key != prefix:
-                    continue
-                found = True
-                fvs = self.sdb.get_entry("ROUTE_TABLE", key)
-                if fvs.get("state") == value:
-                    return
-            time.sleep(1)
-
-        assert found
-        assert fvs.get("state") == \
-            value, f"Expected state '{value}', but got '{fvs.get('state')}'"
-
-    def get_asic_db_key(self, destination):
-        route_entries = self.adb.get_keys(
-            "ASIC_STATE:SAI_OBJECT_TYPE_ROUTE_ENTRY")
-        for route_entry in route_entries:
-            if json.loads(route_entry)["dest"] == destination:
-                return route_entry
-        return None
-
-    def check_route_entries_with_vrf(self, destinations, vrf_oids):
-        def _access_function():
-            route_entries = self.adb.get_keys(
-                "ASIC_STATE:SAI_OBJECT_TYPE_ROUTE_ENTRY")
-            route_destination_vrf = [(json.loads(route_entry)["dest"],
-                                      json.loads(route_entry)["vr"])
-                                     for route_entry in route_entries]
-            return (all((destination, vrf_oid) in route_destination_vrf
-                        for destination, vrf_oid in zip(destinations, vrf_oids)), None)
-
-        wait_for_result(_access_function)
-
-    def check_route_entries_nexthop(self, destinations, vrf_oids, nexthops):
-        def _access_function_nexthop():
-            nexthop_entries = self.adb.get_keys(
-                "ASIC_STATE:SAI_OBJECT_TYPE_NEXT_HOP")
-            nexthop_oids = \
-                dict([(self.adb.get_entry(
-                    "ASIC_STATE:SAI_OBJECT_TYPE_NEXT_HOP",
-                    key)["SAI_NEXT_HOP_ATTR_IP"], key)
-                                 for key in nexthop_entries])
-            return (all(nexthop in nexthop_oids for nexthop in nexthops),
-                    nexthop_oids)
-
-        status, nexthop_oids = wait_for_result(_access_function_nexthop)
-
-        def _access_function_route_nexthop():
-            route_entries = self.adb.get_keys(
-                "ASIC_STATE:SAI_OBJECT_TYPE_ROUTE_ENTRY")
-            route_destination_nexthop = \
-                dict([((json.loads(route_entry)["dest"],
-                        json.loads(route_entry)["vr"]),
-                       self.adb.get_entry(
-                        "ASIC_STATE:SAI_OBJECT_TYPE_ROUTE_ENTRY",
-                        route_entry).get("SAI_ROUTE_ENTRY_ATTR_NEXT_HOP_ID"))
-                      for route_entry in route_entries])
-            return (all(route_destination_nexthop.get(
-                          (destination, vrf_oid)) == nexthop_oids.get(nexthop)
-                        for destination, vrf_oid, nexthop in zip(destinations, vrf_oids, nexthops)), None)
-
-        wait_for_result(_access_function_route_nexthop)
-
-    def check_deleted_route_entries(self, destinations):
-        def _access_function():
-            route_entries = self.adb.get_keys("ASIC_STATE:SAI_OBJECT_TYPE_ROUTE_ENTRY")
-            route_destinations = [json.loads(route_entry)["dest"] for route_entry in route_entries]
-            return (all(destination not in route_destinations for destination in destinations), None)
-
-        wait_for_result(_access_function)
-
-    def clear_srv_config(self, dvs):
-        dvs.servers[0].runcmd("ip address flush dev eth0")
-        dvs.servers[1].runcmd("ip address flush dev eth0")
-        dvs.servers[2].runcmd("ip address flush dev eth0")
-        dvs.servers[3].runcmd("ip address flush dev eth0")
+class TestZmqRouteBase(TestRouteBase):
 
     def toggle_zmq(self, dvs, enable=True):
         cdb_value = "true" if enable else "false"
@@ -205,7 +59,7 @@ class TestRouteBase(object):
         return list(current_entries - initial_entries)[0]
 
 
-class TestRoute(TestRouteBase):
+class TestRoute(TestZmqRouteBase):
 
     """ Functionality tests for route """
 
