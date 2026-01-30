0a6c4f5e34e4ca476eacdb504bfff2608c707505
mint570 <runmingwu@google.com>
2026-01-30 13:14:16 -0500

    Add infra of supporting unique label to be used for SAI label attribute.
    Signed-off-by: SRAVANI KANASANI <kanasanis@google.com>

--- orchagent/orchdaemon.cpp
+++ orchagent/orchdaemon.cpp
@@ -8,6 +8,7 @@
 #include "orchdaemon.h"
 #include "logger.h"
 #include <sairedis.h>
+#include "namelabelmapper.h"
 #include "warm_restart.h"
 #include <iostream>
 #include "orch_zmq_config.h"
@@ -75,7 +76,7 @@ StpOrch *gStpOrch;
 MuxOrch *gMuxOrch;
 IcmpOrch *gIcmpOrch;
 HFTelOrch *gHFTOrch;
-
+NameLabelMapper *gLabelMapper;
 bool gIsNatSupported = false;
 event_handle_t g_events_handle;
 
@@ -187,6 +188,7 @@ void OrchDaemon::disableRingBuffer() {
 bool OrchDaemon::init()
 {
     SWSS_LOG_ENTER();
+    gLabelMapper = new NameLabelMapper();
 
     string platform = getenv("platform") ? getenv("platform") : "";
 
@@ -1111,7 +1113,8 @@ bool OrchDaemon::warmRestoreAndSyncUp()
     SWSS_LOG_ENTER();
 
     WarmStart::setWarmStartState("orchagent", WarmStart::INITIALIZED);
-
+    gLabelMapper->readMapperFromDb();
+     
     for (Orch *o : m_orchList)
     {
         o->bake();
@@ -1182,6 +1185,7 @@ bool OrchDaemon::warmRestoreAndSyncUp()
      * The "RECONCILED" state of orchagent doesn't mean the state related to neighbor is up to date.
      */
     WarmStart::setWarmStartState("orchagent", WarmStart::RECONCILED);
+    gLabelMapper->deleteMapperInDb();
     return true;
 }
 
