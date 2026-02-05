0abb5350d33c9dc27d37e2c6cb6faa7d412f0c86
Saikrishna Arcot <sarcot@microsoft.com>
2026-02-05 14:43:22 -0800

    Add more tests
    Signed-off-by: Saikrishna Arcot <sarcot@microsoft.com>

--- tests/mock_tests/teamsync_ut.cpp
+++ tests/mock_tests/teamsync_ut.cpp
@@ -3,11 +3,15 @@
 #include <dlfcn.h>
 #include <stdexcept>
 #include <team.h>
+#include <teamdctl.h>
 #include "teamsync.h"
 
 static unsigned int (*callback_sleep)(unsigned int seconds) = NULL;
 static int (*callback_team_init)(struct team_handle *th, uint32_t ifindex) = NULL;
 static int (*callback_team_change_handler)(struct team_handle *th, struct team_change_handler *handler, void *priv) = NULL;
+static int (*callback_teamdctl_connect)(struct teamdctl *tdc, const char *team_name, const char *addr, const char *cli_type) = NULL;
+static int (*callback_teamdctl_config_get_raw_direct)(struct teamdctl *tdc, char **response) = NULL;
+static void (*callback_teamdctl_disconnect)(struct teamdctl *tdc) = NULL;
 
 static unsigned int cb_sleep(unsigned int seconds)
 {
@@ -35,7 +39,7 @@ int team_init(struct team_handle *th, uint32_t ifindex)
 {
     if (callback_team_init)
     {
-        return cb_team_init(th, ifindex);
+        return callback_team_init(th, ifindex);
     }
     int (*realfunc)(struct team_handle *, uint32_t) =
         (int    (*)(struct team_handle *, uint32_t))(dlsym (RTLD_NEXT, "team_init"));
@@ -51,11 +55,65 @@ int team_change_handler(struct team_handle *th, struct team_change_handler *hand
 {
     if (callback_team_change_handler)
     {
-        return cb_team_change_handler(th, handler, priv);
+        return callback_team_change_handler(th, handler, priv);
     }
     int (*realfunc)(struct team_handle *, struct team_change_handler*, void*) =
         (int    (*)(struct team_handle *, struct team_change_handler*, void*))(dlsym (RTLD_NEXT, "team_change_handler"));
     return realfunc(th, handler, priv);
+}
+
+static int cb_teamdctl_connect(struct teamdctl *tdc, const char *team_name, const char *addr, const char *cli_type)
+{
+    return 0;
+}
+
+int teamdctl_connect(struct teamdctl *tdc, const char *team_name, const char *addr, const char *cli_type)
+{
+    if (callback_teamdctl_connect)
+    {
+        return callback_teamdctl_connect(tdc, team_name, addr, cli_type);
+    }
+    int (*realfunc)(struct teamdctl *, const char *, const char *, const char *) =
+        (int    (*)(struct teamdctl *, const char *, const char *, const char *))(dlsym (RTLD_NEXT, "teamdctl_connect"));
+    return realfunc(tdc, team_name, addr, cli_type);
+}
+
+static int cb_teamdctl_config_get_raw_direct_force_error(struct teamdctl *tdc, char **response)
+{
+    // Forced error
+    return 1;
+}
+
+static int cb_teamdctl_config_get_raw_direct_success(struct teamdctl *tdc, char **response)
+{
+    return 0;
+}
+
+int teamdctl_config_get_raw_direct(struct teamdctl *tdc, char **response)
+{
+    if (callback_teamdctl_config_get_raw_direct)
+    {
+        return callback_teamdctl_config_get_raw_direct(tdc, response);
+    }
+    int (*realfunc)(struct teamdctl *, char **) =
+        (int    (*)(struct teamdctl *, char **))(dlsym (RTLD_NEXT, "teamdctl_config_get_raw_direct"));
+    return realfunc(tdc, response);
+}
+
+static void cb_teamdctl_disconnect(struct teamdctl *tdc)
+{
+}
+
+void teamdctl_disconnect(struct teamdctl *tdc)
+{
+    if (callback_teamdctl_disconnect)
+    {
+        callback_teamdctl_disconnect(tdc);
+        return;
+    }
+    int (*realfunc)(struct teamdctl *) =
+        (int    (*)(struct teamdctl *))(dlsym (RTLD_NEXT, "teamdctl_disconnect"));
+    realfunc(tdc);
 }
 
 namespace teamportsync_test
@@ -67,6 +125,9 @@ namespace teamportsync_test
             callback_sleep = cb_sleep;
             callback_team_init = NULL;
             callback_team_change_handler = NULL;
+            callback_teamdctl_connect = NULL;
+            callback_teamdctl_config_get_raw_direct = cb_teamdctl_config_get_raw_direct_force_error;
+            callback_teamdctl_disconnect = cb_teamdctl_disconnect;
         }
 
         virtual void TearDown() override
@@ -74,6 +135,9 @@ namespace teamportsync_test
             callback_sleep = NULL;
             callback_team_init = NULL;
             callback_team_change_handler = NULL;
+            callback_teamdctl_connect = NULL;
+            callback_teamdctl_config_get_raw_direct = NULL;
+            callback_teamdctl_disconnect = NULL;
         }
     };
 
@@ -97,15 +161,25 @@ namespace teamportsync_test
         }
     }
 
-    TEST_F(TeamPortSyncTest, TeamdctlConnectFailure)
+    TEST_F(TeamPortSyncTest, TeamdctlNoConfig)
     {
         callback_team_init = cb_team_init;
         callback_team_change_handler = cb_team_change_handler;
+        callback_teamdctl_connect = cb_teamdctl_connect;
         try {
             swss::TeamSync::TeamPortSync("testLag", 4, NULL);
             FAIL();
         } catch (std::runtime_error &exception) {
-            EXPECT_THAT(exception.what(), testing::HasSubstr("Unable to connect to teamd"));
+            EXPECT_THAT(exception.what(), testing::HasSubstr("Unable to get config from teamd"));
         }
+    }
+
+    TEST_F(TeamPortSyncTest, AllSuccess)
+    {
+        callback_team_init = cb_team_init;
+        callback_team_change_handler = cb_team_change_handler;
+        callback_teamdctl_connect = cb_teamdctl_connect;
+        callback_teamdctl_config_get_raw_direct = cb_teamdctl_config_get_raw_direct_success;
+        swss::TeamSync::TeamPortSync("testLag", 4, NULL);
     }
 }
