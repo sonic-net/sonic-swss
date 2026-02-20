b5695ea9ecbbf39f4a271f8cb69b2153ae72b137
mint570 <runmingwu@google.com>
2026-02-20 04:30:47 -0500

    Add IPv6 src IP as new ternary key field for table ipv6_tunnel_termination_table

--- orchagent/p4orch/p4orch_util.h
+++ orchagent/p4orch/p4orch_util.h
@@ -95,6 +95,13 @@ constexpr char *kTos = "tos";
 constexpr char *kMirrorAsIpv4Erspan = "mirror_as_ipv4_erspan";
 constexpr char *kL3AdmitAction = "admit_to_l3";
 constexpr char *kTunnelAction = "mark_for_p2p_tunnel_encap";
+constexpr char* kIpv6TunnelTermAction = "mark_for_tunnel_decap_and_set_vrf";
+constexpr char* kDecapSrcIpv6 = "src_ipv6";
+constexpr char* kDecapDstIpv6 = "dst_ipv6";
+constexpr char* kDecapSrcIpv6Ip = "src_ipv6_ip";
+constexpr char* kDecapSrcIpv6Mask = "src_ipv6_mask";
+constexpr char* kDecapDstIpv6Ip = "dst_ipv6_ip";
+constexpr char* kDecapDstIpv6Mask = "dst_ipv6_mask";
 
 // Field names in P4RT TABLE DEFINITION APP DB entry.
 constexpr char *kTables = "tables";
@@ -300,6 +307,18 @@ struct P4AclRuleAppDbEntry
     P4AclMeterAppDb meter;
 };
 
+struct Ipv6TunnelTermAppDbEntry
+{
+  // Match
+  swss::IpAddress src_ipv6_ip;
+  swss::IpAddress src_ipv6_mask;
+  swss::IpAddress dst_ipv6_ip;
+  swss::IpAddress dst_ipv6_mask;
+  // Action
+  std::string vrf_id;
+  std::string action_str;
+};
+
 struct DepObject
 {
     sai_object_type_t sai_object;
@@ -383,6 +402,10 @@ class KeyGenerator
                                           const uint32_t &priority);
 
     static std::string generateTunnelKey(const std::string &tunnel_id);
+
+    static std::string generateIpv6TunnelTermKey(const swss::IpAddress& src_ipv6_ip,
+                                                 const swss::IpAddress& src_ipv6_mask,
+                                                 const swss::IpAddress& dst_ipv6_ip, const swss::IpAddress& dst_ipv6_mask);
 
     static std::string generateExtTableKey(const std::string &table_name, const std::string &table_key);
 
