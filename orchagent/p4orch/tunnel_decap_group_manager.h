9048f56991c1b8e01120d33119ee3041b9173ac6
mint570 <runmingwu@google.com>
2026-02-20 04:30:47 -0500

    Add IPv6 src IP as new ternary key field for table ipv6_tunnel_termination_table

--- orchagent/p4orch/tunnel_decap_group_manager.h
+++ orchagent/p4orch/tunnel_decap_group_manager.h
@@ -18,7 +18,8 @@ extern "C" {
 // Ipv6TunnelTermTableEntry holds TunnelDecapGroupManager's internal cache of
 // tunnel termination table entry. Example:
 // P4RT:FIXED_IPV6_TUNNEL_TERMINATION_TABLE:{"match/dst_ipv6_64bit":
-//   "2607:f8b0:c145:9300:: & ffff:ffff:ffff:ff00::"}
+//   "2607:f8b0:c145:9300:: & ffff:ffff:ffff:ff00::",
+//   "match/src_ipv6_64bit":"2607:f8b0:c145:9300:: & ffff:ffff:ffff:ff00::"}
 //   "action" = "mark_for_tunnel_decap_and_set_vrf",
 //   "param/vrf_id" = "b4-traffic",
 //   "controller_metadata" = "..."
@@ -29,6 +30,8 @@ struct Ipv6TunnelTermTableEntry {
 
   // Fields from P4 table.
   // Match
+  swss::IpAddress src_ipv6_ip;
+  swss::IpAddress src_ipv6_mask;
   swss::IpAddress dst_ipv6_ip;
   swss::IpAddress dst_ipv6_mask;
   // Action
@@ -39,9 +42,11 @@ struct Ipv6TunnelTermTableEntry {
   // SAI OID of the vrf_id for SAI_TUNNEL_TERM_TABLE_ENTRY_ATTR_VR_ID
   sai_object_id_t vrf_oid = SAI_NULL_OBJECT_ID;
 
-  Ipv6TunnelTermTableEntry(const swss::IpAddress& dst_ipv6_ip,
-                           const swss::IpAddress& dst_ipv6_mask,
-                           const std::string& vrf_id);
+   Ipv6TunnelTermTableEntry(const swss::IpAddress& src_ipv6_ip,
+                            const swss::IpAddress& src_ipv6_mask,
+                            const swss::IpAddress& dst_ipv6_ip,
+                            const swss::IpAddress& dst_ipv6_mask,
+                            const std::string& vrf_id);
 };
 // LINT.ThenChange(tunnel_decap_group_manager.cpp:verify_state_cache)
 
