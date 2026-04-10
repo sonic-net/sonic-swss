925e07c80d3d31cc8d6af1fcf1e216f5ff3d1ca4
mint570 <runmingwu@google.com>
2026-04-10 10:42:28 +0000

    Support the WCMP group watch port pruning when the port LACP state changes.
    Signed-off-by: SRAVANI KANASANI <kanasanis@google.com>

--- orchagent/p4orch/p4orch.h
+++ orchagent/p4orch/p4orch.h
@@ -63,6 +63,8 @@ class P4Orch : public ZmqOrch
     bool bake() override;
     void handlePortStatusUpdate(const std::string& alias,
                               const sai_port_oper_status_t& status);
+    void handleLagMemberLacpStatusUpdate(const std::string& alias,
+                                       bool lacp_enable);
     TablesInfo *tablesinfo = NULL;
 
     // m_p4TableToManagerMap: P4 APP DB table name, P4 Object Manager
