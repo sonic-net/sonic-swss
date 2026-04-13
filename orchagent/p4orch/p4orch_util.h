4a350277f42cedcb47edaf43564e0fda8eeaf2c3
mint570 <runmingwu@google.com>
2026-04-13 10:55:33 +0000

    Add support to redirect to a L3 multicast group.
    Signed-off-by: SRAVANI KANASANI <kanasanis@google.com>

--- orchagent/p4orch/p4orch_util.h
+++ orchagent/p4orch/p4orch_util.h
@@ -73,6 +73,7 @@ constexpr char *kStage = "stage";
 constexpr char *kSize = "size";
 constexpr char *kPriority = "priority";
 constexpr char *kPacketColor = "packet_color";
+constexpr char *kObjectType = "object_type";
 constexpr char *kMeterUnit = "meter/unit";
 constexpr char *kCounterUnit = "counter/unit";
 constexpr char kFieldDelimiter = '/';
@@ -280,6 +281,7 @@ struct P4ActionParamName
 {
     std::string sai_action;
     std::string p4_param_name;
+    std::string sai_object_type;  // optionally included for some sai_actions.
 };
 
 struct P4PacketActionWithColor
@@ -418,6 +420,9 @@ class KeyGenerator
     static std::string generateMulticastRouterInterfaceRifKey(
         const std::string& multicast_replica_port,
         const swss::MacAddress& src_mac);
+
+    static std::string generateL3MulticastGroupKey(
+        const std::string& multicast_group_id);
 
     static std::string generateIpMulticastKey(const std::string& vrf_id,
                                               const swss::IpAddress& ip_dst);
