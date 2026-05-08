37abcc89d5b91e24ea86109d74e4ae1bc90bcb37
mint570 <runmingwu@google.com>
2026-05-08 11:03:08 +0000

    Use SAI bulk API for mcast group.
    Signed-off-by: SRAVANI KANASANI <kanasanis@google.com>

--- orchagent/p4orch/tests/mock_sai_ipmc_group.cpp
+++ orchagent/p4orch/tests/mock_sai_ipmc_group.cpp
@@ -45,3 +45,38 @@ sai_status_t mock_get_ipmc_group_member_attribute(
       ipmc_group_member_id, attr_count, attr_list);
 }
 
+sai_status_t mock_create_ipmc_groups(_In_ sai_object_id_t switch_id,
+                                     _In_ uint32_t object_count,
+                                     _In_ const uint32_t* attr_count,
+                                     _In_ const sai_attribute_t** attr_list,
+                                     _In_ sai_bulk_op_error_mode_t mode,
+                                     _Out_ sai_object_id_t* object_id,
+                                     _Out_ sai_status_t* object_statuses) {
+  return mock_sai_ipmc_group->create_ipmc_groups(switch_id, object_count,
+                                                 attr_count, attr_list, mode,
+                                                 object_id, object_statuses);
+}
+
+sai_status_t mock_remove_ipmc_groups(_In_ uint32_t object_count,
+                                     _In_ const sai_object_id_t* object_id,
+                                     _In_ sai_bulk_op_error_mode_t mode,
+                                     _Out_ sai_status_t* object_statuses) {
+  return mock_sai_ipmc_group->remove_ipmc_groups(object_count, object_id, mode,
+                                                 object_statuses);
+}
+
+sai_status_t mock_set_ipmc_groups_attribute(
+    _In_ uint32_t object_count, _In_ const sai_object_id_t* object_id,
+    _In_ const sai_attribute_t* attr_list, _In_ sai_bulk_op_error_mode_t mode,
+    _Out_ sai_status_t* object_statuses) {
+  return mock_sai_ipmc_group->set_ipmc_groups_attribute(
+      object_count, object_id, attr_list, mode, object_statuses);
+}
+
+sai_status_t mock_get_ipmc_groups_attribute(
+    _In_ uint32_t object_count, _In_ const sai_object_id_t* object_id,
+    _In_ const uint32_t* attr_count, _Inout_ sai_attribute_t** attr_list,
+    _In_ sai_bulk_op_error_mode_t mode, _Out_ sai_status_t* object_statuses) {
+  return mock_sai_ipmc_group->get_ipmc_groups_attribute(
+      object_count, object_id, attr_count, attr_list, mode, object_statuses);
+}
--- orchagent/p4orch/tests/mock_sai_ipmc_group.h
+++ orchagent/p4orch/tests/mock_sai_ipmc_group.h
@@ -39,6 +39,36 @@ class MockSaiIpmcGroup {
                sai_status_t(_In_ sai_object_id_t ipmc_group_member_id,
                             _In_ uint32_t attr_count,
                             _Inout_ sai_attribute_t* attr_list));
+
+  MOCK_METHOD7(create_ipmc_groups,
+               sai_status_t(_In_ sai_object_id_t switch_id,
+                            _In_ uint32_t object_count,
+                            _In_ const uint32_t* attr_count,
+                            _In_ const sai_attribute_t** attr_list,
+                            _In_ sai_bulk_op_error_mode_t mode,
+                            _Out_ sai_object_id_t* object_id,
+                            _Out_ sai_status_t* object_statuses));
+
+  MOCK_METHOD4(remove_ipmc_groups,
+               sai_status_t(_In_ uint32_t object_count,
+                            _In_ const sai_object_id_t* object_id,
+                            _In_ sai_bulk_op_error_mode_t mode,
+                            _Out_ sai_status_t* object_statuses));
+
+  MOCK_METHOD5(set_ipmc_groups_attribute,
+               sai_status_t(_In_ uint32_t object_count,
+                            _In_ const sai_object_id_t* object_id,
+                            _In_ const sai_attribute_t* attr_list,
+                            _In_ sai_bulk_op_error_mode_t mode,
+                            _Out_ sai_status_t* object_statuses));
+
+  MOCK_METHOD6(get_ipmc_groups_attribute,
+               sai_status_t(_In_ uint32_t object_count,
+                            _In_ const sai_object_id_t* object_id,
+                            _In_ const uint32_t* attr_count,
+                            _Inout_ sai_attribute_t** attr_list,
+                            _In_ sai_bulk_op_error_mode_t mode,
+                            _Out_ sai_status_t* object_statuses));
 };
 
 extern MockSaiIpmcGroup* mock_sai_ipmc_group;
@@ -67,3 +97,26 @@ sai_status_t mock_set_ipmc_group_member_attribute(
 sai_status_t mock_get_ipmc_group_member_attribute(
     _In_ sai_object_id_t ipmc_group_member_id, _In_ uint32_t attr_count,
     _Inout_ sai_attribute_t* attr_list);
+
+sai_status_t mock_create_ipmc_groups(_In_ sai_object_id_t switch_id,
+                                     _In_ uint32_t object_count,
+                                     _In_ const uint32_t* attr_count,
+                                     _In_ const sai_attribute_t** attr_list,
+                                     _In_ sai_bulk_op_error_mode_t mode,
+                                     _Out_ sai_object_id_t* object_id,
+                                     _Out_ sai_status_t* object_statuses);
+
+sai_status_t mock_remove_ipmc_groups(_In_ uint32_t object_count,
+                                     _In_ const sai_object_id_t* object_id,
+                                     _In_ sai_bulk_op_error_mode_t mode,
+                                     _Out_ sai_status_t* object_statuses);
+
+sai_status_t mock_set_ipmc_groups_attribute(
+    _In_ uint32_t object_count, _In_ const sai_object_id_t* object_id,
+    _In_ const sai_attribute_t* attr_list, _In_ sai_bulk_op_error_mode_t mode,
+    _Out_ sai_status_t* object_statuses);
+
+sai_status_t mock_get_ipmc_groups_attribute(
+    _In_ uint32_t object_count, _In_ const sai_object_id_t* object_id,
+    _In_ const uint32_t* attr_count, _Inout_ sai_attribute_t** attr_list,
+    _In_ sai_bulk_op_error_mode_t mode, _Out_ sai_status_t* object_statuses);
