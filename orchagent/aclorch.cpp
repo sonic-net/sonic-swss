f067e23585dc78f995bc6ef1ab3e7343cdf87b2f
rajkumar1 <rajkumar1@arista.com>
2026-08-19 19:42:06 +0000

    Fix orchagent  on shutdown in active-standby dualtor topologies.
    Signed-off-by: rajkumar1 <rajkumar1@arista.com>

--- orchagent/aclorch.cpp
+++ orchagent/aclorch.cpp
@@ -33,6 +33,7 @@ extern sai_object_id_t   gSwitchId;
 extern PortsOrch*        gPortsOrch;
 extern CrmOrch *gCrmOrch;
 extern SwitchOrch *gSwitchOrch;
+extern AclOrch *gAclOrch;
 extern PolicerOrch *gPolicerOrch;
 extern string gMySwitchType;
 extern Directory<Orch*> gDirectory;
@@ -4486,6 +4487,8 @@ AclOrch::~AclOrch()
     }
 
     deleteDTelWatchListTables();
+
+    gAclOrch = nullptr;
 }
 
 void AclOrch::update(SubjectType type, void *cntx)
@@ -6284,7 +6287,7 @@ sai_object_id_t AclOrch::getTableById(string table_id)
         return SAI_NULL_OBJECT_ID;
     }
 
-    for (auto it : m_AclTables)
+    for (const auto& it : m_AclTables)
     {
         if (it.second.id == table_id)
         {
